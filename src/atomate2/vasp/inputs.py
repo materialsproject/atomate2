18e41bf0d93d74e6e8b1a22b483bca1a7268e67f
Alex Ganose <alexganose@googlemail.com>
2021-06-15 12:16:32 +0100

    Fix loading vasp input set

--- src/atomate2/vasp/inputs.py
+++ src/atomate2/vasp/inputs.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
+import importlib
+import logging
 import typing
-
-from custodian.cli.run_vasp import load_class
 
 if typing.TYPE_CHECKING:
     from typing import Optional
@@ -12,6 +12,8 @@ if typing.TYPE_CHECKING:
     from pymatgen.core.structure import Structure
 
 __all__ = ["write_vasp_input_set"]
+
+logger = logging.getLogger(__name__)
 
 
 def write_vasp_input_set(
@@ -42,10 +44,20 @@ def write_vasp_input_set(
     input_set_kwargs = {} if input_set_kwargs is None else input_set_kwargs
     write_input_kwargs = {} if write_input_kwargs is None else write_input_kwargs
 
-    vis_cls = load_class("pymatgen.io.vasp.sets", input_set)
+    if "." in input_set:
+        module, input_set = input_set.rsplit(".", 1)
+    else:
+        module = "pymatgen.io.vasp.sets"
+
+    try:
+        vis_cls = getattr(importlib.import_module(module), input_set)
+    except (ModuleNotFoundError, AttributeError, ImportError):
+        raise ImportError(f"Could not import input set {input_set} from {module}.")
+
     if from_prev:
         vis = vis_cls.from_prev(".", structure=structure, **input_set_kwargs)
     else:
         vis = vis_cls(structure, **input_set_kwargs)
 
+    logger.info("Writing VASP input set.")
     vis.write_input(".", **write_input_kwargs)
