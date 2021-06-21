36b47130e77c89b3a3c633ea2be6fae71d0acf71
Alex Ganose <alexganose@googlemail.com>
2021-06-21 08:19:27 +0100

    Add band structure maker

--- src/atomate2/vasp/flows/core.py
+++ src/atomate2/vasp/flows/core.py
@@ -10,7 +10,8 @@ from jobflow import Flow, Maker
 from pymatgen.core.structure import Structure
 
 from atomate2.vasp.jobs.base import BaseVaspMaker
-from atomate2.vasp.jobs.core import RelaxMaker
+from atomate2.vasp.jobs.core import NonSCFMaker, RelaxMaker, StaticMaker
+from atomate2.vasp.schemas.calculation import VaspObject
 
 __all__ = ["DoubleRelaxMaker"]
 
@@ -56,3 +57,46 @@ class DoubleRelaxMaker(Maker):
         relax2.name += " 2"
 
         return Flow([relax1, relax2], relax2.output, name=self.name)
+
+
+class BandStructureMaker(Maker):
+    name: str = "band structure"
+    bandstructure_type: str = "both"
+    static_maker: BaseVaspMaker = field(default_factory=StaticMaker)
+    nscf_maker: NonSCFMaker = field(default_factory=NonSCFMaker)
+
+    def make(self, structure, prev_vasp_dir=None):
+        static_job = self.static_maker.make(structure, prev_vasp_dir=prev_vasp_dir)
+        jobs = [static_job]
+
+        outputs = {}
+        if self.bandstructure_type in ("both", "uniform"):
+            uniform_job = self.nscf_maker.make(
+                structure, prev_vasp_dir=static_job.output.dir_name, mode="uniform"
+            )
+            uniform_job.name += " uniform"
+            jobs.append(uniform_job)
+            output = {
+                "uniform": uniform_job.output,
+                "uniform_bs": uniform_job.output.vasp_objects[VaspObject.BANDSTRUCTURE],
+            }
+            outputs.update(output)
+
+        if self.bandstructure_type in ("both", "line"):
+            line_job = self.nscf_maker.make(
+                structure, prev_vasp_dir=static_job.output.dir_name, mode="line"
+            )
+            line_job.name += " line"
+            jobs.append(line_job)
+            output = {
+                "line": line_job.output,
+                "line_bs": line_job.output.vasp_objects[VaspObject.BANDSTRUCTURE],
+            }
+            outputs.update(output)
+
+        if self.bandstructure_type not in ("both", "line", "uniform"):
+            raise ValueError(
+                f"Unrecognised bandstructure type {self.bandstructure_type}"
+            )
+
+        return Flow(jobs, outputs, name=self.name)
