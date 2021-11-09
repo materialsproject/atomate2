ae3f8b6882e87fc55e1fff840e8869337b7d5996
Alex Ganose <alexganose@googlemail.com>
2021-11-09 18:33:39 +0000

    Update elastic test data

--- tests/vasp/flows/test_elastic.py
+++ tests/vasp/flows/test_elastic.py
@@ -5,6 +5,7 @@ def test_elastic(mock_vasp, clean_dir, si_structure):
 
     from atomate2.common.schemas.elastic import ElasticDocument
     from atomate2.vasp.flows.elastic import ElasticMaker
+    from atomate2.vasp.powerups import update_user_kpoints_settings
 
     # mapping from job name to directory containing test files
     ref_paths = {
@@ -14,7 +15,8 @@ def test_elastic(mock_vasp, clean_dir, si_structure):
         "elastic relax 4/6": "Si_elastic/elastic_relax_4_6",
         "elastic relax 5/6": "Si_elastic/elastic_relax_5_6",
         "elastic relax 6/6": "Si_elastic/elastic_relax_6_6",
-        "tight relax": "Si_elastic/tight_relax",
+        "tight relax 1": "Si_elastic/tight_relax_1",
+        "tight relax 2": "Si_elastic/tight_relax_2",
     }
 
     # settings passed to fake_run_vasp; adjust these to check for certain INCAR settings
@@ -25,7 +27,8 @@ def test_elastic(mock_vasp, clean_dir, si_structure):
         "elastic relax 4/6": {"incar_settings": ["NSW", "ISMEAR"]},
         "elastic relax 5/6": {"incar_settings": ["NSW", "ISMEAR"]},
         "elastic relax 6/6": {"incar_settings": ["NSW", "ISMEAR"]},
-        "tight relax": {"incar_settings": ["NSW", "ISMEAR"]},
+        "tight relax 1": {"incar_settings": ["NSW", "ISMEAR"]},
+        "tight relax 2": {"incar_settings": ["NSW", "ISMEAR"]},
     }
 
     # automatically use fake VASP and write POTCAR.spec during the test
@@ -34,11 +37,7 @@ def test_elastic(mock_vasp, clean_dir, si_structure):
     # generate flow
     si_prim = SpacegroupAnalyzer(si_structure).get_primitive_standard_structure()
     flow = ElasticMaker().make(si_prim)
-    flow.update_maker_kwargs(
-        {"_set": {"input_set_generator->user_kpoints_settings->grid_density": 100}},
-        name_filter="relax",
-        dict_mod=True,
-    )
+    update_user_kpoints_settings(flow, {"grid_density": 100}, name_filter="relax")
 
     # Run the flow or job and ensure that it finished running successfully
     responses = run_locally(flow, create_folders=True, ensure_success=True)
