Fix calculation of dielectric constants in amset workflow

src/atomate2/vasp/flows/amset.py
@@ -245,7 +245,7 @@ class VaspAmsetMaker(Maker):
         phonon_frequency = calculate_polar_phonon_frequency(
             dielectric.output.structure,
             dielectric.output.calcs_reversed[0].output.normalmode_frequencies,
-            dielectric.output.calcs_reversed[0].output.normalmode_eigenvectors,
+            dielectric.output.calcs_reversed[0].output.normalmode_eigenvecs,
             dielectric.output.calcs_reversed[0].output.outcar.born,
         )
 
@@ -263,8 +263,11 @@ class VaspAmsetMaker(Maker):
         # note: the naming of dielectric constants in VASP and pymatgen is wrong
         high_freq_dielectric = dielectric.output.calcs_reversed[0].output.epsilon_static
         static_dielectric = job(np.sum)(
-            dielectric.output.calcs_reversed[0].output.epsilon_ionic,
-            high_freq_dielectric,
+            [
+                dielectric.output.calcs_reversed[0].output.epsilon_ionic,
+                high_freq_dielectric,
+            ],
+            axis=0,
         )
 
         jobs += [
@@ -420,7 +423,7 @@ class HSEVaspAmsetMaker(Maker):
         phonon_frequency = calculate_polar_phonon_frequency(
             dielectric.output.structure,
             dielectric.output.calcs_reversed[0].output.normalmode_frequencies,
-            dielectric.output.calcs_reversed[0].output.normalmode_eigenvectors,
+            dielectric.output.calcs_reversed[0].output.normalmode_eigenvecs,
             dielectric.output.calcs_reversed[0].output.outcar.born,
         )
 
@@ -438,8 +441,11 @@ class HSEVaspAmsetMaker(Maker):
         # note: the naming of dielectric constants in VASP and pymatgen is wrong
         high_freq_dielectric = dielectric.output.calcs_reversed[0].output.epsilon_static
         static_dielectric = job(np.sum)(
-            dielectric.output.calcs_reversed[0].output.epsilon_ionic,
-            high_freq_dielectric,
+            [
+                dielectric.output.calcs_reversed[0].output.epsilon_ionic,
+                high_freq_dielectric,
+            ],
+            axis=0,
         )
 
         jobs += [

src/atomate2/vasp/jobs/amset.py
@@ -257,7 +257,7 @@ def run_amset_deformations(
         statics.append(static_job)
 
         # extract the outputs we want (only the dir name)
-        outputs.append(static_job.dir_name)
+        outputs.append(static_job.output.dir_name)
 
     static_flow = Flow(statics, outputs)
     return Response(replace=static_flow)
@@ -352,16 +352,15 @@ def calculate_polar_phonon_frequency(
         - "frequencies" (list[float]): A list of all phonon frequencies.
         - "weights" (list[float]): A list of weights for the frequencies.
     """
-    frequencies = np.array(frequencies)
-    eigenvectors = np.array(eigenvectors)
-    born_effective_charges = np.array(born_effective_charges)
-
     effective_frequency, weights = calculate_effective_phonon_frequency(
-        frequencies, eigenvectors, born_effective_charges, structure
+        np.array(frequencies),
+        np.array(eigenvectors),
+        np.array(born_effective_charges),
+        structure,
     )
     return {
         "frequency": effective_frequency,
-        "weights": weights,
+        "weights": weights.tolist(),
         "frequencies": frequencies,
     }
 