676e7de584e0357df716657066e4bd0dd9147011
jmmshn <jmmshn@gmail.com>
2022-03-09 09:33:31 -0800

    WIP ccd wf

--- src/atomate2/vasp/flows/defect.py
+++ src/atomate2/vasp/flows/defect.py
@@ -4,18 +4,19 @@
 import logging
 from dataclasses import dataclass, field
 
-from jobflow import Maker
+from jobflow import Flow, Maker
 from pymatgen.core.structure import Structure
 
 from atomate2.vasp.jobs.base import BaseVaspMaker
 from atomate2.vasp.jobs.core import RelaxMaker, StaticMaker
+from atomate2.vasp.jobs.defect import calculate_energy_curve
 from atomate2.vasp.sets.core import RelaxSetGenerator
 from atomate2.vasp.sets.defect import AtomicRelaxSetGenerator
 
 logger = logging.getLogger(__name__)
 
 
-DEFAULT_DISTORTIONS = (-0.15 - 0.1, 0.05, 0.05, 0.1, 0.15, 1)
+DEFAULT_DISTORTIONS = (-0.15 - 0.1, 0.05, 0, 0.05, 0.1, 0.15, 1)
 DEFECT_RELAX_GEN = AtomicRelaxSetGenerator(use_structure_charge=True)
 
 
@@ -51,10 +52,35 @@ class ConfigurationCoordinateMaker(Maker):
             The excited charge state of the defect
 
         """
-        struct1 = structure.copy()
+        name = f"{self.name}: {structure.formula}({charge_state1}-{charge_state2})"
+        struct1: Structure = structure.copy()
         struct1.set_charge(charge_state1)
-        struct2 = structure.copy()
+        struct2: Structure = structure.copy()
         struct2.set_charge(charge_state2)
 
-        self.relax_maker.make(struct1)
-        self.relax_maker.make(struct2)
+        relax1 = self.relax_maker.make(struct1)
+        relax2 = self.relax_maker.make(struct2)
+
+        struct1 = relax1.output.structure
+        struct2 = relax2.output.structure
+        static1 = self.static_maker.make(struct1)
+        static2 = self.static_maker.make(struct2)
+        static1_dir = static1.output.dir_name
+        static2_dir = static2.output.dir_name
+
+        deformations1 = calculate_energy_curve(
+            struct1,
+            struct2,
+            distortions=self.distortions,
+            static_maker=self.static_maker,
+            prev_base_dir=static1_dir,
+        )
+        deformations2 = calculate_energy_curve(
+            struct2,
+            struct1,
+            distortions=self.distortions,
+            static_maker=self.static_maker,
+            prev_base_dir=static2_dir,
+        )
+        jobs = [relax1, relax2, static1, static2, deformations1, deformations2]
+        return Flow(jobs=jobs, name=name, output=[deformations1, deformations2])
--- src/atomate2/vasp/jobs/defect.py
+++ src/atomate2/vasp/jobs/defect.py
@@ -1,5 +1,85 @@
 """Definition of defect job maker."""
-
 import logging
+from pathlib import Path
+from typing import Iterable
+
+import numpy as np
+from jobflow import Flow, Response, job
+from pymatgen.core import Structure
+
+from atomate2.vasp.jobs.core import StaticMaker
 
 logger = logging.getLogger(__name__)
+
+
+@job
+def calculate_energy_curve(
+    ref: Structure,
+    distorted: Structure,
+    distortions: Iterable[float],
+    static_maker: StaticMaker,
+    prev_vasp_dir: str | Path | None = None,
+):
+    """
+    Compute the total energy curve as you distort a reference structure to a distorted structure.
+
+    Parameters
+    ----------
+    ref : pymatgen.core.structure.Structure
+        pymatgen structure corresponding to the ground (final) state
+    distorted : pymatgen.core.structure.Structure
+        pymatgen structure corresponding to the excited (initial) state
+    static_maker : atomate2.vasp.jobs.core.StaticMaker
+        StaticMaker object
+    distortions : tuple
+        list of distortions to apply
+
+    Returns
+    -------
+    Response
+        Response object
+    """
+    jobs = []
+    outputs = []
+
+    # add the static job for the reference structure
+    static_maker.make(ref)
+
+    distorted_structures = ref.interpolate(distorted, nimages=sorted(distortions))
+    # add all the distorted structures
+    for _, d_struct in enumerate(distorted_structures):
+        static_job = static_maker.make(d_struct, prev_vasp_dir=prev_vasp_dir)
+        jobs.append(static_job)
+        dQ = get_dQ(ref, d_struct)
+        outputs.append((dQ, static_job.output))
+
+    add_flow = Flow(jobs, outputs)
+    return Response(output=outputs, replace=add_flow)
+
+
+def get_dQ(ref: Structure, distorted: Structure) -> float:
+    """
+    Calculate dQ from the initial and final structures.
+
+    Parameters
+    ----------
+    ground : pymatgen.core.structure.Structure
+        pymatgen structure corresponding to the ground (final) state
+    excited : pymatgen.core.structure.Structure
+        pymatgen structure corresponding to the excited (initial) state
+
+    Returns
+    -------
+    float
+        the dQ value (amu^{1/2} Angstrom)
+    """
+    return np.sqrt(
+        np.sum(
+            list(
+                map(
+                    lambda x: x[0].distance(x[1]) ** 2 * x[0].specie.atomic_mass,
+                    zip(ref, distorted),
+                )
+            )
+        )
+    )
