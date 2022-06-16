788e7aa6aca84d28af9edaa5a743b0c806654b60
David Waroquiers <david.waroquiers@matgenix.com>
2022-06-16 13:02:05 +0200

    Removed old commented code in abinit.utils.settings.

--- src/atomate2/abinit/utils/settings.py
+++ src/atomate2/abinit/utils/settings.py
@@ -2,32 +2,6 @@
 
 from abipy.flowtk.tasks import TaskManager
 
-# from pydantic import BaseSettings, Field
-
-
-# class AbinitAtomateSettings(BaseSettings):
-#     """Settings for abinit in atomate2."""
-#
-#     MPIRUN_CMD: str = Field("mpirun", description="Mpirun command.")
-#
-#     # ABINIT specific settings
-#     ABINIT_CMD: str = Field("abinit", description="Abinit command.")
-#     MRGDDB_CMD: str = Field("mrgddb", description="Mrgddb command.")
-#     ANADDB_CMD: str = Field("anaddb", description="Anaddb command.")
-#     COPY_DEPS: bool = Field(
-#         False,
-#         description="Copy (True) or link file dependencies between jobs.",
-#     )
-#     AUTOPARAL: bool = Field(
-#         False,
-#         description="Use autoparal to determine optimal parallel configuration.",
-#     )
-#     ABIPY_MANAGER_FILE: str = Field(
-#         None,
-#         description="Config file for task manager of abipy.",
-#     )
-#     MAX_RESTARTS: int = Field(5, description="Maximum number of restarts of a job.")
-
 
 def get_abipy_manager(settings):
     """Get abipy manager.
