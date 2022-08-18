2cfa3388861828518789dfbde585730f71a2cb87
rdguha <rdguha@lbl.gov>
2022-08-18 16:19:31 -0700

    add input sets for different qchem calculations

--- src/atomate2/qchem/sets/core.py
+++ src/atomate2/qchem/sets/core.py
@@ -10,4 +10,123 @@ from typing import Any, Dict, List, Optional, Tuple
 import numpy as np
 from pymatgen.core.structure import Molecule
 
-from atomate2.qchem.sets.base import Q 
+from atomate2.qchem.sets.base import QChemInputGenerator
+
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    "SinglePointSetGenerator",
+    "OptSetGenerator",
+    "TransitionStateSetGenerator",
+    "ForceSetGenerator",
+    "FreqSetGenerator",
+    "PESScanSetGenerator",
+]
+
+@dataclass
+class SinglePointSetGenerator(QChemInputGenerator):
+    """Class to generate QChem Single Point input sets."""
+
+    def get_basis_set_updates(
+            self,
+            molecule: Molecule,
+            prev_basis: str = None,
+            prev_scf: str = None,
+            new_geom_opt: dict = None,
+            nbo_params: dict = None,
+            overwrite_inputs: dict = None, 
+        ) -> dict:
+            """
+            Get updates to the basis set for a single point calculation.
+            """
+            return {"basis_set": "def2-tzvppd", "scf_algorithm": "diis", "job_type": "sp"}
+
+@dataclass
+class OptSetGenerator(QChemInputGenerator):
+    """Class to generate QChem Optimization input sets."""
+
+    def get_basis_set_updates(
+            self,
+            molecule: Molecule,
+            prev_basis: str = None,
+            prev_scf: str = None,
+            new_geom_opt: dict = None,
+            nbo_params: dict = None,
+            overwrite_inputs: dict = None, 
+        ) -> dict:
+            """
+            Get updates to the basis set for a geometry optimization calculation.
+            """
+            return {"basis_set": "def2-tzvppd", "scf_algorithm": "diis", "job_type": "opt"}
+
+@dataclass
+class TransitionStateSetGenerator(QChemInputGenerator):
+    """Class to generate QChem Transition State calculation input sets."""
+
+    def get_basis_set_updates(
+            self,
+            molecule: Molecule,
+            prev_basis: str = None,
+            prev_scf: str = None,
+            new_geom_opt: dict = None,
+            nbo_params: dict = None,
+            overwrite_inputs: dict = None, 
+        ) -> dict:
+            """
+            Get updates to the basis set for a transition state calculation.
+            """
+            return {"basis_set": "def2-tzvppd", "scf_algorithm": "diis", "job_type": "ts"}
+
+@dataclass
+class ForceSetGenerator(QChemInputGenerator):
+    """Class to generate QChem force input sets."""
+
+    def get_basis_set_updates(
+            self,
+            molecule: Molecule,
+            prev_basis: str = None,
+            prev_scf: str = None,
+            new_geom_opt: dict = None,
+            nbo_params: dict = None,
+            overwrite_inputs: dict = None, 
+        ) -> dict:
+            """
+            Get updates to the basis set for a force(gradient) calculation.
+            """
+            return {"basis_set": "def2-tzvppd", "scf_algorithm": "diis", "job_type": "force"}
+
+@dataclass
+class FreqSetGenerator(QChemInputGenerator):
+    """Class to generate QChem frequency calculation input sets."""
+
+    def get_basis_set_updates(
+            self,
+            molecule: Molecule,
+            prev_basis: str = None,
+            prev_scf: str = None,
+            new_geom_opt: dict = None,
+            nbo_params: dict = None,
+            overwrite_inputs: dict = None, 
+        ) -> dict:
+            """
+            Get updates to the basis set for a frequency calculation.
+            """
+            return {"basis_set": "def2-tzvppd", "scf_algorithm": "diis", "job_type": "freq"}
+
+@dataclass
+class PESScanSetGenerator(QChemInputGenerator):
+    """Class to generate QChem PES scan input sets."""
+
+    def get_basis_set_updates(
+            self,
+            molecule: Molecule,
+            prev_basis: str = None,
+            prev_scf: str = None,
+            new_geom_opt: dict = None,
+            nbo_params: dict = None,
+            overwrite_inputs: dict = None, 
+        ) -> dict:
+            """
+            Get updates to the basis set for a pes scan calculation.
+            """
+            return {"basis_set": "def2-tzvppd", "scf_algorithm": "diis", "job_type": "pes_scan"}
