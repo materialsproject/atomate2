e382da33e884f59f917e5eb6d89aeba426cc366f
JaGeo <janine.george@bam.de>
2022-09-12 10:51:29 +0200

    Fix structure precision error1

--- src/atomate2/vasp/schemas/calculation.py
+++ src/atomate2/vasp/schemas/calculation.py
@@ -673,7 +673,7 @@ class Calculation(BaseModel):
         volumetric_files = [] if volumetric_files is None else volumetric_files
         vasprun = Vasprun(vasprun_file, **vasprun_kwargs)
         outcar = Outcar(outcar_file)
-        contcar = Poscar(contcar_file)
+        contcar = Poscar.from_file(contcar_file)
         completed_at = str(datetime.fromtimestamp(vasprun_file.stat().st_mtime))
 
         output_file_paths = _get_output_file_paths(volumetric_files)
