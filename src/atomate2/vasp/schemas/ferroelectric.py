00f1d9c96b53d94b39a33e72dcda3a63a6f69265
fraricci <frankyricci@gmail.com>
2022-10-31 16:05:17 -0700

    fix typing

--- src/atomate2/vasp/schemas/ferroelectric.py
+++ src/atomate2/vasp/schemas/ferroelectric.py
@@ -1,6 +1,6 @@
 """Schemas for crystal symmetry."""
 
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from jobflow.utils import ValueEnum
 from pydantic import BaseModel, Field
@@ -26,13 +26,13 @@ class PolarizationDocument(BaseModel):
         None, title="WF id", description="The workflow id"
     )
 
-    task_label_order: list[str] = Field(
+    task_label_order: List[str] = Field(
         None,
         title="Task Labels Ordered",
         description="Task labels in order from nonpolar to polar",
     )
 
-    polarization_change: list[float] = Field(
+    polarization_change: List[float] = Field(
         None,
         title="Polarization Vector",
         description="The polarization vector",
@@ -44,22 +44,22 @@ class PolarizationDocument(BaseModel):
         description="The norm of the polarization vector",
     )
 
-    same_branch_polarization: list[list[float]]  = Field(
+    same_branch_polarization: List[List[float]]  = Field(
         None, title="Same Branch Polarization Vectors",
         description="Polarization vectors in the same branch",
     )
 
-    raw_electron_polarization:  list[float] = Field(
+    raw_electron_polarization:  List[float] = Field(
         None, title="Raw Electron Polarization",
         description="Electronic contribution to the polarization",
     )
 
-    raw_ion_polarization:  list[float] = Field(
+    raw_ion_polarization:  List[float] = Field(
         None, title="Raw Ions Polarization",
         description="Ionic contribution to the polarization",
     )
 
-    polarization_quanta: list[list[float]] = Field(
+    polarization_quanta: List[List[float]] = Field(
         None, title="Polarization Quanta",
         description="Quanta of polarization for each structure and direction",
     )
@@ -69,12 +69,12 @@ class PolarizationDocument(BaseModel):
         description="Charge of the atoms as in pseudopotentials",
     )
 
-    energies: list[float] = Field(
+    energies: List[float] = Field(
         None, title="Energies",
         description="Total energy of each structure",
     )
 
-    energies_per_atom: list[float] = Field(
+    energies_per_atom: List[float] = Field(
         None, title="Total energy per atom of each structure",
         description=""
     )
@@ -84,7 +84,7 @@ class PolarizationDocument(BaseModel):
         description="VASP Outcar for each structure",
     )
 
-    structures: list[Structure] = Field(
+    structures: List[Structure] = Field(
         None, title="Structures",
         description="All the interpolated structures",
     )
