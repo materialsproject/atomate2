6cb67d10d732a8fe76889fdad5265d2116c8f974
@jmmshn <jmmshn@gmail.com>
2022-11-29 10:32:20 -0800

    test for locpots

--- tests/vasp/flows/test_defect.py
+++ tests/vasp/flows/test_defect.py
@@ -225,6 +225,13 @@ def test_formation_energy_maker(mock_vasp, clean_dir, test_dir, monkeypatch):
         else:
             assert def_ent.corrections["potential_alignment"] != 0
 
+    # check that the locpot data was written:
+    data_out = list(store.query())
+    from monty.serialization import dumpfn
+
+    dumpfn(data_out, "/Users/shen9/Desktop/data_out.json")
+    assert store.additional_stores["data"].count({}) == 3
+
     # assert isinstance(res_defect, FormationEnergyDiagramDocument)
 
     # # check that the defect object was stored
