c29a6b780e39f55665451982ab27046ac42543b1
@jmmshn <jmmshn@gmail.com>
2022-12-02 09:17:52 -0800

    form_en jobs

--- src/atomate2/common/analysis/defects/flows.py
+++ src/atomate2/common/analysis/defects/flows.py
@@ -4,13 +4,19 @@ from __future__ import annotations
 
 import logging
 from dataclasses import dataclass
+from pathlib import Path
 
+import numpy.typing as npt
 from jobflow import Flow, Job, Maker, OutputReference
+from pymatgen.analysis.defects.core import Defect
 from pymatgen.core.structure import Structure
 
 from atomate2.common.analysis.defects.jobs import (
+    bulk_supercell_calculation,
     get_ccd_documents,
     get_charged_structures,
+    get_supercell_from_prv_calc,
+    spawn_defect_calcs,
     spawn_energy_curve_calcs,
 )
 
@@ -137,4 +143,112 @@ class ConfigurationCoordinateMaker(Maker):
             ],
             output=ccd_job.output,
             name=name,
+        )
+
+
+@dataclass
+class FormationEnergyMaker(Maker):
+    """Maker class to help calculate of the formation energy diagram.
+
+    Maker class to calculate formation energy diagrams. The main settings for
+    this maker is the `relax_maker` which contains the settings for the atomic
+    relaxations that each defect supercell will undergo. The `relax_maker`
+    uses a `ChargeStateRelaxSetGenerator` by default but more complex makers
+    like the `HSEDoubleRelaxMaker` can be used for more accurate (but expensive)
+    calculations.
+    If the `validate_maker` is set to True, the maker will check for some basic
+    settings in the `relax_maker` to make sure the calculations are done correctly.
+
+    Attributes
+    ----------
+    relax_maker: Maker
+        A maker to perform a atomic-position-only relaxation on the defect charge
+        states.
+    name: str
+        The name of the flow created by this maker.
+    validate_maker: bool
+        If True, the code will check the relax_maker for specific settings.
+    """
+
+    relax_maker: Maker
+    name: str = "formation energy"
+    validate_maker: bool = True
+
+    def make(
+        self,
+        defect: Defect,
+        dielectric: float | npt.NDArray | None = None,
+        bulk_supercell_dir: str | Path | None = None,
+        supercell_matrix: npt.NDArray | None = None,
+        defect_index: int | str = "",
+    ):
+        """Make a flow to calculate the formation energy diagram.
+
+        Start a series of charged supercell relaxations from a single defect
+        structure. Since the standard finite size correction (Freysoldt) requires
+        a bulk supercell calculation (to obtain the pristine electrostatic potentia),
+        this maker will either perform a bulk supercell calculation or use a existing
+        one if provided.
+        If a value for the dielectric constant is provided, the Freysoldt correction
+        will be applied to the formation energy.
+
+        Parameters
+        ----------
+        defects: Defect
+            List of defects objects to calculate the formation energy diagram for.
+        dielectric: float | NDArray | None
+            The dielectric constant or tensor used to calculate the
+            finite-size correction. If None (default), no finite-size correction will be
+            applied.
+        bulk_supercell_dir: str | Path | None
+            If provided, the bulk supercell calculation will be skipped.
+        supercell_matrix: NDArray | None
+            The supercell transformation matrix. If None, the supercell matrix
+            will be computed automatically.  If `bulk_supercell_dir` is provided,
+            this parameter will be ignored.
+        defect_index : int | str
+            Additional index to give unique names to the defect calculations.
+            Useful for external bookkeeping of symmetry distinct defects.
+
+        Returns
+        -------
+        flow: Flow
+            The workflow to calculate the formation energy diagram.
+        """
+        jobs = []
+
+        if bulk_supercell_dir is None:
+            get_sc_job = bulk_supercell_calculation(
+                uc_structure=defect.structure,
+                relax_maker=self.relax_maker,
+                sc_mat=supercell_matrix,
+            )
+            sc_mat = get_sc_job.output["sc_mat"]
+            bulk_supercell_dir = get_sc_job.output["dir_name"]
+        else:
+            get_sc_job = get_supercell_from_prv_calc(
+                defect.structure, bulk_supercell_dir, supercell_matrix
+            )
+            sc_mat = get_sc_job.output["sc_mat"]
+
+        spawn_output = spawn_defect_calcs(
+            defect=defect,
+            sc_mat=sc_mat,
+            relax_maker=self.relax_maker,
+            defect_index=defect_index,
+        )
+        jobs.extend([get_sc_job, spawn_output])
+
+        # collect_job = collect_defect_outputs(
+        #     defect=defect,
+        #     all_chg_outputs=spawn_output.output,
+        #     bulk_sc_dir=bulk_supercell_dir,
+        #     dielectric=dielectric,
+        # )
+        # jobs.append(collect_job)
+
+        return Flow(
+            jobs=jobs,
+            name=self.name,
+            # output=collect_job.output,
         )
