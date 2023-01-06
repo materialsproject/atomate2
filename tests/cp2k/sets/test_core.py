794577e66b2a26104186094f0f0e06a23cea63ae
nwinner <nwinner@berkeley.edu>
2023-01-06 12:00:55 -0800

    hybrid test

--- tests/cp2k/sets/test_core.py
+++ tests/cp2k/sets/test_core.py
@@ -4,13 +4,14 @@ import pytest
 def test_input_generators(si_structure):
     from atomate2.cp2k.sets.core import (
         StaticSetGenerator, RelaxSetGenerator, CellOptSetGenerator,
-        HybridSetGenerator, HybridStaticSetGenerator, HybridRelaxSetGenerator,
+        HybridStaticSetGenerator, HybridRelaxSetGenerator,
         HybridCellOptSetGenerator, NonSCFSetGenerator, MDSetGenerator
     )
 
     for gen in [StaticSetGenerator(), HybridStaticSetGenerator()]:
         input_set = gen.get_input_set(si_structure)
         assert input_set.cp2k_input["GLOBAL"]["RUN_TYPE"].values[0] == "ENERGY_FORCE"
+        assert input_set.cp2k_input.check("FORCE_EVAL/DFT/KPOINTS")
 
     for gen in [RelaxSetGenerator(), HybridRelaxSetGenerator()]:
         input_set = gen.get_input_set(si_structure)
@@ -22,12 +23,18 @@ def test_input_generators(si_structure):
         input_set = gen.get_input_set(si_structure)
         assert input_set.cp2k_input["GLOBAL"]["RUN_TYPE"].values[0] == "CELL_OPT"
 
-    gen = HybridSetGenerator()
+    gen = HybridStaticSetGenerator()
+    gen.user_input_settings = {
+        'activate_hybrid': {
+            'hybrid_functional': "HSE06", "eps_schwarz": 5e-3, "eps_schwarz_forces": 1e-2
+            }
+        }
     input_set = gen.get_input_set(si_structure)
-    assert input_set.cp2k_input["GLOBAL"]["RUN_TYPE"].values[0] == "ENERGY_FORCE"
     assert input_set.cp2k_input.check("FORCE_EVAL/DFT/XC/HF")
+    assert input_set.cp2k_input["FORCE_EVAL"]["DFT"]["XC"]["HF"]["INTERACTION_POTENTIAL"]['POTENTIAL_TYPE'].values[0] == "SHORTRANGE"
+    assert input_set.cp2k_input["FORCE_EVAL"]["DFT"]["XC"]["HF"]["SCREENING"]['EPS_SCHWARZ'].values[0] == pytest.approx(5e-3)
+    assert input_set.cp2k_input["FORCE_EVAL"]["DFT"]["XC"]["HF"]["SCREENING"]['EPS_SCHWARZ_FORCES'].values[0] == pytest.approx(1e-2)
     assert input_set.cp2k_input.check("FORCE_EVAL/DFT/AUXILIARY_DENSITY_MATRIX_METHOD")
-    assert input_set.cp2k_input.check("FORCE_EVAL/DFT/KPOINTS")
 
     gen = NonSCFSetGenerator()
     input_set = gen.get_input_set(si_structure)
