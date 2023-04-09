9ec2c85452622d005a4c2ae72ac5defda42ee3ce
Andrew <asrosen93@gmail.com>
2023-04-08 21:27:19 -0700

    Fix indexing issue
    Remove numpy import
    
    
    Fix cclib parser on input coords
    
    
    Lint
    
    
    Sanitize the datetime
    
    
    Re-enable strict=True jsanitization test
    
    
    Fix linting
    
    
    Retrigger test
    
    
    Undo changes to datetime
    
    
    Fix lint
    
    
    Remove cclib components

--- src/atomate2/utils/path.py
+++ src/atomate2/utils/path.py
@@ -55,32 +55,3 @@ def strip_hostname(uri_path: str | Path) -> str:
         dir_name = dir_name.split(":", 1)[1]
     return dir_name
 
-
-def find_recent_logfile(dir_name: Path | str, logfile_extensions: str | list[str]):
-    """
-    Find the most recent logfile in a given directory.
-
-    Parameters
-    ----------
-    dir_name
-        The path to the directory to search
-    logfile_extensions
-        The extension (or list of possible extensions) of the logfile to search for.
-        For an exact match only, put in the full file name.
-
-    Returns
-    -------
-    logfile
-        The path to the most recent logfile with the desired extension
-    """
-    mod_time = 0.0
-    logfile = None
-    if type(logfile_extensions) == str:
-        logfile_extensions = [logfile_extensions]
-    for f in os.listdir(dir_name):
-        f_path = os.path.join(dir_name, f)
-        for ext in logfile_extensions:
-            if ext in f and os.path.getmtime(f_path) > mod_time:
-                mod_time = os.path.getmtime(f_path)
-                logfile = os.path.abspath(f_path)
-    return logfile
