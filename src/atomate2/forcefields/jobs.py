e34c875f622bd5a36e356a21869710cacebe1060
matthewkuner <matthewkuner@gmail.com>
2023-05-03 10:44:13 -0700

    add forcefield schemas/makers to atomate2

---
+++ src/atomate2/forcefields/jobs.py
@@ -0,0 +1,77 @@
+"""Job to relax a structure using a force field (aka an interatomic potential)."""
+
+# from typing import List
+# from pydantic import BaseModel, Field
+from dataclasses import dataclass, field
+import logging
+
+from jobflow import Maker, job
+from pymatgen.core.structure import Structure
+from atomate2.forcefields.schemas import FFStructureRelaxDocument
+
+logger = logging.getLogger(__name__)
+
+
+    
+@dataclass
+class CHGNetRelaxMaker(Maker):
+    """
+    CHGNet relax maker.
+    
+    Parameters
+    ----------
+    name: str
+        The job name.
+    relax_cell : bool
+        Whether to allow the cell shape/volume to change during relaxation.
+    relax_kwargs : dict
+        Keyword arguments that will get passed to :obj:`StructOptimizer.relax`.
+    optimizer_kwargs : dict
+        Keyword arguments that will get passed to :obj:`StructOptimizer()`.
+    keep_info : list
+        Which information from the relaxation trajectory to save using the :obj:`.FFStructureRelaxDocument.from_chgnet_result`.
+    
+    """
+    
+    name: str = "CHGNet relax" ################# This is not currently setting the job name properly--unsure what I'm doing wrong
+    relax_cell: bool = False
+    relax_kwargs: dict = field(default_factory=dict)
+    optimizer_kwargs: dict = field(default_factory=dict)
+    keep_info: list = field(
+        default_factory=lambda: [
+            'energies',
+            'forces',
+            'stresses',
+            'magmoms',
+            'atom_positions',
+            'cells'
+        ] # NOTE: the 'atoms' field will always be removed later because it is not serializable (as of May 2023)
+    )
+    
+
+    @job(output_schema=FFStructureRelaxDocument)
+    def make(self, structure: Structure):
+        """
+        Perform a relaxation of a structure using CHGNet
+        
+        Parameters
+        ----------
+        structure: ~pymatgen.core.structure.Structure
+            A pymatgen structure.
+        
+        """
+        from chgnet.model import StructOptimizer
+        
+        relaxer = StructOptimizer(**self.optimizer_kwargs)
+        result = relaxer.relax(structure, relax_cell=self.relax_cell, **self.relax_kwargs)
+        
+        ff_structure_relax_doc = FFStructureRelaxDocument.from_chgnet_result(
+            structure,
+            self.relax_cell,
+            self.relax_kwargs,
+            self.optimizer_kwargs,
+            result, 
+            self.keep_info
+        )
+        
+        return ff_structure_relax_doc
---
+++ src/atomate2/forcefields/schemas.py
@@ -0,0 +1,146 @@
+"""Job to prerelax a structure using an MD Potential."""
+
+from typing import List
+from pydantic import BaseModel, Field
+from dataclasses import dataclass, field
+import logging
+
+# from jobflow import Maker, job
+from pymatgen.core.structure import Structure
+
+
+
+class InputDoc(BaseModel):
+    structure: Structure = Field(
+        None, description="The inputted structure."
+    )
+    relax_cell: bool = Field(
+        None, description="Whether the cell shape/volume was allowed to change during relaxation."
+    )
+    relax_kwargs: dict = Field(
+        None, description="Keyword arguments that were passed to :obj:`StructOptimizer.relax`."
+    )
+    optimizer_kwargs: dict = Field(
+        None, description="Keyword arguments that were passed to :obj:`StructOptimizer`."
+    )
+
+    
+
+class OutputDoc(BaseModel):
+    structure: Structure = Field(
+        None, description="The final, relaxed structure."
+    )
+    energy: float = Field(
+        None, description="Total energy in units of eV"
+    )
+    energy_per_atom: float = Field(
+        None, description="Energy per atom of the final relaxed structure in units of eV/atom"
+    )
+    forces: List[List[float]] = Field(
+        None, description="The force on each atom in units of eV/A^2 for the final structure."
+    )
+    stress: List[float] = Field(
+        None, description="The stress on the cell in units of kbar (in Voigt notation)."
+    )
+    # NOTE: units for stresses were converted to kbar (* -10 from standard output) to comply with MP convention
+    trajectory: dict = Field(
+        None, description="Step-by-step trajectory of the structural relaxation."
+    )
+    steps: int = Field(
+        None, description="total number of steps needed to relax the structure."
+    )
+
+    
+
+class FFStructureRelaxDocument(BaseModel):
+    """Document containing information on structure relaxation using a force field/interatomic potential."""
+
+    input: InputDoc = Field(
+        None, description="The inputted information used to run this job."
+    )
+    output: OutputDoc = Field(
+        None, description="The outputted information from this relaxation job."
+    )
+    MD_potential: str = Field(
+        None, description="name of the interatomic potential used for relaxation.",
+    )
+    MD_potential_version: str = Field(
+        None, description="version of the interatomic potential used for relaxation.",
+    )
+
+    
+    @classmethod
+    def from_chgnet_result(
+        cls,
+        input_structure: Structure,
+        relax_cell: bool,
+        relax_kwargs: dict,
+        optimizer_kwargs: dict,
+        result: dict,
+        keep_info: list
+    ):
+        """
+        Create a FFStructureRelaxDocument for a CHGNet relaxation.
+        
+        Parameters
+        ----------
+        input_structure : .Structure
+            The inputted pymatgen structure.
+        relax_cell: bool
+            Whether the cell shape/volume was allowed to change during pre-relaxation.
+        relax_kwargs : dict
+            Keyword arguments that will get passed to :obj:`StructOptimizer.relax`.
+        optimizer_kwargs : dict
+            Keyword arguments that will get passed to :obj:`StructOptimizer()`.
+        result : dict
+            The outputted results from relaxation.
+        keep_info : list
+            List of which pieces of information from the trajectory is saved in this FFStructureRelaxDocument.
+        """
+
+        input_doc = InputDoc(
+            structure=input_structure,
+            relax_cell=relax_cell,
+            relax_kwargs=relax_kwargs,
+            optimizer_kwargs=optimizer_kwargs
+        )
+        
+        output_structure = result['final_structure']
+        trajectory = result['trajectory'].__dict__
+        # NOTE: units for stresses were converted to kbar (* -10 from standard output) to comply with MP convention
+        for i in range(0,len(trajectory['stresses'])):
+            trajectory['stresses'][i] = trajectory['stresses'][i] * -10
+        final_energy = trajectory['energies'][-1]
+        final_energy_per_atom = trajectory['energies'][-1] / output_structure.num_sites
+        final_forces = trajectory['forces'][-1].tolist()
+        final_stress = trajectory['stresses'][-1].tolist()
+
+        trajectory_to_save = {key: trajectory[key] for key in keep_info}
+        if 'atoms' in keep_info:
+            warning_msg = """
+                WARNING: `Atoms` objects can't be serialized (as of May 2023) and thus are automatically removed.
+                """
+            logging.warning(warning_msg)
+            trajectory_to_save.pop('atoms') # can't serialize `AseAtoms` objects, so remove them from trajectory.
+        
+        steps = len(result['trajectory'])
+        
+        output_doc = OutputDoc(
+            structure=output_structure,
+            energy=final_energy,
+            energy_per_atom=final_energy_per_atom,
+            forces=final_forces,
+            stress=final_stress,
+            trajectory=trajectory_to_save,
+            steps=steps
+        )
+        
+        import chgnet
+        version = chgnet.__version__
+        
+        return cls(
+            input=input_doc,
+            output=output_doc,
+            MD_potential='CHGNet',
+            MD_potential_version=version,
+        )
