3733747a39d839de9a59a62bea88ac341601ca24
@jmmshn <jmmshn@gmail.com>
2023-05-19 21:50:44 -0700

    options

--- src/atomate2/common/flows/defect.py
+++ src/atomate2/common/flows/defect.py
@@ -164,6 +164,7 @@ class FormationEnergyMaker(Maker, ABC):
         messy, it is recommended for each implementation of this maker to check
         some of the most important settings in the `relax_maker`.  Please see
         `FormationEnergyMaker.validate_maker` for more details.
+
     bulk_relax_maker: Maker
         If None, the same `defect_relax_maker` will be used for the bulk supercell.
         A maker to used to perform the bulk supercell calculation. For marginally
@@ -181,13 +182,58 @@ class FormationEnergyMaker(Maker, ABC):
             params = ["NGX", "NGY", "NGZ", "NGXF", "NGYF", "NGZF"]
             ng_settings = dict(zip(params, ng + ngf))
             relax_maker = update_user_incar_settings(relax_maker, ng_settings)
+
     name: str
         The name of the flow created by this maker.
+
+    relax_radius:
+        The radius to include around the defect site for the relaxation.
+        If "auto", the radius will be set to the maximum that will fit inside
+        a periodic cell. If None, all atoms will be relaxed.
+
+    perturb:
+        The amount to perturb the sites in the supercell. Only perturb the
+        sites with selective dynamics set to True. So this setting only works
+        with `relax_radius`.
+
+    collect_defect_entry_data: bool
+        Whether to collect the defect entry data at the end of the flow.
+        If True, the output of all the charge states for each symmetry distinct
+        defect will be collected into a list of dictionaries that can be used
+        to create a DefectEntry. The data here can be trivially combined with
+        phase diagram data from the materials project API to create the formation
+        energy diagrams.
+
+        .. code-block:: python
+        [
+            {
+                'bulk_dir_name': 'computer1:/folder1',
+                'bulk_locpot': {...},
+                'bulk_uuid': '48fb6da7-dc2b-4dcb-b1c8-1203c0f72ce3',
+                'defect_dir_name': 'computer1:/folder2',
+                'defect_entry': {...},
+                'defect_locpot': {...},
+                'defect_uuid': 'e9af2725-d63c-49b8-a01f-391540211750'
+            },
+            {
+                'bulk_dir_name': 'computer1:/folder3',
+                'bulk_locpot': {...},
+                'bulk_uuid': '48fb6da7-dc2b-4dcb-b1c8-1203c0f72ce3',
+                'defect_dir_name': 'computer1:/folder4',
+                'defect_entry': {...},
+                'defect_locpot': {...},
+                'defect_uuid': 'a1c31095-0494-4eed-9862-95311f80a993'
+            }
+        ]
+
     """
 
     defect_relax_maker: Maker
     bulk_relax_maker: Maker | None = None
     name: str = "formation energy"
+    relax_radius: float | str | None = None
+    perturb: float | None = None
+    collect_defect_entry_data: bool = False
 
     def __post_init__(self):
         """Apply post init updates."""
@@ -201,9 +247,6 @@ class FormationEnergyMaker(Maker, ABC):
         bulk_supercell_dir: str | Path | None = None,
         supercell_matrix: npt.NDArray | None = None,
         defect_index: int | str = "",
-        collect_defect_entry_data: bool = False,
-        relax_radius: float | str | None = None,
-        perturb: float | None = None,
     ):
         """Make a flow to calculate the formation energy diagram.
 
@@ -224,16 +267,6 @@ class FormationEnergyMaker(Maker, ABC):
         defect_index : int | str
             Additional index to give unique names to the defect calculations.
             Useful for external bookkeeping of symmetry distinct defects.
-        collect_defect_entry_data: bool
-            Whether to collect the defect entry data at the end of the flow.
-        relax_radius:
-            The radius to include around the defect site for the relaxation.
-            If "auto", the radius will be set to the maximum that will fit inside
-            a periodic cell. If None, all atoms will be relaxed.
-        perturb:
-            The amount to perturb the sites in the supercell. Only perturb the
-            sites with selective dynamics set to True. So this setting only works
-            with `relax_radius`.
 
         Returns
         -------
@@ -273,12 +306,12 @@ class FormationEnergyMaker(Maker, ABC):
                 "bulk_supercell_matrix": sc_mat,
                 "bulk_supercell_uuid": get_sc_job.uuid,
             },
-            relax_radius=relax_radius,
-            perturb=perturb,
+            relax_radius=self.relax_radius,
+            perturb=self.perturb,
         )
         jobs.extend([get_sc_job, spawn_output])
 
-        if collect_defect_entry_data:
+        if self.collect_defect_entry_data:
             if bulk_supercell_dir is not None:
                 raise NotImplementedError(
                     "DefectEntery creation only works when you are explicitly "
