e898110e5143f78d6a00b2dd5e835014b06faf0b
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-06-08 06:59:30 +0200

    add module tests/vasp/test_base.py covering _get_magmoms()

---
+++ tests/vasp/test_base.py
@@ -0,0 +1,28 @@
+import pytest
+from pymatgen.core import Lattice, Structure
+
+from atomate2.vasp.sets.base import _get_magmoms
+
+
+def test_get_magmoms():
+    magmoms = {"Co": 0.8, "Fe": 2.2}  # dummy magmoms
+
+    # structure with Co that will be assigned magmoms
+    structure_with_magmoms = Structure(
+        lattice=Lattice.cubic(3),
+        species=["Co", "Fe"],
+        coords=[[0, 0, 0], [0.5, 0.5, 0.5]],
+    )
+    # structure that does not have magmoms but has 'Co'
+    structure_without_magmoms = structure_with_magmoms.copy()
+    for site in structure_with_magmoms:
+        site.properties["magmom"] = magmoms[site.species_string]
+
+    msg = "Co without an oxidation state is initialized as low spin by default"
+    # check there are no warnings
+    for struct in [structure_with_magmoms, structure_without_magmoms]:
+        with pytest.warns(UserWarning) as warns:
+            mag_values = _get_magmoms(magmoms, struct)
+        assert mag_values == list(magmoms.values())
+        assert len(warns) == 1
+        assert str(warns[0].message).startswith(msg)
