c4d50b5ca2e0b92b7d859c251966a9b891d1cbb2
Matt McDermott <mcdermott@berkeley.edu>
2023-06-15 14:00:56 -0700

    clean up magnetic ordering documentation/typing

--- src/atomate2/common/flows/magnetism.py
+++ src/atomate2/common/flows/magnetism.py
@@ -2,22 +2,22 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from typing import Dict, Literal, Optional, Sequence
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Dict, Literal, Optional, Sequence
 
 from jobflow import Flow, Job, Maker, Response
-from pymatgen.core import Element
-from pymatgen.core.structure import Structure
 
-from atomate2.vasp.flows.core import DoubleRelaxMaker
-from atomate2.vasp.jobs.base import BaseVaspMaker
-from atomate2.vasp.jobs.core import StaticMaker, TightRelaxMaker
+from atomate2.common.jobs.magnetism import (
+    analyze_orderings,
+    generate_magnetic_orderings,
+    run_ordering_calculations,
+)
+
+if TYPE_CHECKING:
+    from pymatgen.core import Element
+    from pymatgen.core.structure import Structure
 
 __all__ = ["MagneticOrderingsMaker"]
-
-from atomate2.common.jobs.magnetism import (analyze_orderings,
-                                            generate_magnetic_orderings,
-                                            run_ordering_calculations)
 
 
 @dataclass
@@ -25,106 +25,112 @@ class MagneticOrderingsMaker(Maker):
     """
     Maker to calculate possible collinear magnetic orderings for a material.
 
-    Given an input material, possible orderings will be enumerated and ranked based
-    on symmetry up to a certain maximum number of orderings. Each ordering will be
+    Given an input structure, possible magnetic orderings will be enumerated and ranked based
+    on symmetry up to a maximum number of orderings. Each ordering will be
     relaxed and a higher quality static calculation performed to obtain a total energy.
-    The lowest energy ordering is the predicted stable ordering.
+    The lowest energy ordering is the predicted ground-state collinear ordering.
 
-    This approach was found to have decent performance for a wide range of test
-    materials and was benchmarked in DOI: 10.1038/s41524-019-0199-7
+    This approach performed decently using VASP for a wide range of test materials in a
+    benchmark. It was originally implemented in atomate (v1) for VASP as the MagneticOrderingsWF.
+    Please refer to the following paper for more information and cite appropriately:
 
-    This Maker is based on the MagneticOrderingsWF in a previous version of atomate:
-
-    https://github.com/hackingmaterials/atomate/blob/main/atomate/vasp/workflows/base/magnetism.py
+        Horton, M.K., Montoya, J.H., Liu, M. et al. High-throughput prediction of the
+        ground-state collinear magnetic order of inorganic materials using Density
+        Functional Theory. npj Comput Mater 5, 64 (2019).
+        https://doi.org/10.1038/s41524-019-0199-7
 
     .. Note::
         Good performance of this workflow is ultimately dependent on an appropriate
-        choice of Hubbard U, Hund J values and/or functional. The defaults will work
+        choice of Hubbard U, Hund J values and/or the functional. The defaults will work
         well for many transition metal oxides.
 
     Parameters
     ----------
     name : str
-        Name of the flows produced by this maker.
-    default_magmoms : Optional[Dict]
-        mapping of magnetic elements to their initial magnetic moments in µB,
-        generally these are chosen to be high-spin since they can relax to a
-        low-spin configuration during a DFT electronic configuration
+        Name of the flows produced by this Maker.
+    static_maker : Maker
+        Maker used to peform static calculations for total energy (e.g., atomate2.vasp.jobs.StaticMaker).
+    relax_maker : Maker | None
+        Maker used to perform relaxations of the enumerated structures (e.g.,
+        atomate2.vasp.jobs.RelaxMaker). If None, relaxations will be skipped (i.e., only static calculations).
+    default_magmoms : dict | None
+        Optional default mapping of magnetic elements to their initial magnetic moments
+        in µB. Generally these are chosen to be high-spin, since they can relax to a
+        low-spin configuration during a DFT electronic configuration. If None, will use
+        the default values provided in pymatgen/analysis/magnetism/default_magmoms.yaml.
     strategies : tuple
-        different ordering strategies to use, choose from ferromagnetic, antiferromagnetic,
-        antiferromagnetic_by_motif, ferrimagnetic_by_motif and
-        ferrimagnetic_by_species (here, "motif", means to use a different ordering
-        parameter for symmetry inequivalent sites)
+        Different ordering strategies to use. Choose from ferromagnetic, antiferromagnetic,
+        antiferromagnetic_by_motif, ferrimagnetic_by_motif,
+        ferrimagnetic_by_species, or nonmagnetic. Here, "motif", means to use a different ordering
+        parameter for symmetry inequivalent sites.
     automatic : bool
-        if True, will automatically choose sensible strategies
+        If True, will automatically choose sensible strategies.
     truncate_by_symmetry : bool
-        if True, will remove very unsymmetrical orderings that are likely physically implausible
-    enumerator_kwargs : Optional[Dict]
-        Keyword arguments for MagneticStructureEnumerator in pymatgen
-    relax_maker : .BaseVaspMaker or None
-        A maker to perform the relaxation on the bulk. Set to ``None`` to skip the
-        relaxation.
-    static_maker : .BaseVaspMaker
-        Maker used to peform static calculations for total energy.
+        If True, will remove very unsymmetrical orderings that are likely physically implausible.
+    transformation_kwargs : dict | None
+        Keyword arguments provided to MagOrderingTransformation in pymatgen.
     """
 
-    name: str = "magnetic_ordering"
-    default_magmoms: Optional[Dict[Element, float]] = None
+    name: str = "magnetic_orderings"
+    static_maker: Maker
+    relax_maker: Maker | None = None
+    default_magmoms: dict[Element, float] | None = None
     strategies: Sequence[
-        Literal["ferromagnetic", "antiferromagnetic", "antiferromagnetic_by_motif", ""]
+        Literal[
+            "ferromagnetic",
+            "antiferromagnetic",
+            "antiferromagnetic_by_motif",
+            "ferrimagnetic_by_motif",
+            "ferrimagnetic_by_species",
+            "nonmagnetic",
+        ]
     ] = ("ferromagnetic", "antiferromagnetic")
-    automatic: bool = False
+    automatic: bool = True
     truncate_by_symmetry: bool = True
-    enumerator_kwargs: Optional[Dict] = None
-    relax_maker: Optional[BaseVaspMaker] = field(
-        default_factory=lambda: DoubleRelaxMaker(relax_maker=TightRelaxMaker())
-    )
-    static_maker: BaseVaspMaker = field(default_factory=lambda: StaticMaker)
+    transformation_kwargs: dict | None = None
 
     def make(
         self,
         structure: Structure,
-        # TODO: likely don't need prev_vasp_dir?
-        # prev_vasp_dir: str | Path | None = None,
     ):
         """
-        Make flow to calculate a possible ground-state magnetic orderings.
+        Make a flow to calculate possible ground-state collinear magnetic orderings for
+        a given input structure.
 
         Parameters
         ----------
-        structure : .Structure
-            A pymatgen structure.
+        structure : Structure
+            A pymatgen structure object.
+
+        Returns
+        -------
+        flow: Flow
+            The magnetic ordering worfklow.
         """
         jobs = []
 
-        orderings: Job = generate_magnetic_orderings(
+        orderings = generate_magnetic_orderings(
             structure,
             default_magmoms=self.default_magmoms,
             strategies=self.strategies,
-            # TODO: remove this argument and subsume into `strategies`?
             automatic=self.automatic,
             truncate_by_symmetry=self.truncate_by_symmetry,
-            enumerator_kwargs=self.enumerator_kwargs
+            transformation_kwargs=self.transformation_kwargs,
         )
         jobs.append(orderings)
 
-        # TODO: allow some orderings to fail
         if self.relax_maker is not None:
-            # optionally relax the structure
-            relaxation_calcs: Response = run_ordering_calculations(
-                orderings.output,
-                maker=self.relax_maker
+            relaxation_calcs = run_ordering_calculations(
+                orderings.output, maker=self.relax_maker
             )
             jobs.append(relaxation_calcs)
 
-        static_calcs: Response = run_ordering_calculations(
+        static_calcs = run_ordering_calculations(
             relaxation_calcs.output if self.relax_maker else orderings.output,
-            maker=self.static_maker
+            maker=self.static_maker,
         )
 
-        analyze_orderings: Response = analyze_orderings(
-            static_calcs.output
-        )
+        analyze_job = analyze_orderings(static_calcs.output)
 
-        flow = Flow(jobs=jobs, output=analyze_orderings.output, name=self.name)
+        flow = Flow(jobs=jobs, output=analyze_job.output, name=self.name)
         return flow
