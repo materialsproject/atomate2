f727ffff9220e50bc624e79edd695333a336862f
mjwen <wenxx151@gmail.com>
2023-07-01 13:18:01 -0500

    Add some docstrings

--- tests/vasp/jobs/test_elastic.py
+++ tests/vasp/jobs/test_elastic.py
@@ -11,22 +11,20 @@ from atomate2.vasp.jobs.elastic import generate_elastic_deformations
 
 @pytest.mark.parametrize("conventional", [False, True])
 def test_reduce_expand_deformation(clean_dir, si_structure, conventional):
-    """Test reduced and expanded strains are the same
+    """Test reduced and expanded strains are the same all space groups."""
 
-    all space groups."""
-
+    # can use any structure to get all the strains
     full_strains = _get_strains(si_structure, conventional, sym_reduce=False)
 
-    structure = Structure(
-        lattice=[[0, 2.73, 2.73], [2.73, 0, 2.73], [2.73, 2.73, 0]],
-        species=["Si", "Si"],
-        coords=[[0, 0, 0], [0.25, 0.25, 0.25]],
-    )
+    structures = _get_prototype_structures()
 
-    _test_one_structure(structure, full_strains, conventional)
+    for structure in structures:
+        _test_one_structure(structure, full_strains, conventional)
 
 
 def _test_one_structure(structure: Structure, full_strains, conventional):
+    """Test reduced and expanded strains are the same for one structure."""
+
     reduced_strains = _get_strains(structure, conventional, sym_reduce=True)
 
     dummy_stresses = [Stress(np.zeros((3, 3)))] * len(reduced_strains)
@@ -49,6 +47,8 @@ def _test_one_structure(structure: Structure, full_strains, conventional):
 
 
 def _get_strains(structure, conventional, sym_reduce):
+    """Return a list of strains associated with the deformed structures."""
+
     job = generate_elastic_deformations(
         structure,
         order=2,
@@ -60,3 +60,15 @@ def _get_strains(structure, conventional, sym_reduce):
     response = run_locally(job, ensure_success=True)
     deformations = response[job.uuid][1].output
     return [d.green_lagrange_strain for d in deformations]
+
+
+def _get_prototype_structures():
+    """Return a list of prototype structures, one from each of the 230 space groups."""
+
+    structure = Structure(
+        lattice=[[0, 2.73, 2.73], [2.73, 0, 2.73], [2.73, 2.73, 0]],
+        species=["Si", "Si"],
+        coords=[[0, 0, 0], [0.25, 0.25, 0.25]],
+    )
+
+    return [structure]
