cec458de90f7ce5211c75109d397d6b52fb93ac3
Matt McDermott <mcdermott@berkeley.edu>
2023-07-11 11:55:13 -0700

    fix bug in task creation in magnetism builder

--- src/atomate2/vasp/builders/magnetism.py
+++ src/atomate2/vasp/builders/magnetism.py
@@ -84,15 +84,6 @@ class MagneticOrderingBuilder(Builder):
                 "metadata.ordering": {"$exists": True},
             }
         )
-        # props = [
-        #     "uuid",
-        #     "output.formula_pretty",
-        #     "output.structure",
-        #     "output.output.energy",
-        #     "output.dir_name",
-        #     "metadata",
-        # ]
-
         self.logger.info("Grouping by formula...")
         nformulas = len(self.tasks.distinct("output.formula_pretty", criteria=criteria))
         results = self.tasks.groupby("output.formula_pretty", criteria=criteria)
@@ -144,12 +135,12 @@ class MagneticOrderingBuilder(Builder):
 
         outputs = []
         for task in static_tasks:
-            relax_task, relax_output = None, None
-            for task in relax_tasks:
-                if task["uuid"] == task["metadata"]["parent_uuid"]:
-                    relax_task = task
+            relax_output = None
+            for r_task in relax_tasks:
+                if r_task["uuid"] == task["metadata"]["parent_uuid"]:
                     relax_output = MagneticOrderingRelaxation.from_task_document(
-                        relax_task
+                        r_task["output"],
+                        uuid=r_task["uuid"],
                     )
                     break
             output = MagneticOrderingOutput.from_task_document(
@@ -181,7 +172,6 @@ class MagneticOrderingBuilder(Builder):
         items : list of .MagneticOrderingDocument
             A list of magnetic ordering documents to add to the database.
         """
-        print(items)
         self.logger.info(f"Updating {len(items)} magnetic orderings documents")
         self.magnetic_orderings.update(items, key="ground_state_uuid")
 
@@ -231,136 +221,3 @@ def _group_orderings(tasks: list[dict], tol: float) -> list[list[dict]]:
             grouped_tasks.append([task])
 
     return grouped_tasks
-
-    # formula = parent_structure.formula
-    # formula_pretty = parent_structure.composition.reduced_formula
-
-    # energies = [calc.energy_per_atom for calc in outputs]
-    # ground_state_energy = min(energies)
-
-    # # TODO: make this check with a tolerance
-    # possible_ground_state_idxs = [i for i in energies if i == ground_state_energy]
-    # if len(possible_ground_state_idxs) > 1:
-    #     logger.warning(
-    #         f"Multiple identical energies exist, duplicate calculations for {formula}?"
-    #     )
-    # idx = possible_ground_state_idxs[0]
-    # ground_state_energy = energies[idx]
-    # ground_state_uuid = outputs[idx].uuid
-
-    # docs = []
-    # for output in outputs:
-    #     doc = {}
-    #     doc["formula"] = formula
-    #     doc["formula_pretty"] = formula_pretty
-    #     doc["parent_structure"] = output["structure"]
-    #     doc["ground_state_energy"] = ground_state_energy
-    #     doc["ground_state_uuid"] = ground_state_uuid
-    #     doc["parent_structure"] = parent_structure
-
-    #     input_analyzer = CollinearMagneticStructureAnalyzer(
-    #         input_structure, threshold=0.61
-    #     )
-
-    #     final_analyzer = CollinearMagneticStructureAnalyzer(
-    #         final_structure, threshold=0.61
-    #     )
-
-    #     if d["task_id"] == ground_state_task_id:
-    #         stable = True
-    #         decomposes_to = None
-    #     else:
-    #         stable = False
-    #         decomposes_to = ground_state_task_id
-    #     energy_above_ground_state_per_atom = (
-    #         d["output"]["energy_per_atom"] - ground_state_energy
-    #     )
-
-    #     # tells us the order in which structure was guessed
-    #     # 1 is FM, then AFM..., -1 means it was entered manually
-    #     # useful to give us statistics about how many orderings
-    #     # we actually need to calculate
-    #     task_label = d["task_label"].split(" ")
-    #     ordering_index = task_label.index("ordering")
-    #     ordering_index = int(task_label[ordering_index + 1])
-    #     if self.get("origins", None):
-    #         ordering_origin = self["origins"][ordering_index]
-    #     else:
-    #         ordering_origin = None
-
-    #     final_magmoms = final_structure.site_properties["magmom"]
-    #     magmoms = {"vasp": final_magmoms}
-
-    #     if self["perform_bader"]:
-    #         # if bader has already been run during task ingestion,
-    #         # use existing analysis
-    #         if "bader" in d:
-    #             magmoms["bader"] = d["bader"]["magmom"]
-    #         # else try to run it
-    #         else:
-    #             try:
-    #                 dir_name = d["dir_name"]
-    #                 # strip hostname if present, implicitly assumes
-    #                 # ToDB task has access to appropriate dir
-    #                 if ":" in dir_name:
-    #                     dir_name = dir_name.split(":")[1]
-    #                 magmoms["bader"] = bader_analysis_from_path(dir_name)["magmom"]
-    #                 # prefer bader magmoms if we have them
-    #                 final_magmoms = magmoms["bader"]
-    #             except Exception as e:
-    #                 magmoms["bader"] = f"Bader analysis failed: {e}"
-
-    #     input_order_check = [0 if abs(m) < 0.61 else m for m in input_magmoms]
-    #     final_order_check = [0 if abs(m) < 0.61 else m for m in final_magmoms]
-    #     ordering_changed = not np.array_equal(
-    #         np.sign(input_order_check), np.sign(final_order_check)
-    #     )
-
-    #     symmetry_changed = (
-    #         final_structure.get_space_group_info()[0]
-    #         != input_structure.get_space_group_info()[0]
-    #     )
-
-    #     total_magnetization = abs(
-    #         d["calcs_reversed"][0]["output"]["outcar"]["total_magnetization"]
-    #     )
-    #     num_formula_units = sum(
-    #         d["calcs_reversed"][0]["composition_unit_cell"].values()
-    #     ) / sum(d["calcs_reversed"][0]["composition_reduced"].values())
-    #     total_magnetization_per_formula_unit = total_magnetization / num_formula_units
-    #     total_magnetization_per_unit_volume = (
-    #         total_magnetization / final_structure.volume
-    #     )
-
-    #     summary = {
-    #         "formula": formula,
-    #         "formula_pretty": formula_pretty,
-    #         "parent_structure": self["parent_structure"].as_dict(),
-    #         "wf_meta": d["wf_meta"],  # book-keeping
-    #         "task_id": d["task_id"],
-    #         "structure": final_structure.as_dict(),
-    #         "magmoms": magmoms,
-    #         "input": {
-    #             "structure": input_structure.as_dict(),
-    #             "ordering": input_analyzer.ordering.value,
-    #             "symmetry": input_structure.get_space_group_info()[0],
-    #             "index": ordering_index,
-    #             "origin": ordering_origin,
-    #             "input_index": self.get("input_index", None),
-    #         },
-    #         "total_magnetization": total_magnetization,
-    #         "total_magnetization_per_formula_unit": total_magnetization_per_formula_unit,
-    #         "total_magnetization_per_unit_volume": total_magnetization_per_unit_volume,
-    #         "ordering": final_analyzer.ordering.value,
-    #         "ordering_changed": ordering_changed,
-    #         "symmetry": final_structure.get_space_group_info()[0],
-    #         "symmetry_changed": symmetry_changed,
-    #         "energy_per_atom": d["output"]["energy_per_atom"],
-    #         "stable": stable,
-    #         "decomposes_to": decomposes_to,
-    #         "energy_above_ground_state_per_atom": energy_above_ground_state_per_atom,
-    #         "energy_diff_relax_static": energy_diff_relax_static,
-    #         "created_at": datetime.utcnow(),
-    #     }
-    #     docs.append(MagnetismDocument(**doc))
-    # return docs
