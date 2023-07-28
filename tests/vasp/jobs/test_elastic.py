0eb1e24e144429adad068d2cc86a9c04d89892a0
mjwen <wenxx151@gmail.com>
2023-07-28 14:40:57 -0500

    Add test for testing reduce and expanding strains

--- tests/vasp/jobs/test_elastic.py
+++ tests/vasp/jobs/test_elastic.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 from jobflow import run_locally
 from pymatgen.analysis.elasticity import Stress
-from pymatgen.core import Structure
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 from atomate2 import SETTINGS
 from atomate2.common.schemas.elastic import _expand_strains
@@ -10,22 +10,17 @@ from atomate2.vasp.jobs.elastic import generate_elastic_deformations
 
 
 @pytest.mark.parametrize("conventional", [False, True])
-def test_reduce_expand_deformation(clean_dir, si_structure, conventional):
-    """Test reduced and expanded strains are the same all space groups."""
+def test_reduce_expand_strains(clean_dir, symmetry_structure, conventional):
+    """Test the reduced and expanded strains are the same."""
 
-    # can use any structure to get all the strains
-    full_strains = _get_strains(si_structure, conventional, sym_reduce=False)
+    if conventional:
+        spg = SpacegroupAnalyzer(symmetry_structure, symprec=SETTINGS.SYMPREC)
+        structure = spg.get_conventional_standard_structure()
+    else:
+        structure = symmetry_structure
 
-    structures = _get_prototype_structures()
-
-    for structure in structures:
-        _test_one_structure(structure, full_strains, conventional)
-
-
-def _test_one_structure(structure: Structure, full_strains, conventional):
-    """Test reduced and expanded strains are the same for one structure."""
-
-    reduced_strains = _get_strains(structure, conventional, sym_reduce=True)
+    full_strains = _get_strains(structure, sym_reduce=False)
+    reduced_strains = _get_strains(structure, sym_reduce=True)
 
     dummy_stresses = [Stress(np.zeros((3, 3)))] * len(reduced_strains)
     dummy = ["dummy"] * len(reduced_strains)
@@ -41,34 +36,21 @@ def _test_one_structure(structure: Structure, full_strains, conventional):
 
     assert len(full_strains) == len(recovered_strains)
 
-    # TODO, this can be slow
     for fs in full_strains:
         assert any(np.allclose(fs, rs) for rs in recovered_strains)
 
 
-def _get_strains(structure, conventional, sym_reduce):
-    """Return a list of strains associated with the deformed structures."""
+def _get_strains(structure, sym_reduce):
+    """Get applied strains to deform the deformed structures."""
 
     job = generate_elastic_deformations(
         structure,
         order=2,
-        conventional=conventional,
         symprec=SETTINGS.SYMPREC,
         sym_reduce=sym_reduce,
     )
 
     response = run_locally(job, ensure_success=True)
     deformations = response[job.uuid][1].output
+
     return [d.green_lagrange_strain for d in deformations]
-
-
-def _get_prototype_structures():
-    """Return a list of prototype structures, one from each of the 230 space groups."""
-
-    structure = Structure(
-        lattice=[[0, 2.73, 2.73], [2.73, 0, 2.73], [2.73, 2.73, 0]],
-        species=["Si", "Si"],
-        coords=[[0, 0, 0], [0.25, 0.25, 0.25]],
-    )
-
-    return [structure]
