2dce7a7bc987b08378ea45eb828ce684573975b9
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-09-26 16:41:26 -0700

    add MatPES(Meta)GGAStaticSetGenerator

--- src/atomate2/vasp/sets/mp.py
+++ src/atomate2/vasp/sets/mp.py
@@ -190,3 +190,52 @@ class MPMetaGGARelaxSetGenerator(RelaxSetGenerator):
         """
         # unset GGA, shouldn't be set anyway but doesn't hurt to be sure
         return {"LCHARG": True, "LWAVE": True, "GGA": None}
+
+
+@dataclass
+class MatPESGGAStaticSetGenerator(StaticSetGenerator):
+    """Class to generate MP-compatible VASP GGA static input sets."""
+
+    config_dict: dict = field(default_factory=lambda: _BASE_MATPES_PBE_STATIC_SET)
+    auto_ismear: bool = False
+    auto_kspacing: bool = True
+
+
+@dataclass
+class MatPESMetaGGAStaticSetGenerator(StaticSetGenerator):
+    """Class to generate MP-compatible VASP GGA static input sets."""
+
+    config_dict: dict = field(default_factory=lambda: _BASE_MATPES_PBE_STATIC_SET)
+    auto_ismear: bool = False
+    auto_kspacing: bool = True
+
+    def get_incar_updates(
+        self,
+        structure: Structure,
+        prev_incar: dict = None,
+        bandgap: float = None,
+        vasprun: Vasprun = None,
+        outcar: Outcar = None,
+    ) -> dict:
+        """
+        Get updates to the INCAR for this calculation type.
+
+        Parameters
+        ----------
+        structure
+            A structure.
+        prev_incar
+            An incar from a previous calculation.
+        bandgap
+            The band gap.
+        vasprun
+            A vasprun from a previous calculation.
+        outcar
+            An outcar from a previous calculation.
+
+        Returns
+        -------
+        dict
+            A dictionary of updates to apply.
+        """
+        return {"METAGGA": "R2SCAN", "ALGO": "ALL", "GGA": "--"}  # unset GGA
