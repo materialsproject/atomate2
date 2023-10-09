c97ab2cb576a65ad470568ada76361cb80ea5f6f
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-09 09:42:31 -0700

    fix typo: popp to pop
    for population

--- tests/vasp/lobster/schemas/test_lobster.py
+++ tests/vasp/lobster/schemas/test_lobster.py
@@ -97,15 +97,15 @@ def test_LobsterTaskDocument(lobster_test_dir):
     assert np.isclose(doc.site_potentials["Ewald_splitting"], 3.14)
     assert len(doc.gross_populations) == 8
     assert doc.gross_populations[5]["element"] == "As"
-    expected_gross_popp = {
+    expected_gross_pop = {
         "4s": 1.38,
         "4p_y": 1.18,
         "4p_z": 1.18,
         "4p_x": 1.18,
         "total": 4.93,
     }
-    gross_popp_here = doc.gross_populations[5]["Loewdin GP"]
-    assert expected_gross_popp == gross_popp_here
+    gross_pop_here = doc.gross_populations[5]["Loewdin GP"]
+    assert expected_gross_pop == gross_pop_here
     assert np.allclose(
         doc.charges["Mulliken"],
         [0.13, 0.13, 0.13, 0.13, -0.13, -0.13, -0.13, -0.13],
@@ -128,14 +128,7 @@ def test_LobsterTaskDocument(lobster_test_dir):
     assert np.allclose(
         doc2.site_potentials["Loewdin"],
         [
-            -15.09,
-            -15.09,
-            -15.09,
-            -15.09,
-            -15.09,
-            -15.09,
-            -15.09,
-            -15.09,
+            *[-15.09] * 8,
             14.78,
             14.78,
             8.14,
@@ -156,12 +149,12 @@ def test_LobsterTaskDocument(lobster_test_dir):
     assert np.isclose(doc2.site_potentials["Ewald_splitting"], 3.14)
     assert len(doc2.gross_populations) == 22
     assert doc2.gross_populations[10]["element"] == "F"
-    expected_gross_popp = {
+    expected_gross_pop = {
         "2s": 1.98,
         "2p_y": 1.97,
         "2p_z": 1.97,
         "2p_x": 1.97,
         "total": 7.88,
     }
-    gross_popp_here = doc2.gross_populations[10]["Mulliken GP"]
-    assert expected_gross_popp == gross_popp_here
+    gross_pop_here = doc2.gross_populations[10]["Mulliken GP"]
+    assert expected_gross_pop == gross_pop_here
