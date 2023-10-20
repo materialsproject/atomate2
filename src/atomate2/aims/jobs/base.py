fc424f399f41f7dbe3b0add35b69ec8187e26ec5
Andrey Sobolev <sobolev@fhi.mpg.de>
2023-10-20 13:20:27 +0200

    Factored update file job out of convergence maker

--- src/atomate2/aims/jobs/base.py
+++ src/atomate2/aims/jobs/base.py
@@ -247,9 +247,13 @@ class ConvergenceMaker(Maker):
             )
             next_base_job.append_name(append_str=f" {idx}")
 
-            update_file_job = self.update_convergence_file(
+            update_file_job = update_convergence_file(
                 prev_dir=prev_dir_no_host,
                 job_dir=next_base_job.output.dir_name,
+                criterion_name=self.criterion_name,
+                epsilon=self.epsilon,
+                convergence_field=self.convergence_field,
+                convergence_steps=self.convergence_steps,
                 output=next_base_job.output,
             )
 
@@ -264,55 +268,70 @@ class ConvergenceMaker(Maker):
         task_doc = AimsTaskDoc.from_directory(prev_dir_no_host)
         return ConvergenceSummary.from_aims_calc_doc(task_doc)
 
-    @job(name="Writing a convergence file")
-    def update_convergence_file(
-        self, prev_dir: str | Path, job_dir: str | Path, output
-    ):
-        """Write a convergence file.
 
-        Parameters
-        ----------
-        prev_dir: str or Path
-            The previous calculation directory
-        job_dir: str or Path
-            The current calculation directory
-        output: .ConvergenceSummary
-            The current output of the convergence flow
-        """
+@job(name="Writing a convergence file")
+def update_convergence_file(
+    prev_dir: str | Path,
+    job_dir: str | Path,
+    criterion_name: str,
+    epsilon: float,
+    convergence_field: str,
+    convergence_steps: list,
+    output,
+):
+    """Write a convergence file.
+
+    Parameters
+    ----------
+    prev_dir: str or Path
+        The previous calculation directory
+    job_dir: str or Path
+        The current calculation directory
+    criterion_name: str
+        A name for the convergence criterion. Must be in the run results
+    epsilon: float
+        A difference in criterion value for subsequent runs
+    convergence_field: str
+        An input parameter that changes to achieve convergence
+    convergence_steps: Iterable
+        An iterable of the possible values for the convergence field.
+        If the iterable is depleted and the convergence is not reached,
+        that the job is failed
+    output: .ConvergenceSummary
+        The current output of the convergence flow
+    """
+    idx = 0
+    if prev_dir is not None:
+        prev_dir_no_host = str(prev_dir).split(":")[-1]
+        convergence_file = Path(prev_dir_no_host) / CONVERGENCE_FILE_NAME
+        if convergence_file.exists():
+            with open(convergence_file) as f:
+                convergence_data = json.load(f)
+                idx = convergence_data["idx"] + 1
+    else:
         idx = 0
-        if prev_dir is not None:
-            prev_dir_no_host = str(prev_dir).split(":")[-1]
-            convergence_file = Path(prev_dir_no_host) / CONVERGENCE_FILE_NAME
-            if convergence_file.exists():
-                with open(convergence_file) as f:
-                    convergence_data = json.load(f)
-                    idx = convergence_data["idx"] + 1
-        else:
-            idx = 0
-            convergence_data = {
-                "criterion_name": self.criterion_name,
-                "criterion_values": [],
-                "convergence_field_name": self.convergence_field,
-                "convergence_field_values": [],
-                "converged": False,
-            }
-        convergence_data["convergence_field_values"].append(self.convergence_steps[idx])
-        convergence_data["criterion_values"].append(
-            getattr(output.output, self.criterion_name)
-        )
-        convergence_data["idx"] = idx
+        convergence_data = {
+            "criterion_name": criterion_name,
+            "criterion_values": [],
+            "convergence_field_name": convergence_field,
+            "convergence_field_values": [],
+            "converged": False,
+        }
+    convergence_data["convergence_field_values"].append(convergence_steps[idx])
+    convergence_data["criterion_values"].append(getattr(output.output, criterion_name))
+    convergence_data["idx"] = idx
 
-        if len(convergence_data["criterion_values"]) > 1:
-            # checking for convergence
-            convergence_data["converged"] = (
-                abs(
-                    convergence_data["criterion_values"][-1]
-                    - convergence_data["criterion_values"][-2]
-                )
-                < self.epsilon
+    if len(convergence_data["criterion_values"]) > 1:
+        # checking for convergence
+        convergence_data["converged"] = (
+            abs(
+                convergence_data["criterion_values"][-1]
+                - convergence_data["criterion_values"][-2]
             )
+            < epsilon
+        )
 
-        split_job_dir = str(job_dir).split(":")[-1]
-        convergence_file = Path(split_job_dir) / CONVERGENCE_FILE_NAME
-        with open(convergence_file, "w") as f:
-            json.dump(convergence_data, f)
+    split_job_dir = str(job_dir).split(":")[-1]
+    convergence_file = Path(split_job_dir) / CONVERGENCE_FILE_NAME
+    with open(convergence_file, "w") as f:
+        json.dump(convergence_data, f)
