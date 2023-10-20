edb4e825b27f6f1ef5a013bcad45d088c66039ff
Thomas Purcell <purcell@fhi-berlin.mpg.de>
2023-10-20 09:59:55 +0200

    Correct issues that came from resolving merge conflicts
    1) The vasp.flows.phonons and forcefields.flows.phonons make funtion not needed
    2) Correct typing/annotations linting issues

--- src/atomate2/vasp/flows/phonons.py
+++ src/atomate2/vasp/flows/phonons.py
@@ -1,13 +1,17 @@
 """Define the VASP PhononMaker."""
+from __future__ import annotations
+
 from dataclasses import dataclass, field
-from typing import Union
+from typing import TYPE_CHECKING
 
 from atomate2.common.flows.phonons import BasePhononMaker
 from atomate2.vasp.flows.core import DoubleRelaxMaker
-from atomate2.vasp.jobs.base import BaseVaspMaker
 from atomate2.vasp.jobs.core import DielectricMaker, StaticMaker, TightRelaxMaker
 from atomate2.vasp.jobs.phonons import PhononDisplacementMaker
 from atomate2.vasp.sets.core import StaticSetGenerator
+
+if TYPE_CHECKING:
+    from atomate2.vasp.jobs.base import BaseVaspMaker
 
 
 @dataclass
@@ -111,89 +115,28 @@ class PhononMaker(BasePhononMaker):
     sym_reduce: bool = True
     symprec: float = 1e-4
     displacement: float = 0.01
-    min_length: Union[float, None] = 20.0
+    min_length: float | None = 20.0
     prefer_90_degrees: bool = True
     get_supercell_size_kwargs: dict = field(default_factory=dict)
-    use_symmetrized_structure: Union[str, None] = None
+    use_symmetrized_structure: str | None = None
     create_thermal_displacements: bool = True
     generate_frequencies_eigenvectors_kwargs: dict = field(default_factory=dict)
     kpath_scheme: str = "seekpath"
     store_force_constants: bool = True
     socket: bool = False
     code: str = "vasp"
-    bulk_relax_maker: Union[BaseVaspMaker, None] = field(
+    bulk_relax_maker: BaseVaspMaker | None = field(
         default_factory=lambda: DoubleRelaxMaker.from_relax_maker(TightRelaxMaker())
     )
-    static_energy_maker: Union[BaseVaspMaker, None] = field(
+    static_energy_maker: BaseVaspMaker | None = field(
         default_factory=lambda: StaticMaker(
             input_set_generator=StaticSetGenerator(auto_ispin=True)
         )
     )
-    born_maker: Union[BaseVaspMaker, None] = field(default_factory=DielectricMaker)
+    born_maker: BaseVaspMaker | None = field(default_factory=DielectricMaker)
     phonon_displacement_maker: BaseVaspMaker = field(
         default_factory=PhononDisplacementMaker
     )
-# TARP: Merge conflict resolution I want to see what exactly changed in the make for the new common workflow
-#     def make(
-#         self,
-#         structure: Structure,
-#         prev_vasp_dir: str | Path | None = None,
-#         born: list[Matrix3D] | None = None,
-#         epsilon_static: Matrix3D | None = None,
-#         total_dft_energy_per_formula_unit: float | None = None,
-#         supercell_matrix: Matrix3D | None = None,
-#     ) -> Flow:
-#         """
-#         Make flow to calculate the phonon properties.
-
-#         Parameters
-#         ----------
-#         structure : .Structure
-#             A pymatgen structure. Please start with a structure that is nearly fully
-#             optimized as the internal optimizers have very strict settings!
-#         prev_vasp_dir : str or Path or None
-#             A previous vasp calculation directory to use for copying outputs.
-#         born: Matrix3D
-#             Instead of recomputing born charges and epsilon, these values can also be
-#             provided manually. If born and epsilon_static are provided, the born run
-#             will be skipped it can be provided in the VASP convention with information
-#             for every atom in unit cell. Please be careful when converting structures
-#             within in this workflow as this could lead to errors
-#         epsilon_static: Matrix3D
-#             The high-frequency dielectric constant to use instead of recomputing born
-#             charges and epsilon. If born, epsilon_static are provided, the born run
-#             will be skipped
-#         total_dft_energy_per_formula_unit: float
-#             It has to be given per formula unit (as a result in corresponding Doc).
-#             Instead of recomputing the energy of the bulk structure every time, this
-#             value can also be provided in eV. If it is provided, the static run will be
-#             skipped. This energy is the typical output dft energy of the dft workflow.
-#             No conversion needed.
-#         supercell_matrix: list
-#             Instead of min_length, also a supercell_matrix can be given, e.g.
-#             [[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]
-#         """
-#         if self.use_symmetrized_structure not in [None, "primitive", "conventional"]:
-#             raise ValueError(
-#                 "use_symmetrized_structure can only be primitive, conventional, None"
-#             )
-
-#         if (
-#             not self.use_symmetrized_structure == "primitive"
-#             and self.kpath_scheme != "seekpath"
-#         ):
-#             raise ValueError(
-#                 "You can only use other kpath schemes with the primitive standard "
-#                 "structure"
-#             )
-
-#         if self.kpath_scheme not in [
-#             "seekpath",
-#             "hinuma",
-#             "setyawan_curtarolo",
-#             "latimer_munro",
-#         ]:
-#             raise ValueError("kpath scheme is not implemented")
 
     @property
     def prev_calc_dir_argname(self):
