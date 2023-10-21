78bff852602ada929266d6e819dc5f9c980910b7
JaGeo <janine.george@bam.de>
2023-10-21 07:22:54 +0200

    Fix imports

--- tests/forcefields/flows/test_elastic.py
+++ tests/forcefields/flows/test_elastic.py
@@ -1,16 +1,12 @@
-import numpy as np
-
-from atomate2.common.schemas.elastic import ElasticDocument
-from atomate2.forcefields.flows.elastic import ElasticMaker
-from atomate2.forcefields.jobs import M3GNetRelaxMaker
-
-
 def test_elastic_wf(clean_dir, si_structure):
     from jobflow import run_locally
     from numpy.testing import assert_allclose
     from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
-    # TODO test with Alumnium
+    from atomate2.common.schemas.elastic import ElasticDocument
+    from atomate2.forcefields.flows.elastic import ElasticMaker
+    from atomate2.forcefields.jobs import M3GNetRelaxMaker
+
     si_prim = SpacegroupAnalyzer(si_structure).get_primitive_standard_structure()
 
     # !!! Generate job
