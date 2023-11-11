433c5b8d01672f5e70386dffeec1ed5ab10e3a74
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-11-11 09:08:35 -0800

    add optional 3rd static for PBE+U in MatPesGGAPlusMetaGGAStaticMaker if structure contains +U elements

--- src/atomate2/vasp/flows/matpes.py
+++ src/atomate2/vasp/flows/matpes.py
@@ -35,6 +35,9 @@ class MatPesGGAPlusMetaGGAStaticMaker(Maker):
         Maker to generate the first VASP static.
     static2 : .BaseVaspMaker
         Maker to generate the second VASP static.
+    static3 : .BaseVaspMaker or None
+        Maker to generate the optional third VASP static. Defaults to GGA static with
+        +U corrections if structure contains elements with +U corrections, else to None.
     """
 
     name: str = "MatPES GGA plus meta-GGA static"
@@ -53,10 +56,24 @@ class MatPesGGAPlusMetaGGAStaticMaker(Maker):
             copy_vasp_kwargs={"additional_vasp_files": ("WAVECAR",)}
         )
     )
+    # optional 3rd PBE+U static in case structure contains elements with +U corrections
+    static3: Maker | None = field(
+        default_factory=lambda: MatPesGGAStaticMaker(
+            input_set_generator=MatPesGGAStaticSetGenerator(
+                user_incar_settings={"LDAU:": True},  # enable +U corrections
+            ),
+            copy_vasp_kwargs={"additional_vasp_files": ("WAVECAR",)},
+        )
+    )
 
     def make(self, structure: Structure, prev_dir: str | Path | None = None):
         """
-        Create a flow with two chained statics.
+        Create a flow with MatPES statics.
+
+        By default, a PBE static is followed by an r2SCAN static and optionally a PBE+U
+        static if the structure contains elements with +U corrections. The PBE static is
+        run with LWAVE=True so its WAVECAR can be passed as a pre-conditioned starting
+        point to both the r2SCAN static and the PBE+U static.
 
         Parameters
         ----------
@@ -68,9 +85,21 @@ class MatPesGGAPlusMetaGGAStaticMaker(Maker):
         Returns
         -------
         Flow
-            A flow containing two statics.
+            A flow containing 2 or 3 statics.
         """
         static1 = self.static1.make(structure, prev_dir=prev_dir)
         static2 = self.static2.make(structure, prev_dir=static1.output.dir_name)
         output = {"static1": static1.output, "static2": static2.output}
-        return Flow([static1, static2], output=output, name=self.name)
+        jobs = [static1, static2]
+
+        # add third static if structure contains elements with +U corrections
+        if self.static3 and {*structure.elements} & {
+            *self.static3.input_set_generator.config_dict.get("INCAR", {}).get(
+                "LDAUU", []
+            )
+        }:
+            static3 = self.static3.make(structure, prev_dir=static1.output.dir_name)
+            output["static3"] = static3.output
+            jobs += [static3]
+
+        return Flow(jobs=jobs, output=output, name=self.name)
