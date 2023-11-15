2caf10c24a64852babe9ceef5f7948b0a9bf4117
Rhys Goodall <rhys.goodall@outlook.com>
2023-11-15 08:41:18 -0800

    fea: add MACE RelaxMaker and StaticMaker (#611)
    * fea: add MACE RelaxMaker and StaticMaker
    
    * fea: add mace to reqs
    
    * doc: add placeholders for the task_doc_kwargs and optimizer_kwargs
    
    * fix: pin test to use float64 as model trained as double
    
    * fix: set dtypes for tests
    
    * remove torch.set_default_dtype(torch.float32)
    
    * remove potential_device from MACERelaxMaker and MACEStaticMaker, absorbed into potential_kwargs
    
    fix tests
    fix doc string
    
    * fix outdated PULL_REQUEST_TEMPLATE.md: no longer using black
    
    * add pytest fixture to revert torch default dtype
    in MACE tests
    
    ---------
    
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

--- tests/forcefields/test_jobs.py
+++ tests/forcefields/test_jobs.py
@@ -1,3 +1,4 @@
+import pytest
 from jobflow import run_locally
 from pytest import approx, importorskip
 
@@ -8,10 +9,19 @@ from atomate2.forcefields.jobs import (
     GAPStaticMaker,
     M3GNetRelaxMaker,
     M3GNetStaticMaker,
+    MACERelaxMaker,
+    MACEStaticMaker,
 )
 from atomate2.forcefields.schemas import ForceFieldTaskDocument
 
-importorskip("quippy")
+
+@pytest.fixture()
+def revert_torch_default_dtype():
+    import torch
+
+    default_dtype = torch.get_default_dtype()
+    yield
+    torch.set_default_dtype(default_dtype)
 
 
 def test_chgnet_static_maker(si_structure):
@@ -82,16 +92,59 @@ def test_m3gnet_relax_maker(si_structure):
     assert output1.output.n_steps == 14
 
 
+def test_mace_static_maker(si_structure, test_dir, revert_torch_default_dtype):
+    task_doc_kwargs = {"ionic_step_data": ("structure", "energy")}
+
+    # generate job
+    # NOTE the test model is not trained on Si, so the energy is not accurate
+    job = MACEStaticMaker(
+        potential_param_file_name=test_dir / "forcefields" / "mace" / "MACE.model",
+        potential_kwargs={"default_dtype": "float64", "device": "cpu"},
+        task_document_kwargs=task_doc_kwargs,
+    ).make(si_structure)
+
+    # run the flow or job and ensure that it finished running successfully
+    responses = run_locally(job, ensure_success=True)
+
+    # validation the outputs of the job
+    output1 = responses[job.uuid][1].output
+    assert isinstance(output1, ForceFieldTaskDocument)
+    assert output1.output.energy == approx(-0.068231, rel=1e-4)
+    assert output1.output.n_steps == 1
+
+
+def test_mace_relax_maker(si_structure, test_dir, revert_torch_default_dtype):
+    # translate one atom to ensure a small number of relaxation steps are taken
+    si_structure.translate_sites(0, [0, 0, 0.1])
+
+    # generate job
+    # NOTE the test model is not trained on Si, so the energy is not accurate
+    job = MACERelaxMaker(
+        potential_param_file_name=test_dir / "forcefields" / "mace" / "MACE.model",
+        potential_kwargs={"default_dtype": "float64", "device": "cpu"},
+        steps=25,
+    ).make(si_structure)
+
+    # run the flow or job and ensure that it finished running successfully
+    responses = run_locally(job, ensure_success=True)
+
+    # validating the outputs of the job
+    output1 = responses[job.uuid][1].output
+    assert isinstance(output1, ForceFieldTaskDocument)
+    assert output1.output.energy == approx(-0.051099, rel=1e-4)
+    assert output1.output.n_steps == 10
+
+
 def test_gap_static_maker(si_structure, test_dir):
+    importorskip("quippy")
+
     task_doc_kwargs = {"ionic_step_data": ("structure", "energy")}
 
     # generate job
     # Test files have been provided by Yuanbin Liu (University of Oxford)
     job = GAPStaticMaker(
         potential_args_str="IP GAP",
-        potential_param_file_name=str(
-            test_dir / "forcefields" / "gap" / "gap_file.xml"
-        ),
+        potential_param_file_name=test_dir / "forcefields" / "gap" / "gap_file.xml",
         task_document_kwargs=task_doc_kwargs,
     ).make(si_structure)
 
@@ -106,6 +159,8 @@ def test_gap_static_maker(si_structure, test_dir):
 
 
 def test_gap_relax_maker(si_structure, test_dir):
+    importorskip("quippy")
+
     # translate one atom to ensure a small number of relaxation steps are taken
     si_structure.translate_sites(0, [0, 0, 0.1])
 
