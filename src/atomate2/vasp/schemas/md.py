3e6f8271f1f4b5644337038eab469acc1e1e24a6
Guido Petretto <g.petretto@gmail.com>
2023-11-22 14:24:53 +0100

    fix typing

--- src/atomate2/vasp/schemas/md.py
+++ src/atomate2/vasp/schemas/md.py
@@ -1,7 +1,5 @@
 """Schemas for MD documents."""
 
-from typing import List
-
 from emmet.core.structure import StructureMetadata
 from pydantic import Field
 from pymatgen.core import Structure
@@ -12,7 +10,7 @@ class MultiMDOutput(StructureMetadata):
 
     structure: Structure = Field("Final structure of the last step of the flow")
     vasp_dir: str = Field("Path to the last vasp folder of the flow")
-    traj_ids: List[str] = Field("List of uuids of the MD calculations in the flow")
-    full_traj_ids: List[str] = Field(
+    traj_ids: list[str] = Field("List of uuids of the MD calculations in the flow")
+    full_traj_ids: list[str] = Field(
         "List of uuids of the MD calculations in the flow and in previous linked flows"
     )
