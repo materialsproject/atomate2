253ee7659bc85fb748a5f657b8cda429b29cd88a
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-12-03 11:15:10 -0800

    mv tests/test_data/vasp/(matpes_pbe_r2scan_flow->matpes_static_flow)

--- tests/vasp/flows/test_matpes.py
+++ tests/vasp/flows/test_matpes.py
@@ -10,10 +10,10 @@ from atomate2.vasp.flows.matpes import MatPesStaticFlowMaker
 
 def test_matpes_static_flow_maker(mock_vasp, clean_dir, vasp_test_dir):
     # map from job name to directory containing reference output files
-    pre_relax_dir = "matpes_pbe_r2scan_flow/pbe_static"
+    pre_relax_dir = "matpes_static_flow/pbe_static"
     ref_paths = {
         "MatPES GGA static": pre_relax_dir,
-        "MatPES meta-GGA static": "matpes_pbe_r2scan_flow/r2scan_static",
+        "MatPES meta-GGA static": "matpes_static_flow/r2scan_static",
     }
     si_struct = Structure.from_file(f"{vasp_test_dir}/{pre_relax_dir}/inputs/POSCAR")
 
