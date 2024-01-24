6c8782b92dfb139082ad714e3f79bdaa2d0d4d60
Alex Ganose <utf@users.noreply.github.com>
2024-01-24 10:45:01 +0000

    Revert "[WIP] First pass at porting QChem into atomate2, waiting on concurrent PR approvals"

--- src/atomate2/qchem/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Output schemas for QChem calculations."""
-# The whole schema framework should not be
-# necessary anymore. Will delete after consultation
--- tests/qchem/test_drones.py
+++
@@ -1,7 +0,0 @@
-from atomate2.qchem.drones import QChemDrone
-
-
-def test_structure_optimization(qchem_test_dir):
-    drone = QChemDrone()
-    doc = drone.assimilate(qchem_test_dir / "water_single_point" / "outputs")
-    assert doc
