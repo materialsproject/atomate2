563628970c428d54537ce94854b3dd6ae91a4e2a
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-01-25 10:29:11 +0100

    remove dead code in qchem/sets/core.py

--- src/atomate2/qchem/sets/core.py
+++ src/atomate2/qchem/sets/core.py
@@ -12,40 +12,32 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class SinglePointSetGenerator(QCInputGenerator):
-    """Class to generate QChem Single Point input sets."""
+    """Generate QChem Single Point input sets."""
 
-    # def get_input_set_updates(self) -> dict:
-    #     """Get updates to the input dict for a single point calculation."""
     def __init__(self) -> None:
         super().__init__(job_type="sp", scf_algorithm="diis", basis_set="def2-tzvppd")
 
 
 @dataclass
 class OptSetGenerator(QCInputGenerator):
-    """Class to generate QChem Optimization input sets."""
+    """Generate QChem Optimization input sets."""
 
-    # def get_input_set_updates(self) -> dict:
-    #     """Get updates to the input dict for a geometry optimization calculation."""
     def __init__(self) -> None:
         super().__init__(job_type="opt", scf_algorithm="diis", basis_set="def2-tzvppd")
 
 
 @dataclass
 class TransitionStateSetGenerator(QCInputGenerator):
-    """Class to generate QChem Transition State calculation input sets."""
+    """Generate QChem Transition State calculation input sets."""
 
-    # def get_basis_set_updates(self) -> dict:
-    #     """Get updates to the input dict for a transition state calculation."""
     def __init__(self) -> None:
         super().__init__(job_type="ts", scf_algorithm="diis", basis_set="def2-tzvppd")
 
 
 @dataclass
 class ForceSetGenerator(QCInputGenerator):
-    """Class to generate QChem force input sets."""
+    """Generate QChem force input sets."""
 
-    # def get_basis_set_updates(self) -> dict:
-    #     """Get updates to the input dict for a force(gradient) calculation."""
     def __init__(self) -> None:
         super().__init__(
             job_type="force", scf_algorithm="diis", basis_set="def2-tzvppd"
@@ -54,20 +46,16 @@ class ForceSetGenerator(QCInputGenerator):
 
 @dataclass
 class FreqSetGenerator(QCInputGenerator):
-    """Class to generate QChem frequency calculation input sets."""
+    """Generate QChem frequency calculation input sets."""
 
-    # def get_basis_set_updates(self) -> dict:
-    #     """Get updates to the input dict for a frequency calculation."""
     def __init__(self) -> None:
         super().__init__(job_type="freq", scf_algorithm="diis", basis_set="def2-tzvppd")
 
 
 @dataclass
 class PESScanSetGenerator(QCInputGenerator):
-    """Class to generate QChem PES scan input sets."""
+    """Generate QChem PES scan input sets."""
 
-    # def get_basis_set_updates(self) -> dict:
-    #     """Get updates to the input dict for a pes scan calculation."""
     def __init__(self) -> None:
         super().__init__(
             job_type="pes_scan", scf_algorithm="diis", basis_set="def2-tzvppd"
