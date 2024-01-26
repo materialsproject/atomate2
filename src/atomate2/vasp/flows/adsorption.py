69956c423309d366480c386671206b0d5f1377dc
itsshawnzhang <wonforward@outlook.com>
2024-01-25 17:26:31 -0800

    [WIP] adding surface adsorption calculation flow

--- src/atomate2/vasp/flows/adsorption.py
+++ src/atomate2/vasp/flows/adsorption.py
@@ -1,24 +1,106 @@
 """Flow for calculating surface adsorption energies."""
 from __future__ import annotations
-
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
 from jobflow import Flow, Maker
 
-from atomate2.vasp.jobs.adsorption import ()
-
-from atomate2.common.jobs.utils import structure_to_conventional, structure_to_primitive
+from atomate2.vasp.jobs.adsorption import (
+    get_molecule_structure,
+    adslabRelaxMaker,
+    StaticMaker,
+    moleculeRelaxMaker,
+    generate_slab_job,
+    generate_adslab_jobs,
+    run_adsorption_calculations
+)
 from atomate2.vasp.flows.core import DoubleRelaxMaker
-from atomate2.vasp.jobs.core import DielectricMaker, StaticMaker, TightRelaxMaker
-from atomate2.vasp.sets.core import StaticSetGenerator
+from atomate2.vasp.jobs.core import TightRelaxMaker
 
 if TYPE_CHECKING:
     from pathlib import Path
-
-    from pymatgen.core.structure import Structure
+    from pymatgen.core.structure import Structure, Molecule
     from atomate2.vasp.jobs.base import BaseVaspMaker
 
 @dataclass
 class AdsorptionMaker(Maker):
     """Makes a flow for calculating adsorption energies."""
+    name: str = "adsorption"
+    min_ads_distance: float = 2.0
+    min_vacuum: float = 20.0
+    min_slab_size: float = 10.0
+    min_lw: float = 10.0
+    prefer_90_degrees: bool = True
+    get_supercell_size_kwargs: dict = field(default_factory=dict)
+    use_symmetrized_structure: str | None = None
+
+    bulk_relax_maker: BaseVaspMaker = field(
+        default_factory=lambda: DoubleRelaxMaker.from_relax_maker(TightRelaxMaker())
+    )
+    mol_relax_maker: BaseVaspMaker = field(default_factory=moleculeRelaxMaker())
+
+    adslab_relax_maker: BaseVaspMaker = field(default_factory=adslabRelaxMaker())
+
+    static_energy_maker: BaseVaspMaker = field(default_factory=adslabStaticMaker())
+
+    def make(
+            self,
+            molecule: Molecule,
+            structure: Structure,
+            supercell_vector: [],
+            surface_vector: [],
+            prev_dir: str | Path | None = None,
+            molecule_dft_energy: float | None = None,
+    ) -> Flow:
+
+        if self.use_symmetrized_structure not in [None, "primitive", "conventional"]:
+            raise ValueError(
+                "use_symmetrized_structure can only be primitive, conventional, None"
+            )
+
+        jobs = []
+
+        molecule_structure = get_molecule_structure(molecule)
+
+        if self.molecule_dft_energy is None:
+            molOptimize = self.mol_relax_maker.make(molecule, prev_dir=prev_dir)
+            jobs.append(molOptimize)
+            optimized_molecule = molOptimize.output.structure
+
+            mol_static_job = StaticMaker.make(molecule_structure)
+            jobs.append(mol_static_job)
+            molecule_dft_energy = mol_static_job.output.energy
+
+
+        bulkOptimize = self.bulk_relax_maker.make(structure, prev_dir=prev_dir)
+        jobs.append(bulkOptimize)
+        optimized_bulk = bulkOptimize.output.structure
+
+
+
+        slab_job = generate_slab_job(
+            optimized_bulk,
+            self.supercell_idx,
+            self.surface_idx,
+            self.prefer_90_degrees,
+            self.min_vacuum
+        )
+        jobs.append(slab_job)
+
+        slab_static_job = StaticMaker.make(slab_job.output.structure)
+        jobs.append(slab_static_job)
+        slab_energy = slab_static_job.output.energy
+
+        adslab_jobs = generate_adslab_jobs(
+            optimized_bulk,
+            optimized_molecule,
+            self.supercell_idx,
+            self.surface_idx,
+            self.prefer_90_degrees,
+            self.min_vacuum,
+            self.min_ads_length
+        )
+        jobs.append(adslab_jobs)
+
+        adsorption_calc = run_adsorption_calculations()
+
--- src/atomate2/vasp/jobs/adsorption.py
+++ src/atomate2/vasp/jobs/adsorption.py
@@ -1,4 +1,4 @@
-"""Jobs for running adsorption calculations."""
+"""Jobs used in the calculation of surface adsorption energy."""
 
 from __future__ import annotations
 
@@ -8,10 +8,126 @@ from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
 from jobflow import Flow, Response, job
+from pymatgen.core import Structure, Molecule, Element
 
 from pymatgen.analysis.adsorption import AdsorbateSiteFinder
-from pymatgen.core.surface import Slab, generate_all_slabs
-from pymatgen.io.vasp.sets import MVLSlabSet
-from pymatgen.transformations.advanced_transformations import SlabTransformation
-from pymatgen.transformations.standard_transformations import SupercellTransformation
+from pymatgen.core.surface import SlabGenerator
+from pymatgen.transformations.standard_transformations import RotationTransformation
 
+from atomate2.vasp.jobs.base import BaseVaspMaker
+from atomate2.vasp.sets.core import StaticSetGenerator
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    import numpy as np
+    from emmet.core.math import Matrix3D
+
+    from atomate2.forcefields.jobs import ForceFieldStaticMaker
+    from atomate2.vasp.sets.base import VaspInputGenerator
+
+logger = logging.getLogger(__name__)
+
+
+@job
+def get_molecule_structure(molecule: Molecule) -> Structure:
+    """Get the molecule structure."""
+    return molecule.get_boxed_structure(10, 10, 10, offset=np.array([5, 5, 5]))
+
+@job
+def generate_slab_job(
+        structure: Structure,
+        supercell_index,
+        surface_idx,
+        prefer_90_degrees,
+        min_vacuum
+        ):
+
+
+@job
+def generate_adslab_jobs(
+    bulk_structure,
+    molecule_structure,
+    min_slab_size,
+    surface_idx,
+    min_vacuum_size,
+    min_lw,
+    ):
+
+    slab_generator = SlabGenerator(bulk_structure, surface_idx, min_slab_size, min_vacuum_size, primitive=False, center_slab=True)
+    slab = slab_generator.get_slab()
+    ads_slabs = AdsorbateSiteFinder(slab).generate_adsorption_structures(molecule_structure, translate=True, min_lw)
+
+@job
+def run_adsorption_calculations():
+    """Generate adsorption structures."""
+    pass
+
+@dataclass
+class moleculeRelaxMaker(BaseVaspMaker):
+    name: str = "adsorption relaxation"
+    input_set_generator: VaspInputGenerator = field(
+        default_factory=lambda: StaticSetGenerator(
+            user_kpoints_settings={"grid_density": 7_000},
+            user_incar_settings={
+                "ALGO": "Normal",
+                "IBRION": 2,
+                "ISIF": 2,
+                "ENCUT": 700,
+                "GGA": "RP",
+                "EDIFF": 1e-5,
+                "LAECHG": False,
+                "LREAL": False,
+                "LCHARG": False,
+                "LDAU": False,
+                "NSW": 300,
+                "NELM": 500,
+            },
+            auto_ispin=True,
+        )
+    )
+
+@dataclass
+class adslabRelaxMaker(BaseVaspMaker):
+    name: str = "adsorption relaxation"
+    input_set_generator: VaspInputGenerator = field(
+        default_factory=lambda: StaticSetGenerator(
+            user_kpoints_settings={"grid_density": 7_000},
+            user_incar_settings={
+                "ALGO": "Normal",
+                "IBRION": 2,
+                "ISIF": 2,
+                "ENCUT": 700,
+                "GGA": "RP",
+                "EDIFF": 1e-5,
+                "LAECHG": False,
+                "LREAL": False,
+                "LCHARG": False,
+                "LDAU": False,
+                "NSW": 300,
+                "NELM": 500,
+            },
+            auto_ispin=True,
+        )
+    )
+
+@dataclass
+class StaticMaker(BaseVaspMaker):
+    name: str = "adsorption static calculation
+    input_set_generator: VaspInputGenerator = field(
+        default_factory=lambda: StaticSetGenerator(
+            user_kpoints_settings={"grid_density": 7_000},
+            user_incar_settings={
+                "ALGO": "Normal",
+                "ENCUT": 700,
+                "GGA": "RP",
+                "EDIFF": 1e-7,
+                "LAECHG": False,
+                "LREAL": False,
+                "LCHARG": False,
+                "LDAU": False,
+                "NELM": 500,
+            },
+            auto_ispin=True,
+        )
+    )
