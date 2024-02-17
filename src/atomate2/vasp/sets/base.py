88ce66e348ba529f9a9a8174caa3f8943318758e
Andrew S. Rosen <asrosen93@gmail.com>
2024-02-17 14:09:36 -0800

    Update "likely metal" `BadInputSetWarning`

--- src/atomate2/vasp/sets/base.py
+++ src/atomate2/vasp/sets/base.py
@@ -170,11 +170,11 @@ class VaspInputSet(InputSet):
         if (
             all(k.is_metal for k in self.poscar.structure.composition)
             and self.incar.get("NSW", 0) > 0
-            and self.incar.get("ISMEAR", 1) < 1
+            and (incar.get("ISMEAR", 1) < 0 or (incar.get("ISMEAR", 1) == 0 and incar.get("SIGMA", 0.2) > 0.05))
         ):
             warnings.warn(
-                "Relaxation of likely metal with ISMEAR < 1 detected. Please see VASP "
-                "recommendations on ISMEAR for metals.",
+                "Relaxation of likely metal with ISMEAR < 0 or ISMEAR = 1 with a small SIGMA detected.
+                "Please see VASP recommendations on ISMEAR for metals.",
                 BadInputSetWarning,
                 stacklevel=1,
             )
