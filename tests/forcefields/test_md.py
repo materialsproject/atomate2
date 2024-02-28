71728a34a7d3172bbc61fe4a8f95aea488fd5a47
Yuan Chiang <qaqowotut@gmail.com>
2024-02-28 15:17:09 -0800

    add stresses to trajeoctroy observer, add pressure schedule test

--- tests/forcefields/test_md.py
+++ tests/forcefields/test_md.py
@@ -101,3 +101,32 @@ def test_temp_schedule(ff_name, si_structure, clean_dir):
     ]
 
     assert temp_history[-1] > temp_schedule[0]
+
+
+@pytest.mark.parametrize("ff_name", ["MACE", "CHGNet"])
+def test_press_schedule(ff_name, si_structure, clean_dir):
+    nsteps = 200
+    press_schedule = [0, 1e4]
+
+    structure = si_structure.to_conventional() * (2, 2, 2)
+
+    # MACE changes the default dtype, ensure consistent dtype here
+    torch.set_default_dtype(torch.float32)
+
+    job = _to_maker[ff_name](
+        nsteps=nsteps, traj_file="md_traj.json.gz",
+        dynamics="nose-hoover",
+        pressure=press_schedule,
+        ase_md_kwargs=dict(
+            ttime=50.0 * units.fs,
+            pfactor=(75.0 * units.fs)**2 * units.GPa,
+            )
+    ).make(structure)
+    response = run_locally(job, ensure_success=True)
+    taskdoc = response[next(iter(response))][1].output
+
+    stress_history = [
+        step["stresses"] for step in taskdoc.forcefield_objects["trajectory"].frame_properties
+    ]
+
+    assert stress_history[-1].trace() > stress_history[0].trace()
