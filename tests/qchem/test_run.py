0c0191ca474ab759a409f48e3ffd4774fe4ba43f
rdguha <rdguha@lbl.gov>
2024-02-28 15:00:10 -0800

    fixed linting for test_run

--- tests/qchem/test_run.py
+++ tests/qchem/test_run.py
@@ -1,12 +1,12 @@
-from atomate2.qchem.run import _DEFAULT_HANDLERS, JobType, run_qchem, should_stop_children
 from atomate2.qchem.drones import QChemDrone
+from atomate2.qchem.run import should_stop_children
+
 
 def test_stop_children(qchem_test_dir):
     drone = QChemDrone()
     task_doc = drone.assimilate(qchem_test_dir / "water_single_point" / "outputs")
-    chk_stop_children = should_stop_children(task_document=task_doc, handle_unsuccessful=False)
+    chk_stop_children = should_stop_children(
+        task_document=task_doc, handle_unsuccessful=False
+    )
 
-    assert chk_stop_children == False
-
-
-
+    assert chk_stop_children is False
