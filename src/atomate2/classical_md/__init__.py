57a06fb17b0fae5e55734b90e5a8d2f3f91f4453
orioncohen <orion@lbl.gov>
2024-03-18 13:26:32 -0700

    Add as_dict monkeypatch for openff.Molecule to init

---
+++ src/atomate2/classical_md/__init__.py
@@ -0,0 +1,11 @@
+from openff.toolkit.topology.molecule import Molecule
+
+
+def openff_mol_as_monty_dict(self):
+    mol_dict = self.to_dict()
+    mol_dict["@module"] = "openff.toolkit.topology"
+    mol_dict["@class"] = "Molecule"
+    return mol_dict
+
+
+Molecule.as_dict = openff_mol_as_monty_dict
