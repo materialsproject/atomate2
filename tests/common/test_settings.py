24b4c58d07fc8e0b66462f781857829e389d5788
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-03-30 14:56:50 +0100

    test no warning if config path is default and file does not exist

--- tests/common/test_settings.py
+++ tests/common/test_settings.py
@@ -3,13 +3,23 @@ from pathlib import Path
 import pytest
 from pydantic import ValidationError
 
-from atomate2.settings import _ENV_PREFIX, Atomate2Settings
+from atomate2.settings import _DEFAULT_CONFIG_FILE_PATH, _ENV_PREFIX, Atomate2Settings
 
 
-def test_empty_and_invalid_config_file(clean_dir, monkeypatch: pytest.MonkeyPatch):
+def test_empty_and_invalid_config_file(
+    clean_dir, monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture[str]
+):
+    # test no warning if config path is default and file does not exist
+    env_var_name = f"{_ENV_PREFIX.upper()}CONFIG_FILE"
+    monkeypatch.delenv(env_var_name, raising=False)
+    settings = Atomate2Settings()
+    assert settings.CONFIG_FILE == _DEFAULT_CONFIG_FILE_PATH
+    stdout, stderr = capsys.readouterr()
+    assert stdout == ""
+    assert stderr == ""
+
     # set path to load settings from with ATOMATE2_CONFIG_FILE env variable
     config_file_path = Path.cwd() / "test-atomate2-config.yaml"
-    env_var_name = f"{_ENV_PREFIX.upper()}CONFIG_FILE"
     monkeypatch.setenv(env_var_name, str(config_file_path))
 
     settings = Atomate2Settings()
