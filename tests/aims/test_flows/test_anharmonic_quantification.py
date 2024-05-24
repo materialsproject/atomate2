1f524ebeabe4927177af1bc82f93d9af313625fa
Tom Purcell <purcellt@arizona.edu>
2024-05-23 18:24:47 -0700

    Add the test data for mock aims
    Tests should now work outside of socket calcs

--- tests/aims/test_flows/test_anharmonic_quantification.py
+++ tests/aims/test_flows/test_anharmonic_quantification.py
@@ -1,29 +1,52 @@
 import os
 
 import numpy as np
+import pytest
 from jobflow import run_locally
-from pymatgen.io.aims.sets.core import StaticSetGenerator
+from pymatgen.io.aims.sets.core import SocketIOSetGenerator, StaticSetGenerator
 
 from atomate2.aims.flows.phonons import PhononMaker
 from atomate2.aims.jobs.core import RelaxMaker, StaticMaker
-from atomate2.aims.jobs.phonons import PhononDisplacementMaker
+from atomate2.aims.jobs.phonons import (
+    PhononDisplacementMaker,
+    PhononDisplacementMakerSocket,
+)
 from atomate2.common.flows.anharmonicity import BaseAnharmonicityMaker
 
-cwd = os.get_cwd()
+cwd = os.getcwd()
 
 
-def test_anharmonic_quantification(si, tmp_path, species_dir):
+def test_anharmonic_quantification(si, tmp_path, mock_aims, species_dir):
+    # mapping from job name to directory containing test files
+    ref_paths = {
+        "Relaxation calculation": "phonon-relax-si",
+        "phonon static aims 1/1": "phonon-disp-si",
+        "SCF Calculation": "phonon-energy-si",
+        "phonon static aims anharmonicity quant. 1/1": "anharm-os-si",
+    }
+
+    # settings passed to fake_run_aims; adjust these to check for certain input settings
+    fake_run_aims_kwargs = {}
+
+    # automatically use fake FHI-aims
+    mock_aims(ref_paths, fake_run_aims_kwargs)
+
     parameters = {
         "k_grid": [2, 2, 2],
         "species_dir": (species_dir / "light").as_posix(),
+        "rlsy_symmetry": "all",
+        "sc_accuracy_rho": 1e-06,
+        "sc_accuracy_forces": 0.0001,
+        "relativistic": "atomic_zora scalar",
     }
 
     parameters_phonon_disp = dict(compute_forces=True, **parameters)
+    parameters_phonon_disp["rlsy_symmetry"] = None
 
     phonon_maker = PhononMaker(
         bulk_relax_maker=RelaxMaker.full_relaxation(user_params=parameters),
         static_energy_maker=StaticMaker(
-            input_set_generator=StaticSetGenerator(user_params=parameters)
+            input_set_generator=StaticSetGenerator(user_params=parameters_phonon_disp)
         ),
         use_symmetrized_structure="primitive",
         phonon_displacement_maker=PhononDisplacementMaker(
@@ -46,5 +69,51 @@ def test_anharmonic_quantification(si, tmp_path, species_dir):
     os.chdir(tmp_path)
     responses = run_locally(flow, create_folders=True, ensure_success=True)
     os.chdir(cwd)
+    assert np.round(responses[flow.job_uuids[-1]][1].output, 3) == 0.104
 
-    assert np.round(responses[flow.job_uuids[-1]][1].output, 3) == 0.163
+
+@pytest.mark.skip(reason="Currently not mocked and needs FHI-aims binary")
+def test_anharmonic_quantification_socket(si, tmp_path, species_dir):
+    # mapping from job name to directory containing test files
+    parameters = {
+        "k_grid": [2, 2, 2],
+        "species_dir": (species_dir / "light").as_posix(),
+        "rlsy_symmetry": "all",
+        "sc_accuracy_rho": 1e-06,
+        "sc_accuracy_forces": 0.0001,
+        "relativistic": "atomic_zora scalar",
+    }
+
+    parameters_phonon_disp = dict(
+        compute_forces=True, use_pimd_wrapper=("localhost", 12347), **parameters
+    )
+    parameters_phonon_disp["rlsy_symmetry"] = None
+
+    phonon_maker = PhononMaker(
+        bulk_relax_maker=RelaxMaker.full_relaxation(user_params=parameters),
+        static_energy_maker=StaticMaker(
+            input_set_generator=StaticSetGenerator(user_params=parameters)
+        ),
+        use_symmetrized_structure="primitive",
+        phonon_displacement_maker=PhononDisplacementMakerSocket(
+            input_set_generator=SocketIOSetGenerator(
+                user_params=parameters_phonon_disp,
+            )
+        ),
+        socket=True,
+    )
+
+    maker = BaseAnharmonicityMaker(
+        phonon_maker=phonon_maker,
+    )
+    maker.name = "anharmonicity"
+    flow = maker.make(
+        si,
+        supercell_matrix=np.array([-1, 1, 1, 1, -1, 1, 1, 1, -1]).reshape((3, 3)),
+    )
+
+    # run the flow or job and ensure that it finished running successfully
+    os.chdir(tmp_path)
+    responses = run_locally(flow, create_folders=True, ensure_success=True)
+    os.chdir(cwd)
+    assert np.round(responses[flow.job_uuids[-1]][1].output, 3) == 0.125
