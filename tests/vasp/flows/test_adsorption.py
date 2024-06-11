9001a2de1b036872bb6de97a9a35fa895cb4ffa5
itsshawnzhang <wonforward@outlook.com>
2024-06-11 14:11:26 -0700

    [wip] updating the test file

--- tests/vasp/flows/test_adsorption.py
+++ tests/vasp/flows/test_adsorption.py
@@ -9,12 +9,17 @@ from atomate2.vasp.flows.adsorption import AdsorptionMaker
 def test_adsorption(mock_vasp, clean_dir, test_dir):
     # mapping from job name to directory containing test files
     ref_paths = {
-        "ads relax bulk": "Au_adsorption/bulk",
+        "ads relax bulk": "Au_adsorption/bulk_relax",
         "ads relax mol": "Au_adsorption/mol",
         "ads relax slab": "Au_adsorption/slab",
-        "elastic relax 1/3": "Au_adsorption/ads_relax_1_3",
-        "elastic relax 2/3": "Au_adsorption/ads_relax_2_3",
-        "elastic relax 3/3": "Au_adsorption/ads_relax_3_3",
+        "ads static mol": "Au_adsorption/mol_static",
+        "ads static slab": "Au_adsorption/slab_static",
+        "ads relax 1/3": "Au_adsorption/ads_relax_1_3",
+        "ads relax 2/3": "Au_adsorption/ads_relax_2_3",
+        "ads relax 3/3": "Au_adsorption/ads_relax_3_3",
+        "ads static 1/3": "Au_adsorption/ads_static_1_3",
+        "ads static 2/3": "Au_adsorption/ads_static_2_3",
+        "ads static 3/3": "Au_adsorption/ads_rstatic_3_3",
     }
 
     # settings passed to fake_run_vasp; adjust these to check for certain INCAR settings
@@ -22,9 +27,14 @@ def test_adsorption(mock_vasp, clean_dir, test_dir):
         "ads relax bulk": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
         "ads relax mol": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
         "ads relax slab": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
-        "elastic relax 1/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
-        "elastic relax 2/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
-        "elastic relax 3/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads static mol": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads static slab": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads relax 1/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads relax 2/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads relax 3/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads static 1/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads static 2/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
+        "ads static 3/3": {"incar_settings": ["NSW", "ISMEAR", "ISIF"]},
     }
 
     # automatically use fake VASP and write POTCAR.spec during the test
@@ -33,7 +43,9 @@ def test_adsorption(mock_vasp, clean_dir, test_dir):
     molcule = Structure.from_file(test_dir / "vasp/Au_adsorption/mol/POSCAR")
     bulk_structure = Structure.from_file(test_dir / "vasp/Au_adsorption/bulk/POSCAR")
 
-    flow = AdsorptionMaker().make(molecule=molcule, structure=bulk_structure)
+    flow = AdsorptionMaker().make(
+        molecule=molcule, structure=bulk_structure, min_lw=5.0, min_slab_size=4.0
+    )
 
     # Run the flow or job and ensure that it finished running successfully
     responses = run_locally(flow, create_folders=True, ensure_success=True)
@@ -43,13 +55,22 @@ def test_adsorption(mock_vasp, clean_dir, test_dir):
 
     # Verify job names and order
     expected_job_names = [
-        "molecule relaxation job",
-        "molecule static job",
-        "bulk relaxation job",
-        "slab relaxation job",
-        "slab static job",
-        "adslabs job",
-        "adsorption calculation",
+        "molecule relaxation maker - molecule relaxation job",
+        "molecule static maker - molecule static job",
+        "bulk relaxation maker - bulk relaxation job",
+        "generate_slab",
+        "generate_adslabs",
+        "adsorption relaxation maker - slab relaxation job",
+        "adsorption static maker - slab static job",
+        "run_adslabs_job",
+        "adsorption relaxation maker - configuration 0",
+        "adsorption relaxation maker - configuration 1",
+        "adsorption relaxation maker - configuration 2",
+        "adsorption static maker - static configuration 0",
+        "adsorption static maker - static configuration 1",
+        "adsorption static maker - static configuration 2",
+        "adsorption_calculations",
+        "store_inputs",
     ]
     for response, expected_name in zip(responses, expected_job_names):
         assert (
