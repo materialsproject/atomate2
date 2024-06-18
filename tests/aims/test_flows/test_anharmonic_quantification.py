66898e33054883f4dfaeda523727de0a2525e6cf
Kevin <4kevinbeck5@gmail.com>
2024-06-18 10:09:33 -0700

    Added mode resolved sigma^A test w/ mock_aims

--- tests/aims/test_flows/test_anharmonic_quantification.py
+++ tests/aims/test_flows/test_anharmonic_quantification.py
@@ -132,7 +132,76 @@ def test_anharmonic_quantification_full(si, tmp_path, mock_aims, species_dir):
     os.chdir(cwd)
     assert np.round(responses[flow.job_uuids[-1]][1].output.sigma_A, 3) == 0.120
 
-def test_atom_resolved_anharmonicity(nacl, tmp_path, mock_aims, species_dir):
+def test_mode_resolved_anharmonic_quantification(si, tmp_path, mock_aims, species_dir):
+    # mapping from job name to directory containing test files
+    ref_paths = {
+        "Relaxation calculation": "phonon-relax-mode-resolved",
+        "SCF Calculation": "phonon-energy-mode-resolved",
+        "phonon static aims 1/1": "phonon-disp-mode-resolved",
+        "phonon static aims anharmonicity quant. 1/3": "anharm-mode-resolved_1",
+        "phonon static aims anharmonicity quant. 2/3": "anharm-mode-resolved_2",
+        "phonon static aims anharmonicity quant. 3/3": "anharm-mode-resolved_3",
+    }
+
+    # settings passed to fake_run_aims; adjust these to check for certain input settings
+    fake_run_aims_kwargs = {}
+
+    # automatically use fake FHI-aims
+    mock_aims(ref_paths, fake_run_aims_kwargs)
+
+    parameters = {
+        "species_dir": (species_dir / "light").as_posix(),
+        "rlsy_symmetry": "all",
+        "sc_accuracy_rho": 1e-06,
+        "sc_accuracy_forces": 0.0001,
+        "relativistic": "atomic_zora scalar",
+    }
+
+    parameters_phonon_disp = dict(compute_forces=True, **parameters)
+    parameters_phonon_disp["rlsy_symmetry"] = None
+
+    phonon_maker = PhononMaker(
+        bulk_relax_maker=RelaxMaker.full_relaxation(user_params=parameters, user_kpoints_settings={"density": 5.0}),
+        static_energy_maker=StaticMaker(
+            input_set_generator=StaticSetGenerator(
+                user_params=parameters,
+                user_kpoints_settings={"density": 5.0}
+            )
+        ),
+        use_symmetrized_structure="primitive",
+        phonon_displacement_maker=PhononDisplacementMaker(
+            input_set_generator=StaticSetGenerator(
+                user_params=parameters_phonon_disp,
+                user_kpoints_settings={"density": 5.0}
+            )
+        ),
+    )
+
+    maker = AnharmonicityMaker(
+        phonon_maker=phonon_maker,
+    )
+    maker.name = "anharmonicity"
+    flow = maker.make(
+        si,
+        supercell_matrix=np.array([-2, 2, 2, 2, -2, 2, 2, 2, -2]).reshape((3, 3)),
+        one_shot_approx=False,
+        seed=1234,
+        mode_resolved=True,
+        n_samples=3,
+    )
+
+    # run the flow or job and ensure that it finished running successfully
+    os.chdir(tmp_path)
+    responses = run_locally(flow, create_folders=True, ensure_success=True)
+    os.chdir(cwd)
+    mode_resolved_vals = responses[flow.job_uuids[-1]][1].output.mode_resolved_sigma_A
+    mode_resolved_vals_rounded = np.round(mode_resolved_vals, 3)
+    sigmas = mode_resolved_vals_rounded[:, 1]
+    assert [3.7470, 5.5000*(10**(-2))] in mode_resolved_vals_rounded
+    assert np.round(sigmas.mean(), 3) == 0.186
+    assert np.round(sigmas.std(), 3) == 0.213
+
+def test_atom_resolved_anharmonic_quantification(nacl, tmp_path, mock_aims, species_dir):
     # mapping from job name to directory containing test files
     ref_paths = {
         "Relaxation calculation": "phonon-relax-nacl",
@@ -190,9 +259,10 @@ def test_atom_resolved_anharmonicity(nacl, tmp_path, mock_aims, species_dir):
     os.chdir(tmp_path)
     responses = run_locally(flow, create_folders=True, ensure_success=True)
     os.chdir(cwd)
-    nacl_sigma_dict = responses[flow.job_uuids[-1]][1].output.atom_resolved_sigma_A
-    assert np.round(nacl_sigma_dict["Na"], 3) == 0.076
-    assert np.round(nacl_sigma_dict["Cl"], 3) == 0.072
+    nacl_sigma_vals = responses[flow.job_uuids[-1]][1].output.atom_resolved_sigma_A
+    nacl_sigma_rounded = [(arr[0], np.round(arr[1], 3)) for arr in nacl_sigma_vals]
+    assert ("Na", 0.076) in nacl_sigma_rounded
+    assert ("Cl", 0.072) in nacl_sigma_rounded
 
 @pytest.mark.skip(reason="Currently not mocked and needs FHI-aims binary")
 def test_anharmonic_quantification_socket_oneshot(si, tmp_path, species_dir):
