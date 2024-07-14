6496f65cf68581660a321654e25da14758f4a293
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-07-13 20:34:27 -0400

    test relax_calcs are ase calculator instances
    upload smallest sevennet checkpoint i could find, fails to load for some reason

--- tests/common/jobs/test_phonon.py
+++ tests/common/jobs/test_phonon.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+from ase.calculators.calculator import Calculator
 from jobflow import Flow, run_locally
 from numpy.testing import assert_allclose
 from pymatgen.core import Structure
@@ -17,18 +20,29 @@ def test_phonon_get_supercell_size(clean_dir, si_structure: Structure):
     assert_allclose(responses[job.uuid][1].output, [[6, -2, 0], [0, 6, 0], [-3, -2, 5]])
 
 
-def test_phonon_maker_initialization_with_all_mlff(si_structure):
+def test_phonon_maker_initialization_with_all_mlff(
+    si_structure: Structure, test_dir: Path
+):
     """Test PhononMaker can be initialized with all MLFF static and relax makers."""
 
+    chk_pt_dir = test_dir / "forcefields"
     for mlff in MLFF:
+        calc_kwargs = {
+            MLFF.Nequip: {"model_path": f"{chk_pt_dir}/nequip/nequip_ff_sr_ti_o3.pth"},
+            MLFF.SevenNet: {  # TODO this currently raises NotImplementedError
+                "model": f"{chk_pt_dir}/sevennet/2024-07-11-SevenNet-0-serial.pt"
+            },
+        }.get(mlff, {})
         static_maker = ForceFieldStaticMaker(
             name=f"{mlff} static",
             force_field_name=str(mlff),
+            calculator_kwargs=calc_kwargs,
         )
         relax_maker = ForceFieldRelaxMaker(
             name=f"{mlff} relax",
             force_field_name=str(mlff),
             relax_kwargs={"fmax": 0.00001},
+            calculator_kwargs=calc_kwargs,
         )
 
         try:
@@ -49,6 +63,13 @@ def test_phonon_maker_initialization_with_all_mlff(si_structure):
             assert flow[4].name == "generate_phonon_displacements", f"{flow[4].name=}"
             assert flow[5].name == "run_phonon_displacements", f"{flow[5].name=}"
 
+            # expected_calc = ase_calculator(mlff)
+            relax_calc = phonon_maker.bulk_relax_maker.calculator
+            if mlff == MLFF.Forcefield:
+                assert relax_calc is None, f"{relax_calc=}"
+            else:
+                assert isinstance(relax_calc, Calculator), f"{type(relax_calc)=}"
         except Exception as exc:
+            # TODO this requires py3.11
             exc.add_note(f"Failed to initialize PhononMaker with {mlff=} makers")
             raise
