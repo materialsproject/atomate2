9e3229fbfce8c0b397b83df546cdd5fa6b766ab7
benrich37 <76569522+benrich37@users.noreply.github.com>
2024-09-03 17:35:32 -0600

    first working test

--- tests/jdftx/io/test_JDFTXOutfile.py
+++ tests/jdftx/io/test_JDFTXOutfile.py
@@ -1,10 +1,13 @@
 from pathlib import Path
-from atomate2.jdftx.io.JDFTXOutfile import JDFTXOutfile
+from atomate2.jdftx.io.JDFTXOutfile import JDFTXOutfile, HA2EV
 from pytest import approx
 
-ex_files_dir = Path(__file__).parents[0]
+ex_files_dir = Path(__file__).parents[0] / "example_files"
 
 def test_JDFTXOutfile():
     filename = ex_files_dir / Path("jdftx.out")
+    print(filename)
     jout = JDFTXOutfile.from_file(filename)
-    assert jout.Ecomponents["F"] == approx(-1940.762261217305650)
+    assert jout.Ecomponents["F"] == approx(-1940.762261217305650*HA2EV)
+
+# test_JDFTXOutfile()
