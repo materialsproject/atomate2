f4769d7589d7738d3b09e59597a6f9924c4c7b53
benrich37 <76569522+benrich37@users.noreply.github.com>
2024-09-06 18:44:10 -0600

    This might be overkill but creating class series for storing input settings of different minimization stuff

--- src/atomate2/jdftx/io/JStructure.py
+++ src/atomate2/jdftx/io/JStructure.py
@@ -167,6 +167,136 @@ class JEiters(list):
         self.converged_reason = line_text.split("(")[1].split(")")[0].strip()
 
 
+@dataclass
+class JMinSettings():
+    '''
+    A class for storing generic minimization settings read from a JDFTx out file
+    '''
+    dirUpdateScheme: str = None
+    linminMethod: str = None
+    nIterations: int = None
+    history: int = None
+    knormThreshold: float = None
+    energyDiffThreshold: float = None
+    nEnergyDiff: int = None
+    alphaTstart: float = None
+    alphaTmin: float = None
+    updateTestStepSize: float = None
+    alphaTreduceFactor: float = None
+    alphaTincreaseFactor: float = None
+    nAlphaAdjustMax: int = None
+    wolfeEnergyThreshold: float = None
+    wolfeGradientThreshold: float = None
+    fdTest: bool = None
+
+    def __init__(self, dirUpdateScheme: str = None, linminMethod: str = None,
+                 nIterations: int = None, history: int = None, knormThreshold: float = None,
+                 energyDiffThreshold: float = None, nEnergyDiff: int = None, alphaTstart: float = None,
+                 alphaTmin: float = None, updateTestStepSize: float = None, alphaTreduceFactor: float = None,
+                 alphaTincreaseFactor: float = None, nAlphaAdjustMax: int = None, wolfeEnergyThreshold: float = None,
+                 wolfeGradientThreshold: float = None, fdTest: bool = None):
+        self.dirUpdateScheme = dirUpdateScheme
+        self.linminMethod = linminMethod
+        self.nIterations = nIterations
+        self.history = history
+        self.knormThreshold = knormThreshold
+        self.energyDiffThreshold = energyDiffThreshold
+        self.nEnergyDiff = nEnergyDiff
+        self.alphaTstart = alphaTstart
+        self.alphaTmin = alphaTmin
+        self.updateTestStepSize = updateTestStepSize
+        self.alphaTreduceFactor = alphaTreduceFactor
+        self.alphaTincreaseFactor = alphaTincreaseFactor
+        self.nAlphaAdjustMax = nAlphaAdjustMax
+        self.wolfeEnergyThreshold = wolfeEnergyThreshold
+        self.wolfeGradientThreshold = wolfeGradientThreshold
+        self.fdTest = fdTest
+
+@dataclass
+class JMinSettingsElectronic(JMinSettings):
+    '''
+    A class for storing lattice minimization settings read from a JDFTx out file
+    '''
+    
+
+    def __init__(self, dirUpdateScheme: str = None, linminMethod: str = None,
+                 nIterations: int = None, history: int = None, knormThreshold: float = None,
+                 energyDiffThreshold: float = None, nEnergyDiff: int = None, alphaTstart: float = None,
+                 alphaTmin: float = None, updateTestStepSize: float = None, alphaTreduceFactor: float = None,
+                 alphaTincreaseFactor: float = None, nAlphaAdjustMax: int = None, wolfeEnergyThreshold: float = None,
+                 wolfeGradientThreshold: float = None, fdTest: bool = None):
+        super().__init__(dirUpdateScheme=dirUpdateScheme, linminMethod=linminMethod,
+                         nIterations=nIterations, history=history, knormThreshold=knormThreshold,
+                         energyDiffThreshold=energyDiffThreshold, nEnergyDiff=nEnergyDiff, alphaTstart=alphaTstart,
+                         alphaTmin=alphaTmin, updateTestStepSize=updateTestStepSize, alphaTreduceFactor=alphaTreduceFactor,
+                         alphaTincreaseFactor=alphaTincreaseFactor, nAlphaAdjustMax=nAlphaAdjustMax,
+                         wolfeEnergyThreshold=wolfeEnergyThreshold, wolfeGradientThreshold=wolfeGradientThreshold,
+                         fdTest=fdTest)
+
+@dataclass
+class JMinSettingsFluid(JMinSettings):
+    '''
+    A class for storing lattice minimization settings read from a JDFTx out file
+    '''
+    
+
+    def __init__(self, dirUpdateScheme: str = None, linminMethod: str = None,
+                 nIterations: int = None, history: int = None, knormThreshold: float = None,
+                 energyDiffThreshold: float = None, nEnergyDiff: int = None, alphaTstart: float = None,
+                 alphaTmin: float = None, updateTestStepSize: float = None, alphaTreduceFactor: float = None,
+                 alphaTincreaseFactor: float = None, nAlphaAdjustMax: int = None, wolfeEnergyThreshold: float = None,
+                 wolfeGradientThreshold: float = None, fdTest: bool = None):
+        super().__init__(dirUpdateScheme=dirUpdateScheme, linminMethod=linminMethod,
+                         nIterations=nIterations, history=history, knormThreshold=knormThreshold,
+                         energyDiffThreshold=energyDiffThreshold, nEnergyDiff=nEnergyDiff, alphaTstart=alphaTstart,
+                         alphaTmin=alphaTmin, updateTestStepSize=updateTestStepSize, alphaTreduceFactor=alphaTreduceFactor,
+                         alphaTincreaseFactor=alphaTincreaseFactor, nAlphaAdjustMax=nAlphaAdjustMax,
+                         wolfeEnergyThreshold=wolfeEnergyThreshold, wolfeGradientThreshold=wolfeGradientThreshold,
+                         fdTest=fdTest)
+
+@dataclass
+class JMinSettingsLattice(JMinSettings):
+    '''
+    A class for storing lattice minimization settings read from a JDFTx out file
+    '''
+    
+
+    def __init__(self, dirUpdateScheme: str = None, linminMethod: str = None,
+                 nIterations: int = None, history: int = None, knormThreshold: float = None,
+                 energyDiffThreshold: float = None, nEnergyDiff: int = None, alphaTstart: float = None,
+                 alphaTmin: float = None, updateTestStepSize: float = None, alphaTreduceFactor: float = None,
+                 alphaTincreaseFactor: float = None, nAlphaAdjustMax: int = None, wolfeEnergyThreshold: float = None,
+                 wolfeGradientThreshold: float = None, fdTest: bool = None):
+        super().__init__(dirUpdateScheme=dirUpdateScheme, linminMethod=linminMethod,
+                         nIterations=nIterations, history=history, knormThreshold=knormThreshold,
+                         energyDiffThreshold=energyDiffThreshold, nEnergyDiff=nEnergyDiff, alphaTstart=alphaTstart,
+                         alphaTmin=alphaTmin, updateTestStepSize=updateTestStepSize, alphaTreduceFactor=alphaTreduceFactor,
+                         alphaTincreaseFactor=alphaTincreaseFactor, nAlphaAdjustMax=nAlphaAdjustMax,
+                         wolfeEnergyThreshold=wolfeEnergyThreshold, wolfeGradientThreshold=wolfeGradientThreshold,
+                         fdTest=fdTest)
+        
+@dataclass
+class JMinSettingsIonic(JMinSettings):
+    '''
+    A class for storing ionic minimization settings read from a JDFTx out file
+    '''
+
+    def __init__(self, dirUpdateScheme: str = None, linminMethod: str = None,
+                 nIterations: int = None, history: int = None, knormThreshold: float = None,
+                 energyDiffThreshold: float = None, nEnergyDiff: int = None, alphaTstart: float = None,
+                 alphaTmin: float = None, updateTestStepSize: float = None, alphaTreduceFactor: float = None,
+                 alphaTincreaseFactor: float = None, nAlphaAdjustMax: int = None, wolfeEnergyThreshold: float = None,
+                 wolfeGradientThreshold: float = None, fdTest: bool = None):
+        super().__init__(dirUpdateScheme=dirUpdateScheme, linminMethod=linminMethod,
+                         nIterations=nIterations, history=history, knormThreshold=knormThreshold,
+                         energyDiffThreshold=energyDiffThreshold, nEnergyDiff=nEnergyDiff, alphaTstart=alphaTstart,
+                         alphaTmin=alphaTmin, updateTestStepSize=updateTestStepSize, alphaTreduceFactor=alphaTreduceFactor,
+                         alphaTincreaseFactor=alphaTincreaseFactor, nAlphaAdjustMax=nAlphaAdjustMax,
+                         wolfeEnergyThreshold=wolfeEnergyThreshold, wolfeGradientThreshold=wolfeGradientThreshold,
+                         fdTest=fdTest)
+
+
+
 
 @dataclass
 class JStructure(Structure):
@@ -197,10 +327,7 @@ class JStructure(Structure):
 
     def __init__(self, lattice: np.ndarray, species: list[str], coords: list[np.ndarray], site_properties: dict[str, list]):
         super().__init__(lattice=lattice, species=species, coords=coords, site_properties=site_properties)
-        # self.lattice = lattice
-        # self.species = species
-        # self.coords = coords
-        # self.site_properties = site_properties
+
 
     @classmethod
     def from_text_slice(cls, text_slice: list[str],
