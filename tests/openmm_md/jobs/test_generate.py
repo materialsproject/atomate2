4c2e892debe4e5f6f54b1e2c3d13bf208c09e6b1
orionarcher <ocohen@radical-ai.com>
2024-09-11 15:39:37 -0400

    Refactor openmm utilities and generate to isolate openff dependency and enhance readability.

--- tests/openmm_md/jobs/test_generate.py
+++ tests/openmm_md/jobs/test_generate.py
@@ -2,12 +2,92 @@ import numpy as np
 import openff.toolkit as tk
 from emmet.core.openmm import OpenMMInterchange
 from jobflow import Flow
+from openff.interchange.components._packmol import pack_box
+from openff.units import unit
 from openmm import XmlSerializer
 
 from atomate2.openff.utils import create_mol_spec
 from atomate2.openmm.jobs import EnergyMinimizationMaker
 from atomate2.openmm.jobs.base import BaseOpenMMMaker
-from atomate2.openmm.jobs.generate import generate_openmm_interchange
+from atomate2.openmm.jobs.generate import (
+    XMLMoleculeFF,
+    create_system_from_xml,
+    generate_openmm_interchange,
+)
+
+
+def test_create_system_from_xml(openmm_data):
+    # load strings of xml files into dict
+    ff_xmls = [
+        XMLMoleculeFF.from_file(openmm_data / "opls_xml_files" / "CCO.xml"),
+        XMLMoleculeFF.from_file(openmm_data / "opls_xml_files" / "CO.xml"),
+    ]
+
+    # uncomment to regenerate data
+    # download_opls_xml("CCO", opls_xmls / "CCO.xml")
+    # download_opls_xml("CO", opls_xmls / "CO.xml")
+
+    mol_specs = [
+        {"smiles": "CCO", "count": 10},
+        {"smiles": "CO", "count": 20},
+    ]
+
+    topology = pack_box(
+        molecules=[tk.Molecule.from_smiles(spec["smiles"]) for spec in mol_specs],
+        number_of_copies=[spec["count"] for spec in mol_specs],
+        mass_density=0.8 * unit.grams / unit.milliliter,
+    )
+
+    create_system_from_xml(topology, ff_xmls)
+
+
+def test_xml_molecule_from_file(openmm_data):
+    xml_mol = XMLMoleculeFF.from_file(openmm_data / "opls_xml_files" / "CO.xml")
+
+    assert isinstance(str(xml_mol), str)
+    assert len(str(xml_mol)) > 100
+
+
+def test_to_openff_molecule(openmm_data):
+    xml_mol = XMLMoleculeFF.from_file(openmm_data / "opls_xml_files" / "CO.xml")
+
+    mol = xml_mol.to_openff_molecule()
+    assert len(mol.atoms) == 6
+    assert len(mol.bonds) == 5
+
+
+def test_assign_partial_charges_w_mol(openmm_data):
+    xml_mol = XMLMoleculeFF.from_file(openmm_data / "opls_xml_files" / "CO.xml")
+
+    openff_mol = tk.Molecule()
+
+    atom_c00 = openff_mol.add_atom(6, 0, is_aromatic=False)
+    atom_h02 = openff_mol.add_atom(1, 0, is_aromatic=False)
+    atom_h03 = openff_mol.add_atom(1, 0, is_aromatic=False)
+    atom_h04 = openff_mol.add_atom(1, 0, is_aromatic=False)
+    atom_h05 = openff_mol.add_atom(1, 0, is_aromatic=False)
+    atom_o01 = openff_mol.add_atom(8, 0, is_aromatic=False)
+
+    openff_mol.add_bond(atom_c00, atom_o01, bond_order=1, is_aromatic=False)
+    openff_mol.add_bond(atom_c00, atom_h02, bond_order=1, is_aromatic=False)
+    openff_mol.add_bond(atom_c00, atom_h03, bond_order=1, is_aromatic=False)
+    openff_mol.add_bond(atom_c00, atom_h04, bond_order=1, is_aromatic=False)
+    openff_mol.add_bond(atom_o01, atom_h05, bond_order=1, is_aromatic=False)
+
+    openff_mol.assign_partial_charges("mmff94")
+
+    xml_mol.assign_partial_charges(openff_mol)
+    assert xml_mol.partial_charges[0] > 0.2  # C
+    assert xml_mol.partial_charges[1] < -0.3  # O
+    assert xml_mol.partial_charges[5] > 0.1  # alcohol H
+
+
+def test_assign_partial_charges_w_method(openmm_data):
+    xml_mol = XMLMoleculeFF.from_file(openmm_data / "opls_xml_files" / "CO.xml")
+    xml_mol.assign_partial_charges("mmff94")
+    assert xml_mol.partial_charges[0] > 0.2  # C
+    assert xml_mol.partial_charges[1] < -0.3  # O
+    assert xml_mol.partial_charges[5] > 0.1  # alcohol H
 
 
 def test_generate_openmm_interchange(openmm_data, run_job):
