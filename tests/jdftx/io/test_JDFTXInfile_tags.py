e681f187a0cd0abc05f2f1941a359d74c7bab076
benrich37 <76569522+benrich37@users.noreply.github.com>
2024-09-11 22:09:15 -0600

    Added known value test to testchecks

--- tests/jdftx/io/test_JDFTXInfile_tags.py
+++ tests/jdftx/io/test_JDFTXInfile_tags.py
@@ -1,11 +1,15 @@
 # import pytest
 from atomate2.jdftx.io.JDFTXInfile_master_format import *
+from atomate2.jdftx.io.JDFTXInfile import JDFTXInfile
+from pathlib import Path
+import os
 
-infile = Path(os.getcwd()) / "tests" / "jdftx" / "io" / "example_files" / "example_sp.in"
-testwrite = Path(os.getcwd()) / "tests" / "jdftx" / "io" / "example_files" / "example_sp_copy.in"
-jif = JDFTXInfile.from_file(infile)
-jif.write_file(testwrite)
-jiflist = jif.get_text_list()
-tag_ex = "fluid-anion"
 
-get_tag_object(tag_ex)
+# infile = Path(os.getcwd()) / "tests" / "jdftx" / "io" / "example_files" / "example_sp.in"
+# testwrite = Path(os.getcwd()) / "tests" / "jdftx" / "io" / "example_files" / "example_sp_copy.in"
+# jif = JDFTXInfile.from_file(infile)
+# jif.write_file(testwrite)
+# jiflist = jif.get_text_list()
+# tag_ex = "fluid-anion"
+
+# get_tag_object(tag_ex)
