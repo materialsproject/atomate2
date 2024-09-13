831498f7f6614c19cc658e8dea8947e073655bd2
benrich37 <76569522+benrich37@users.noreply.github.com>
2024-09-12 18:53:01 -0600

    minimal pre-commit sacrifices

--- src/atomate2/jdftx/io/joutstructures.py
+++ src/atomate2/jdftx/io/joutstructures.py
@@ -1,14 +1,30 @@
-from dataclasses import dataclass
+"""Module for JOutStructures class.
+
+This module contains the JOutStructures class for storing a series of
+JOutStructure.
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
 from typing import Any
 
-from jdftx.io.joutstructure_helpers import correct_iter_type, is_lowdin_start_line
-
 from atomate2.jdftx.io.joutstructure import JOutStructure
+from atomate2.jdftx.io.joutstructure_helpers import (
+    correct_iter_type,
+    is_lowdin_start_line,
+)
+
+elec_min_start_flag: str = "-------- Electronic minimization -----------"
 
 
-def get_start_idx(out_slice: list[str], out_slice_start_flag: str = "-------- Electronic minimization -----------") -> int:
-    """
-    Returns the index of the first line of the first structure in the out_slice
+def get_start_idx(
+    out_slice: list[str],
+    out_slice_start_flag: str = elec_min_start_flag,
+) -> int:
+    """Return index of first line of first structure.
+
+    Return the index of the first line of the first structure in the out_slice.
 
     Parameters
     ----------
@@ -26,10 +42,14 @@ def get_start_idx(out_slice: list[str], out_slice_start_flag: str = "-------- El
     return i
 
 
-def get_step_bounds(out_slice: list[str], out_slice_start_flag: str = "-------- Electronic minimization -----------") -> list[list[int, int]]:
-    """
-    Returns a list of lists of integers where each sublist contains the start and end
-    of an individual optimization step (or SCF cycle if no optimization)
+def get_step_bounds(
+    out_slice: list[str],
+    out_slice_start_flag: str = elec_min_start_flag,
+) -> list[list[int]]:
+    """Return list of boundary indices for each structure in out_slice.
+
+    Return a list of lists of integers where each sublist contains the start and end
+    of an individual optimization step (or SCF cycle if no optimization).
 
     Parameters
     ----------
@@ -49,9 +69,8 @@ def get_step_bounds(out_slice: list[str], out_slice_start_flag: str = "-------- 
         if not end_started:
             if out_slice_start_flag in line:
                 bounds = [i]
-            elif bounds is not None:
-                if is_lowdin_start_line(line):
-                    end_started = True
+            elif (bounds is not None) and (is_lowdin_start_line(line)):
+                end_started = True
         elif not len(line.strip()):
             bounds.append(i)
             bounds_list.append(bounds)
@@ -60,13 +79,11 @@ def get_step_bounds(out_slice: list[str], out_slice_start_flag: str = "-------- 
     return bounds_list
 
 
-
-
-
 @dataclass
-class JOutStructures(list[JOutStructure]):
-    """
-    A class for storing a series of JStructure objects
+class JOutStructures:
+    """Class for storing a series of JStructure objects.
+
+    A class for storing a series of JStructure objects.
     """
 
     out_slice_start_flag = "-------- Electronic minimization -----------"
@@ -76,25 +93,28 @@ class JOutStructures(list[JOutStructure]):
     elec_converged: bool = False
     elec_converged_reason: str = None
     _t_s: float = None
-    
+    slices: list[JOutStructure] = field(default_factory=list)
 
     @classmethod
-    def from_out_slice(cls, out_slice: list[str], iter_type: str = "IonicMinimize"):
-        """
-        Create a JStructures object from a slice of an out file's text corresponding
-        to a single JDFTx call
+    def from_out_slice(
+        cls, out_slice: list[str], iter_type: str = "IonicMinimize"
+    ) -> JOutStructures:
+        """Return JStructures object.
+
+        Create a JStructures object from a slice of an out file's text
+        corresponding to a single JDFTx call.
 
         Parameters
         ----------
         out_slice: list[str]
             A slice of a JDFTx out file (individual call of JDFTx)
         """
-        instance = cls([])
-        if not iter_type in ["IonicMinimize", "LatticeMinimize"]:
+        instance = cls()
+        if iter_type not in ["IonicMinimize", "LatticeMinimize"]:
             iter_type = correct_iter_type(iter_type)
         instance.iter_type = iter_type
         start_idx = get_start_idx(out_slice)
-        instance.set_JOutStructure_list(out_slice[start_idx:])
+        instance.set_joutstructure_list(out_slice[start_idx:])
         if instance.iter_type is None and len(instance) > 1:
             raise Warning(
                 "iter type interpreted as single-point calculation, but \
@@ -104,8 +124,9 @@ class JOutStructures(list[JOutStructure]):
 
     @property
     def t_s(self) -> float:
-        """
-        Returns the total time in seconds for the calculation
+        """Return time of calculation.
+
+        Return the total time in seconds for the calculation.
 
         Returns
         -------
@@ -116,15 +137,16 @@ class JOutStructures(list[JOutStructure]):
             return self._t_s
         if len(self):
             if self.iter_type in ["single point", None]:
-                self._t_s = self[-1].elecMinData[-1].t_s
+                self._t_s = self[-1].elecmindata[-1].t_s
             else:
                 self._t_s = self[-1].t_s
         return self._t_s
-    
 
-    def get_JOutStructure_list(self, out_slice: list[str]) -> list[JOutStructure]:
-        """
-        Set relevant variables for the JStructures object by parsing the out_slice
+    def get_joutstructure_list(self, out_slice: list[str]) -> list[JOutStructure]:
+        """Return list of JOutStructure objects.
+
+        Set relevant variables for the JStructures object by parsing the
+        out_slice.
 
         Parameters
         ----------
@@ -132,199 +154,140 @@ class JOutStructures(list[JOutStructure]):
             A slice of a JDFTx out file (individual call of JDFTx)
         """
         out_bounds = get_step_bounds(out_slice)
-        out_list = []
-        print(self.iter_type)
-        for bounds in out_bounds:
-            out_list.append(
-                JOutStructure.from_text_slice(
-                    out_slice[bounds[0] : bounds[1]], iter_type=self.iter_type
-                )
+        # out_list = [
+        #     JOutStructure.from_text_slice(
+        #         out_slice[bounds[0] : bounds[1]], iter_type=self.iter_type
+        #     )
+        #     for bounds in out_bounds
+        # ]
+        # for bounds in out_bounds:
+        #     out_list.append(
+        #         JOutStructure.from_text_slice(
+        #             out_slice[bounds[0] : bounds[1]], iter_type=self.iter_type
+        #         )
+        #     )
+        return [
+            JOutStructure.from_text_slice(
+                out_slice[bounds[0] : bounds[1]], iter_type=self.iter_type
             )
-        return out_list
-    
-    
-    def set_JOutStructure_list(self, out_slice: list[str]) -> None:
-        out_list = self.get_JOutStructure_list(out_slice)
+            for bounds in out_bounds
+        ]
+
+    def set_joutstructure_list(self, out_slice: list[str]) -> None:
+        """Set list of JOutStructure objects to slices.
+
+        Set the list of JOutStructure objects to the slices attribute.
+
+        Parameters
+        ----------
+        out_slice: list[str]
+            A slice of a JDFTx out file (individual call of JDFTx)
+        """
+        out_list = self.get_joutstructure_list(out_slice)
         for jos in out_list:
-            self.append(jos)
-
-
-    def __getattr__(self, name):
-        try:
-            return super().__getattr__(name)
-        except AttributeError:
-            if self:
-                return getattr(self[-1], name)
-            raise AttributeError(f"'JStructures' object has no attribute '{name}'")
-
-
-    
-
-
-    
+            self.slices.append(jos)
 
     def check_convergence(self) -> None:
+        """Set convergence flags.
+
+        Check if the geometry and electronic density of last structure in the
+        list has converged.
         """
-        Check if the geometry and electronic density of last structure in the list has converged
-        """
-        jst = self[-1]
-        if jst.elecMinData.converged:
+        jst = self.slices[-1]
+        if jst.elecmindata.converged:
             self.elec_converged = True
-            self.elec_converged_reason = jst.elecMinData.converged_reason
+            self.elec_converged_reason = jst.elecmindata.converged_reason
         if jst.geom_converged:
             self.geom_converged = True
             self.geom_converged_reason = jst.geom_converged_reason
 
+    def __getatr__(self, name: str) -> Any:
+        """Return attribute value.
 
+        Return the value of an attribute.
 
-# @dataclass
-# class JOutStructures(list[JOutStructure], JOutStructure):
-#     """
-#     A class for storing a series of JStructure objects
-#     """
+        Parameters
+        ----------
+        name: str
+            The name of the attribute
 
-#     out_slice_start_flag = "-------- Electronic minimization -----------"
-#     iter_type: str = None
-#     geom_converged: bool = False
-#     geom_converged_reason: str = None
-#     elec_converged: bool = False
-#     elec_converged_reason: str = None
-#     _t_s: float = None
+        Returns
+        -------
+        value
+            The value of the attribute
+        """
+        if not hasattr(self, name):
+            if not hasattr(self.slices[-1], name):
+                raise AttributeError(f"{self.__class__.__name__} not found: {name}")
+            return getattr(self.slices[-1], name)
+        return getattr(self, name)
 
-#     def __init__(self, *args: Any, **kwargs: Any):
-#         super().__init__(*args, **kwargs)
-#         self._t_s = None
+    def __getitem__(self, key: int | str) -> JOutStructure | Any:
+        """Return item.
 
-#     @classmethod
-#     def from_out_slice(cls, out_slice: list[str], iter_type: str = "IonicMinimize"):
-#         """
-#         Create a JStructures object from a slice of an out file's text corresponding
-#         to a single JDFTx call
+        Return the value of an item.
 
-#         Parameters
-#         ----------
-#         out_slice: list[str]
-#             A slice of a JDFTx out file (individual call of JDFTx)
-#         """
-#         instance = cls()
-#         if iter_type not in ["IonicMinimize", "LatticeMinimize"]:
-#             iter_type = instance.correct_iter_type(iter_type)
-#         instance.iter_type = iter_type
-#         start_idx = instance.get_start_idx(out_slice)
-#         instance.parse_out_slice(out_slice[start_idx:])
-#         if instance.iter_type is None and len(instance) > 1:
-#             raise Warning(
-#                 "iter type interpreted as single-point calculation, but \
-#                            multiple structures found"
-#             )
-#         return instance
+        Parameters
+        ----------
+        key: int | str
+            The key of the item
 
-#     @property
-#     def t_s(self) -> float:
-#         """
-#         Returns the total time in seconds for the calculation
+        Returns
+        -------
+        val
+            The value of the item
+        """
+        val = None
+        if type(key) is int:
+            val = self.getitem_int(key)
+        if type(key) is str:
+            val = self.getitem_str(key)
+        return val
 
-#         Returns
-#         -------
-#         t_s: float
-#             The total time in seconds for the calculation
-#         """
-#         if self._t_s is not None:
-#             return self._t_s
-#         if len(self):
-#             if self.iter_type in ["single point", None]:
-#                 self._t_s = self[-1].elecMinData[-1].t_s
-#             else:
-#                 self._t_s = self[-1].t_s
-#         return self._t_s
+    def getitem_int(self, key: int) -> JOutStructure:
+        """Return JOutStructure object.
 
-#     def __getattr__(self, name):
-#         try:
-#             return super().__getattr__(name)
-#         except AttributeError:
-#             if self:
-#                 return getattr(self[-1], name)
-#             raise AttributeError(f"'JStructures' object has no attribute '{name}'")
+        Return the JOutStructure object at the key index.
 
+        Parameters
+        ----------
+        key: int
+            The index of the JOutStructure object
 
-#     def get_start_idx(self, out_slice: list[str]) -> int:
-#         """
-#         Returns the index of the first line of the first structure in the out_slice
+        Returns
+        -------
+        joutstructure: JOutStructure
+            The JOutStructure object at the key index
+        """
+        return self.slices[key]
 
-#         Parameters
-#         ----------
-#         out_slice: list[str]
-#             A slice of a JDFTx out file (individual call of JDFTx)
+    def getitem_str(self, key: str) -> Any:
+        """Return attribute value.
 
-#         Returns
-#         -------
-#         i: int
-#             The index of the first line of the first structure in the out_slice
-#         """
-#         for i, line in enumerate(out_slice):
-#             if self.out_slice_start_flag in line:
-#                 return i
-#         return i
+        Return the value of an attribute.
 
+        Parameters
+        ----------
+        key: str
+            The name of the attribute
 
-#     def get_step_bounds(self, out_slice: list[str]) -> list[list[int, int]]:
-#         """
-#         Returns a list of lists of integers where each sublist contains the start and end
-#         of an individual optimization step (or SCF cycle if no optimization)
+        Returns
+        -------
+        value
+            The value of the attribute
+        """
+        return getattr(self, key)
 
-#         Parameters
-#         ----------
-#         out_slice: list[str]
-#             A slice of a JDFTx out file (individual call of JDFTx)
+    def __len__(self) -> int:
+        """Return length of JOutStructures object.
 
-#         Returns
-#         -------
-#         bounds_list: list[list[int, int]]
-#             A list of lists of integers where each sublist contains the start and end
-#             of an individual optimization step (or SCF cycle if no optimization)
-#         """
-#         bounds_list = []
-#         bounds = None
-#         end_started = False
-#         for i, line in enumerate(out_slice):
-#             if not end_started:
-#                 if self.out_slice_start_flag in line:
-#                     bounds = [i]
-#                 elif bounds is not None:
-#                     if self.is_lowdin_start_line(line):
-#                         end_started = True
-#             elif not len(line.strip()):
-#                 bounds.append(i)
-#                 bounds_list.append(bounds)
-#                 bounds = None
-#                 end_started = False
-#         return bounds_list
+        Returns the number of geometric optimization steps in the
+        JOutStructures object.
 
-#     def parse_out_slice(self, out_slice: list[str]) -> None:
-#         """
-#         Set relevant variables for the JStructures object by parsing the out_slice
-
-#         Parameters
-#         ----------
-#         out_slice: list[str]
-#             A slice of a JDFTx out file (individual call of JDFTx)
-#         """
-#         out_bounds = self.get_step_bounds(out_slice)
-#         for bounds in out_bounds:
-#             self.append(
-#                 JOutStructure.from_text_slice(
-#                     out_slice[bounds[0] : bounds[1]], iter_type=self.iter_type
-#                 )
-#             )
-
-#     def check_convergence(self) -> None:
-#         """
-#         Check if the geometry and electronic density of last structure in the list has converged
-#         """
-#         jst = self[-1]
-#         if jst.elecMinData.converged:
-#             self.elec_converged = True
-#             self.elec_converged_reason = jst.elecMinData.converged_reason
-#         if jst.geom_converged:
-#             self.geom_converged = True
-#             self.geom_converged_reason = jst.geom_converged_reason
+        Returns
+        -------
+        length: int
+            The number of geometric optimization steps in the JOutStructures
+            object
+        """
+        return len(self.slices)
