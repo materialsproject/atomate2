846d3aa796f610bbf4442d9c108cdf9ca03f4f24
Victor Trinquet <victor.trinquet@uclouvain.be>
2024-10-02 16:49:59 +0200

    remove some comments

--- src/atomate2/abinit/flows/dfpt.py
+++ src/atomate2/abinit/flows/dfpt.py
@@ -135,14 +135,7 @@ class DfptFlowMaker(Maker):
                     perturbation=pert,
                     prev_outputs=static_job.output.dir_name,
                 )
-                # next line throws :
-                # File "/gpfs/home/acad/ucl-modl/vtrinque/Software/jobflow/src
-                # /jobflow/utils/find.py", line 84, in _lookup
-                #     if key in obj:
-                # TypeError: unhashable type: 'dict'
-                # with key being {'append_str': '1/3', 'prepend': False}
                 ddk_job.append_name(f"{ipert+1}/{len(perturbations)}")
-                # ddk_job.name = ddk_job.name + f"{ipert+1}/{len(perturbations)}"
 
                 ddk_jobs.append(ddk_job)
                 outputs["dirs"].append(ddk_job.output.dir_name)
