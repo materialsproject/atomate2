9c4447c9562bc11777ed15c692cb272581c36ebe
Orion Cohen <27712051+orionarcher@users.noreply.github.com>
2024-10-31 04:10:06 -0400

    Allow custom mace model by specifying "model" in calculator kwargs" (#1017)
    * allow custom mace model by specifying "model" in calculator kwargs"
    
    * fix error in trying to turn None into path
    
    * Add support for ORB model
    
    * Specify more dependencies
    
    * remove orb implementation
    
    * add line
    
    * add line
    
    * remove device
    
    * set device
    
    * fix set device
    
    * fix test
    
    * fix linting
    
    * restore test
    
    * remove os and rely on pathlib only
    
    ---------
    
    Co-authored-by: J. George <JaGeo@users.noreply.github.com>
    Co-authored-by: JaGeo <janinegeorge.ulfen@gmail.com>

--- src/atomate2/forcefields/utils.py
+++ src/atomate2/forcefields/utils.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import json
 from contextlib import contextmanager
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from monty.json import MontyDecoder
@@ -59,9 +60,21 @@ def ase_calculator(calculator_meta: str | dict, **kwargs: Any) -> Calculator | N
             calculator = PESCalculator(potential, **kwargs)
 
         elif calculator_name == MLFF.MACE:
-            from mace.calculators import mace_mp
+            from mace.calculators import MACECalculator, mace_mp
 
-            calculator = mace_mp(**kwargs)
+            model = kwargs.get("model")
+            if isinstance(model, str | Path) and Path(model).exists():
+                model_path = model
+                device = kwargs.get("device") or "cpu"
+                if "device" in kwargs:
+                    del kwargs["device"]
+                calculator = MACECalculator(
+                    model_paths=model_path,
+                    device=device,
+                    **kwargs,
+                )
+            else:
+                calculator = mace_mp(**kwargs)
 
         elif calculator_name == MLFF.GAP:
             from quippy.potential import Potential
