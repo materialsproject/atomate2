29f5df709d24911c495aaf89accbe6a1bdeeb1c9
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2024-10-31 11:15:05 -0700

    partial lint

--- tests/vasp/flows/test_approx_neb.py
+++ tests/vasp/flows/test_approx_neb.py
@@ -1,17 +1,17 @@
 """Test ApproxNEB workflow."""
 
 from pathlib import Path
-import pytest
 
+import pytest
 from jobflow import run_locally
-from monty.serialization import loadfn, dumpfn
+from monty.serialization import loadfn
 from pymatgen.core import Structure
 
-from atomate2.common.schemas.neb import NebResult, NebPathwayResult
+from atomate2.common.schemas.neb import NebPathwayResult, NebResult
 from atomate2.vasp.flows.approx_neb import ApproxNEBMaker
 
-def test_approx_neb_flow(mock_vasp, clean_dir, vasp_test_dir):
 
+def test_approx_neb_flow(mock_vasp, clean_dir, vasp_test_dir):
     base_dir = Path(vasp_test_dir) / "ApproxNEB"
     flow_input = loadfn(base_dir / "approx_neb_input.json.gz")
 
@@ -49,7 +49,9 @@ def test_approx_neb_flow(mock_vasp, clean_dir, vasp_test_dir):
     }
 
     fake_run_vasp_kwargs = {
-        key : {"incar_exclude": ["IBRION","ALGO"]} # updated this to generate flow test data more quickly
+        key: {
+            "incar_exclude": ["IBRION", "ALGO"]
+        }  # updated this to generate flow test data more quickly
         for key in ref_paths
     }
 
@@ -57,14 +59,14 @@ def test_approx_neb_flow(mock_vasp, clean_dir, vasp_test_dir):
     for key in [k for k in fake_run_vasp_kwargs if "relax hop" in k]:
         fake_run_vasp_kwargs[key]["check_inputs"] = ["incar", "kpoints", "potcar"]
 
-    mock_vasp(ref_paths,fake_run_vasp_kwargs)
+    mock_vasp(ref_paths, fake_run_vasp_kwargs)
     # Do not ensure success here as the following jobs failed, but the flow should not:
     # ApproxNEB image relax hop 3+0 image 3
     # ApproxNEB image relax hop 3+0 image 5
     # ApproxNEB image relax hop 3+1 image 2
     responses = run_locally(flow, create_folders=True, ensure_success=False)
     output = {
-        job.name : responses[job.uuid][1].output
+        job.name: responses[job.uuid][1].output
         for job in flow.jobs
         if job.uuid in responses
     }
@@ -72,31 +74,35 @@ def test_approx_neb_flow(mock_vasp, clean_dir, vasp_test_dir):
     assert len(output["collate_results"].hops) == 2
 
     assert all(
-        len(getattr(output["collate_results"].hops["3+0"],k)) == 5 # two failed image calcs
-        for k in ("energies","images")
+        len(getattr(output["collate_results"].hops["3+0"], k))
+        == 5  # two failed image calcs
+        for k in ("energies", "images")
     )
     assert all(
-        len(getattr(output["collate_results"].hops["3+1"],k)) == 6 # one failed image calc
-        for k in ("energies","images")
+        len(getattr(output["collate_results"].hops["3+1"], k))
+        == 6  # one failed image calc
+        for k in ("energies", "images")
     )
 
     assert all(
-        isinstance(image,Structure)
+        isinstance(image, Structure)
         for hop in output["collate_results"].hops.values()
         for image in hop.images
     )
 
     assert isinstance(output["collate_results"], NebPathwayResult)
     assert all(
-        isinstance(hop,NebResult) for hop in output["collate_results"].hops.values()
+        isinstance(hop, NebResult) for hop in output["collate_results"].hops.values()
     )
     assert all(
-        output["collate_results"].max_barriers[k] == pytest.approx(
+        output["collate_results"].max_barriers[k]
+        == pytest.approx(
             max(
                 output["collate_results"].forward_barriers[k],
-                output["collate_results"].forward_barriers[k]
+                output["collate_results"].forward_barriers[k],
             )
-        ) for k in output["collate_results"].hops
+        )
+        for k in output["collate_results"].hops
     )
 
     ref_results = loadfn(base_dir / "collate_results.json.gz")
@@ -108,9 +114,8 @@ def test_approx_neb_flow(mock_vasp, clean_dir, vasp_test_dir):
     )
 
     assert all(
-        getattr(output["collate_results"],f"{direction}_barriers")[k] == pytest.approx(
-            getattr(ref_results,f"{direction}_barriers")[k]
-        )
-        for direction in ("forward","reverse")
+        getattr(output["collate_results"], f"{direction}_barriers")[k]
+        == pytest.approx(getattr(ref_results, f"{direction}_barriers")[k])
+        for direction in ("forward", "reverse")
         for k in ref_results.hops
     )
