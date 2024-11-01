c1e670fc84972843b775435d6ddb6817793a3ca8
Vir Karan <virkaran123456@gmail.com>
2024-11-01 15:28:09 -0700

    Redid trajectory reading into taskdoc with a new DumpConvertor class, based on atomate2.ase.utils.TrajectoryObserver. Also accounted for reading in molecules and saving as a trajectory.

--- src/atomate2/lammps/files.py
+++ src/atomate2/lammps/files.py
@@ -1,12 +1,14 @@
 from pathlib import Path
-
-from pymatgen.core import Structure
-
+import numpy as np
+from pymatgen.core import Structure, Molecule
 from pymatgen.io.lammps.generators import BaseLammpsGenerator
 from ase.io import read
 from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.core.trajectory import Trajectory as PmgTrajectory
 from ase.io import Trajectory as AseTrajectory
+from typing import Literal
+from monty.serialization import dumpfn
+from monty.json import MSONable
 
 def write_lammps_input_set(
     structure: Structure | Path,
@@ -17,28 +19,61 @@ def write_lammps_input_set(
     input_set = input_set_generator.get_input_set(structure, **kwargs)
     input_set.write_input(directory)
 
-    
-def trajectory_from_lammps_dump(dump_path, trajectory_format : str = 'pmg'):
-    '''
-    Read a LAMMPS dump file and return a pymatgen Trajectory object.
-    
-    Args:
-        dump_path (str): Path to the LAMMPS dump file.
+
+class DumpConvertor(MSONable):    
+    def __init__(self, dumpfile, store_md_outputs : Literal["no", "partial", "full"]  = 'partial') -> None:
+        self.store_md_outputs = store_md_outputs
+        self.traj = read(dumpfile, index=':')
+        self.is_periodic = any(self.traj[0].pbc)
+        self.frame_properties_keys = ['forces', 'velocities']
         
-    Returns:
-        Trajectory: Pymatgen Trajectory object.
-    '''
+    def to_ase_trajectory(self, filename : str | None = None):
+        for idx, atoms in enumerate(self.traj):
+            with AseTrajectory(filename, 'a' if idx > 0 else 'w', atoms = atoms) as file: #check logic here
+                file.write()
+        return AseTrajectory(filename, 'r')
     
-    lammps_traj = read(dump_path, index=':')
-    
-    if trajectory_format == 'ase':
-        return AseTrajectory(lammps_traj)
-    
-    if trajectory_format == 'pmg':
+    def to_pymatgen_trajectory(self, filename : str | None = 'trajectory.json.gz') -> PmgTrajectory:
+                
+        species = AseAtomsAdaptor.get_structure(self.traj[0], cls = Structure if self.is_periodic else Molecule).species
+
+        frames  = []
+        frame_properties = []
         
-        frames = [AseAtomsAdaptor.get_structure(atoms) for atoms in lammps_traj]
-        veclocities = [atom.get_velocities() for atom in lammps_traj]
-        forces = [atom.get_forces() for atom in lammps_traj]
-        #TODO: add velocities and forces to the frame properties
-        #TODO: account for molecules instead of structures
-    return PmgTrajectory.from_structures(frames)
+        for atoms in self.traj:
+            if self.store_md_outputs:
+                if self.store_md_outputs == 'full':
+                    frame_properties.append({key : getattr(atoms, f'get_{key}')() for key in self.frame_properties_keys})
+                    
+                if self.is_periodic:
+                    frames.append(Structure(lattice = atoms.get_cell(), 
+                                            species = species, 
+                                            coords = atoms.get_positions(), 
+                                            coords_are_cartesian = True)
+                                  )
+                else:
+                    frames.append(Molecule(species = species, 
+                                           coords = atoms.get_positions(), 
+                                           charge = atoms.get_charges(),
+                                           )
+                                  )
+        traj_method = 'from_structures' if self.is_periodic else 'from_molecules'
+        pmg_traj = getattr(PmgTrajectory, traj_method)(
+            frames,
+            frame_properties=frame_properties if frame_properties else None,
+            constant_lattice=False,
+        )
+        
+        if filename:
+            dumpfn(pmg_traj, filename)
+        
+        return pmg_traj
+    
+    def save(self, filename : str | None = None, fmt : Literal["pmg", "ase"] = "pmg"):
+        filename = str(filename) if filename is not None else None
+        if fmt == "pmg":
+            return self.to_pymatgen_trajectory(filename=filename) 
+        elif fmt == "ase":
+            return self.to_ase_trajectory(filename=filename)
+
+        
