e96ac019796d585a694e0ea0b934a82d0cbdf556
Vir Karan <virkaran123456@gmail.com>
2024-11-01 21:00:21 -0700

    Added basic implementation for NVT and NPT sets

--- src/atomate2/lammps/sets/core.py
+++ src/atomate2/lammps/sets/core.py
@@ -1,17 +1,65 @@
 from .base import BaseLammpsSet
+from atomate2.lammps.sets.utils import update_settings, _BASE_LAMMPS_SETTINGS
+from typing import Literal
 
 class LammpsNVTSet(BaseLammpsSet):
     """
     Input set for NVT simulations.
     """
+    ensemble : str = "nvt"
     thermostat : str = "lengevin"
     friction : float = 0.1
     
     def __init__(self, 
-                 thermostat : str = "lengevin",
-                friction : float = 0.1,
+                 thermostat : Literal['langevin', 'nose-hoover'] = 'nose-hoover',
+                 friction : float = None,
                  **kwargs):
-        self.settings.update({'thermostat': self.thermostat, 'friction': self.friction})
+        
+        if thermostat == 'nose-hoover':
+            thermostat = 'temp'
+        if friction is None:
+            friction = 100*kwargs.get('timestep', _BASE_LAMMPS_SETTINGS['timestep'])
+        if friction < kwargs.get('timestep', 0.001):
+            raise ValueError("Friction should be more than the timestep")
+        
+        self.settings = {'thermostat': thermostat, 
+                         'tfriction': friction,
+                         'ensemble': 'nvt'
+                         }
+        
+        self.settings = update_settings(settings=self.settings, **kwargs)
         super().__init__(**kwargs)
-        #TODO: fix this logic
-        #super().__init__(input_set_updates=input_set_updates, **kwargs)
+
+class LammpsNPTSet(BaseLammpsSet):
+    """
+    Input set for NPT simulations.
+    """
+    ensemble : str = "npt"
+    barostat : str = "berendsen"
+    thermostat : str = "nose-hoover"
+    friction : float = 0.1
+    
+    def __init__(self, 
+                 barostat : Literal['berendsen', 'nose-hoover'] = 'berendsen',
+                 thermostat : Literal['langevin', 'nose-hoover'] = 'nose-hoover',
+                 friction : float = None,
+                 **kwargs):
+        
+        if barostat == 'nose-hoover':
+            barostat = 'temp'
+        if thermostat == 'nose-hoover':
+            thermostat = 'temp'
+        if friction is None:
+            friction = 100*kwargs.get('timestep', _BASE_LAMMPS_SETTINGS['timestep'])
+        if friction < kwargs.get('timestep', 0.001):
+            raise ValueError("Friction should be more than the timestep")
+        
+        self.settings = {'barostat': barostat, 
+                         'thermostat': thermostat, 
+                         'pfriction': friction,
+                         'tfriction': friction,
+                         'ensemble': 'npt'
+                         }
+        
+        self.settings = update_settings(settings=self.settings, **kwargs)
+        super().__init__(**kwargs)
