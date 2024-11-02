ec3b7d0adcfa1bc165aeddf57103494dde277580
Vir Karan <virkaran123456@gmail.com>
2024-11-01 20:58:07 -0700

    Streamlined lammps input set settings, moved default settings and keys to json files for easier access, added utility funcs to process settings dicts

--- src/atomate2/lammps/sets/base.py
+++ src/atomate2/lammps/sets/base.py
@@ -8,51 +8,33 @@ import os
 from pathlib import Path
 from string import Template
 
-from monty.io import zopen
 from pymatgen.core import Structure
 from pymatgen.io.core import InputGenerator, InputSet
 from pymatgen.io.lammps.data import CombinedData, LammpsData
 from pymatgen.io.lammps.inputs import LammpsInputFile
 from pymatgen.io.lammps.sets import LammpsInputSet
 from pymatgen.io.lammps.generators import BaseLammpsGenerator, LammpsMinimization
-from typing import Union
+from typing import Union, Literal
+from monty.serialization import loadfn
+from atomate2.lammps.sets.utils import process_ensemble_conditions, update_settings
 
 __author__ = "Ryan Kingsbury, Guillaume Brunin (Matgenix)"
 
 logger = logging.getLogger(__name__)
 template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
 
-_BASE_LAMMPS_SETTINGS = {"atom_style": "atomic",
-                 "start_temp": 300,
-                 "end_temp": 300,
-                 "pressure": 0,
-                 "units": "metal",
-                 "nsteps": 1000,
-                 "timestep": 0.001,
-                 "log_interval": 100,
-                 "traj_interval": 100,
-}
-
 class BaseLammpsSet(BaseLammpsGenerator):
     """
     Basic LAMMPS input set generator.
     """
-
-    atom_style = "atomic"
-    start_temp = 300
-    end_temp = 300
-    pressure = 0
-    units = "metal"
-    nsteps = 1000
-    timestep = 0.001
-    force_field_def = None
-    template = os.path.join(template_dir, "md.template")
             
     def __init__(self, 
                  atom_style : str = "atomic",
+                 ensemble : Literal["nve", "nvt", "npt"] = "nve",
                  start_temp : float = 300,
                  end_temp : float = 300,
-                 pressure : float = 0,
+                 start_pressure : float = 0,
+                 end_pressure : float = 0,
                  units : str = "metal",
                  nsteps : int = 1000,
                  timestep : float = 0.001,
@@ -60,12 +42,17 @@ class BaseLammpsSet(BaseLammpsGenerator):
                  traj_interval : int = 100,
                  force_field : Union[str, dict] = None,
                  template : str = None,
+                 settings : dict = None,
                  **kwargs):
         
+        template = os.path.join(template_dir, "md.template") if template is None else template
+        self.settings = settings
         self.atom_style = atom_style
+        self.ensemble = ensemble
         self.start_temp = start_temp
         self.end_temp = end_temp
-        self.pressure = pressure
+        self.start_pressure = start_pressure
+        self.end_pressure = end_pressure
         self.units = units
         self.nsteps = nsteps
         self.timestep = timestep
@@ -74,21 +61,14 @@ class BaseLammpsSet(BaseLammpsGenerator):
         self.force_field = force_field
         self.species = None
         
-        if template is None:
-            self.template = os.path.join(template_dir, "md.template")
-        else:
-            self.template = template
-        
-        self.settings = _BASE_LAMMPS_SETTINGS.copy()
-        self.settings.update({'atom_style': atom_style,
-                         'start_temp': self.start_temp,
-                         'end_temp': self.end_temp,
-                         'pressure': self.pressure,
-                         'units': self.units,
-                         'nsteps': self.nsteps,
-                         'timestep': self.timestep,
-                         'log_interval': self.log_interval,
-                         'traj_interval': self.traj_interval})
+        process_kwargs = kwargs.copy()
+        process_kwargs.update({'atom_style': self.atom_style, 'ensemble': self.ensemble, 'start_temp': self.start_temp,
+                       'end_temp': self.end_temp, 'start_pressure': self.start_pressure, 'end_pressure': self.end_pressure,
+                       'units': self.units, 'nsteps': self.nsteps, 'timestep': self.timestep, 'log_interval': self.log_interval,
+                       'traj_interval': self.traj_interval})
+                
+        self.settings = update_settings(settings = self.settings, **process_kwargs)
+        self.settings = process_ensemble_conditions(self.settings)
         
         if isinstance(force_field, dict):
             try:
@@ -99,7 +79,7 @@ class BaseLammpsSet(BaseLammpsGenerator):
                 if not isinstance(pair_style, str) or not isinstance(pair_coeff, str) or not isinstance(species, list):
                     raise KeyError
             
-                self.force_field_def = f'pair_style {pair_style}\n pair_coeff {pair_coeff} {species_str}'
+                self.force_field = f'pair_style {pair_style}\n pair_coeff {pair_coeff} {species_str}'
                 
             except KeyError:
                 logger.error(f"Force field parameters (pair_style and pair_coeff) not found in {force_field}, check input format!")
@@ -107,10 +87,8 @@ class BaseLammpsSet(BaseLammpsGenerator):
         else:
             raise TypeError(f"Force field should be a dictionary, got {type(force_field)}")
             
-        if self.force_field_def is not None:
-            self.settings.update({'force_field': self.force_field_def, 'species': species_str})
-        super().__init__(template = self.template, settings = self.settings, **kwargs)
-        
-    @classmethod
-    def generate_sets(cls, settings, **kwargs):
-        return super().__init__(template = cls.template, settings = settings, **kwargs)
+        if self.force_field is not None:
+            self.settings.update({'force_field': self.force_field, 'species': species_str})
+                
+    
+        super().__init__(template = template, settings = self.settings, **kwargs)
