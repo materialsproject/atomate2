dc37da1f0f87c4695f93aaff180f1e80cfbba6eb
Vir Karan <virkaran123456@gmail.com>
2024-11-04 16:37:36 -0800

    Added reudimentary sets for a custom lammps job based on a user provided template and for minimization calcs

--- src/atomate2/lammps/sets/core.py
+++ src/atomate2/lammps/sets/core.py
@@ -1,4 +1,5 @@
-from .base import BaseLammpsSet
+import os
+from .base import BaseLammpsSet, template_dir
 from atomate2.lammps.sets.utils import update_settings, _BASE_LAMMPS_SETTINGS
 from typing import Literal
 
@@ -56,3 +57,19 @@ class LammpsNPTSet(BaseLammpsSet):
         
         self.settings = update_settings(settings=self.settings,**kwargs)
         super().__init__(ensemble=self.ensemble, barostat=self.barostat, **kwargs)
+        
+        
+class LammpsMinimizeSet(BaseLammpsSet):
+    """
+    Input set for minimization simulations.
+    """
+    def __init__(self, **kwargs):
+        template = os.path.join(template_dir, "minimize.template")
+        super().__init__(ensemble='minimize', template=template, **kwargs)
+
+class CustomLammpsSet(BaseLammpsSet):
+    """
+    Input set for custom simulations.
+    """
+    def __init__(self, template : str = None, settings : dict = None, **kwargs):
+        super().__init__(settings=settings, template=template, **kwargs)
