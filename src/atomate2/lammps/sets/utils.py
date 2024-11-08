2b10f7f5b60f775da489ed2eaced8c07c64d85a2
Vir Karan <virkaran123456@gmail.com>
2024-11-08 14:18:36 -0800

    Fixed super init for base set from core sets, fixed update settings func, indent

--- src/atomate2/lammps/sets/utils.py
+++ src/atomate2/lammps/sets/utils.py
@@ -3,49 +3,50 @@ import os
 
 settings_dir = os.path.dirname(os.path.abspath(__file__))
 _BASE_LAMMPS_SETTINGS = loadfn(os.path.join(settings_dir,'BASE_LAMMPS_SETTINGS.json'))
-all_settings_keys = loadfn(os.path.join(settings_dir, 'lammps_settings_keys.json'))
 
-def update_settings(settings : dict = None, **kwargs):
+def update_settings(settings : dict = None, **kwargs) -> dict:
     """
-    Update the settings for the LAMMPS input file.
+    Update the settings for the LAMMPS input file. 
     """
     
     base_settings = _BASE_LAMMPS_SETTINGS.copy()
-        
+
     if settings is None:
         settings = base_settings
     
     for k in base_settings.keys():
         if k not in kwargs.keys():
             settings.update({k: base_settings.get(k)})
+        else:
+            settings.update({k: kwargs.get(k)})
     
-    return base_settings
+    return settings
 
 
-def process_ensemble_conditions(settings : dict):
-        """
-        Process the ensemble conditions for the LAMMPS input file.
-        """
-        ensemble = settings.get('ensemble', 'nve')
-        for k in ["nve", "nvt", "npt"]:
-            settings.update({f'{k}_flag': '#'})
+def process_ensemble_conditions(settings : dict) -> dict:
+    """
+    Process the ensemble conditions for the LAMMPS input file.
+    """
+    ensemble = settings.get('ensemble', 'nve')
+    for k in ["nve", "nvt", "npt"]:
+        settings.update({f'{k}_flag': '#'})
+        
+    if ensemble == 'nvt':
+        if settings.get('thermostat') == 'langevin':
+            settings.update({'nve_flag': 'fix', 'thermseed': settings.get('seed', 0)})
             
-        if ensemble == 'nvt':
-            if settings.get('thermostat') == 'langevin':
-                settings.update({'nve_flag': 'fix', 'thermseed': settings.get('seed', 0)})
-                
-            else: 
-                settings.update({'thermostat': 'nvt temp'})
+        else: 
+            settings.update({'thermostat': 'nvt temp'})
+    
         
-            
-        if ensemble == 'npt':
-            if settings.get('barostat') == 'berendsen':
-                settings.update({'nve_flag': 'fix', 'barostat': 'press/berendsen'})
-            else:
-                settings.update({'barostat': 'npt temp'})
-        
-        if ensemble == 'nph':
-            settings.update({'npt_flag': 'fix', 'barostat': 'nph'})
-        
-        settings.update({f'{ensemble}_flag': 'fix'}) if ensemble in ["nve", "nvt", "npt"] else None
-        return settings
+    if ensemble == 'npt':
+        if settings.get('barostat') == 'berendsen':
+            settings.update({'nve_flag': 'fix', 'barostat': 'press/berendsen'})
+        else:
+            settings.update({'barostat': 'npt temp'})
+    
+    if ensemble == 'nph':
+        settings.update({'npt_flag': 'fix', 'barostat': 'nph'})
+    
+    settings.update({f'{ensemble}_flag': 'fix'}) if ensemble in ["nve", "nvt", "npt"] else None
+    return settings
