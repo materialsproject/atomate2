1dc96a5685a91d82b3c5dcee6972e49a75d578db
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-11-12 19:00:39 -0500

    add asserts in cp2k/test_drones.py test_structure_optimization

--- tests/cp2k/test_drones.py
+++ tests/cp2k/test_drones.py
@@ -1,6 +1,24 @@
+import pytest
+
+
 def test_structure_optimization(cp2k_test_dir):
     from atomate2.cp2k.drones import Cp2kDrone
+    from atomate2.cp2k.schemas.task import TaskDocument
 
     drone = Cp2kDrone()
     doc = drone.assimilate(cp2k_test_dir / "Si_band_structure" / "static" / "outputs")
-    assert doc
+    assert isinstance(doc, TaskDocument)
+    assert doc.output.energy == pytest.approx(-197.4, abs=1e-1)
+    assert doc.output.forces == pytest.approx(
+        [(-1e-08, -1e-08, -1e-08), (2e-08, 2e-08, 2e-08)], abs=1e-1
+    )
+    assert doc.output.stress is None
+    assert doc.output.bandgap is None
+    assert doc.output.cbm == pytest.approx(7.65104451, abs=1e-1)
+    assert doc.output.vbm == pytest.approx(5.09595793, abs=1e-1)
+    assert doc.output.structure.lattice.a == pytest.approx(3.8669, abs=1e-1)
+    assert doc.completed_at == "2023-11-18 00:05:01.267082+00:00"
+    assert doc.state.value == "successful"
+    assert doc.task_label is None
+    assert doc.tags is None
+    assert {*doc.run_stats} == {"overall", "standard"}
