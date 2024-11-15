f659079897e2056385378c03ea17b8d66db6574b
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-11-15 15:44:36 -0500

    fix typo in get_supercell_size tests: orhtorhombic

--- tests/forcefields/test_phonon.py
+++ tests/forcefields/test_phonon.py
@@ -28,7 +28,7 @@ def test_supercell_orthorhombic(clean_dir, si_structure: Structure):
         min_length=5,
         max_length=10,
         prefer_90_degrees=False,
-        allow_orhtorhombic=True,
+        allow_orthorhombic=True,
     )
 
     # run the flow or job and ensure that it finished running successfully
@@ -43,7 +43,7 @@ def test_supercell_orthorhombic(clean_dir, si_structure: Structure):
         min_length=5,
         max_length=10,
         prefer_90_degrees=True,
-        allow_orhtorhombic=True,
+        allow_orthorhombic=True,
     )
 
     # run the flow or job and ensure that it finished running successfully
