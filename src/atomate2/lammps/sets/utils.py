bbaf4d464ec740ce2cc45d16a48a22771c383595
Vir Karan <virkaran123456@gmail.com>
2024-11-15 12:04:05 -0800

    New LammpsInterchange class to allow for serialization of Interchange, moved Interchange definition to utils, default value for force_field now needs a pair_style if an interchage is used as input (using lj/cut/coul/cut 10 as default pair_stlye for now)

--- src/atomate2/lammps/sets/utils.py
+++ src/atomate2/lammps/sets/utils.py
@@ -1,8 +1,56 @@
 from monty.serialization import loadfn
 import os
+import json
+
+try:
+    from openff.interchange import Interchange
+except ImportError:
+
+    class Interchange:  # type: ignore[no-redef]
+        """Dummy class for failed imports of Interchange."""
+
+        def model_validate(self, _: str) -> None:
+            """Parse raw is the first method called on the Interchange object."""
+            raise ImportError(
+                "openff-interchange must be installed for OpenMM makers to"
+                "to support OpenFF Interchange objects."
+            )
 
 settings_dir = os.path.dirname(os.path.abspath(__file__))
 _BASE_LAMMPS_SETTINGS = loadfn(os.path.join(settings_dir,'BASE_LAMMPS_SETTINGS.json'))
+
+
+class LammpsInterchange(Interchange):
+    """
+    A subclass of Interchange that adds a method to convert the object to a dictionary.
+    """
+    
+    @classmethod
+    def from_interchange(cls, interchange: Interchange) -> None:
+        """
+        Load an Interchange object.
+        """
+        
+        return cls(topology=interchange.topology,
+                  collections=interchange.collections,
+                  box=interchange.box,
+                  positions=interchange.positions,
+                  velocities=interchange.velocities,
+                  mdconfig=interchange.mdconfig
+                  )
+    
+    def as_dict(self) -> dict:
+        """
+        Convert the Interchange object to a dictionary.
+        """
+        return json.loads(self.json())
+    
+    @classmethod
+    def from_dict(cls, d: dict) -> "LammpsInterchange":
+        """
+        Load an Interchange object from a dictionary.
+        """
+        return cls(**d)
 
 def update_settings(settings : dict = None, **kwargs) -> dict:
     """
