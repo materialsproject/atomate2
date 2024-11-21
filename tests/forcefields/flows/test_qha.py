a87f0efe2f1e7b5efc326f7efca838cf4144c755
JaGeo <janinegeorge.ulfen@gmail.com>
2024-11-21 20:01:39 +0100

    fix tests

--- tests/forcefields/flows/test_qha.py
+++ tests/forcefields/flows/test_qha.py
@@ -67,3 +67,35 @@ def test_qha_dir_change_defaults(clean_dir, si_structure: Structure, tmp_path: P
     assert len(ph_bs_dos_doc.volumes) == 5
     assert ph_bs_dos_doc.volumes[0] == pytest.approx(ph_bs_dos_doc.volumes[2] * 0.97**3)
     assert ph_bs_dos_doc.volumes[4] == pytest.approx(ph_bs_dos_doc.volumes[2] * 1.03**3)
+
+
+def test_qha_dir_manual_supercell(clean_dir, si_structure: Structure, tmp_path: Path):
+    # TODO brittle due to inability to adjust dtypes in CHGNetRelaxMaker
+
+    flow = CHGNetQhaMaker(
+        number_of_frames=4,
+        ignore_imaginary_modes=True,
+        min_length=10,
+        phonon_maker=PhononMaker(
+            store_force_constants=False,
+            bulk_relax_maker=None,
+            generate_frequencies_eigenvectors_kwargs={
+                "tol_imaginary_modes": 5e-1,
+                "tmin": 0,
+                "tmax": 1000,
+                "tstep": 10,
+            },
+        ),
+    ).make(si_structure)
+
+    # run the flow or job and ensure that it finished running successfully
+    responses = run_locally(flow, create_folders=True, ensure_success=True)
+
+    # print(responses)
+
+    # # validate the outputs
+    ph_bs_dos_doc = responses[flow[-1].uuid][1].output
+    assert isinstance(ph_bs_dos_doc, PhononQHADoc)
+    assert len(ph_bs_dos_doc.volumes) == 5
+    assert ph_bs_dos_doc.volumes[0] == pytest.approx(ph_bs_dos_doc.volumes[2] * 0.97**3)
+    assert ph_bs_dos_doc.volumes[4] == pytest.approx(ph_bs_dos_doc.volumes[2] * 1.03**3)
