0d11119b78636a61a98da6a298412d34c6a80e2a
Vir Karan <virkaran123456@gmail.com>
2024-12-09 19:46:27 -0800

    Added CustomLammpsMaker, cleaned up comments

--- src/atomate2/lammps/jobs/core.py
+++ src/atomate2/lammps/jobs/core.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from atomate2.lammps.sets.core import BaseLammpsSetGenerator, LammpsNVTSet, LammpsNPTSet, LammpsMinimizeSet
+from atomate2.lammps.sets.core import BaseLammpsSetGenerator, LammpsNVTSet, LammpsNPTSet, LammpsMinimizeSet, LammpsInputFile
 from atomate2.lammps.jobs.base import BaseLammpsMaker
 
 @dataclass
@@ -19,9 +19,34 @@ class MinimizationMaker(BaseLammpsMaker):
 
 @dataclass
 class CustomLammpsMaker(BaseLammpsMaker):
+    '''
+    Custom LAMMPS job maker. Use this if you want to use a custom LAMMPS input file.
+    This is esp. useful if you have a more complex job that cannot be achieved with a 
+    combination of minimization, NVT, and NPT jobs. 
+    
+    args:
+        name: str
+            Name of the job
+        inputfile: str | LammpsInputFile
+            Path to the LAMMPS input file or a LammpsInputFile object, can be read with pmg.io.lammps.inputs.LammpsInputFile
+            (Note: make sure pymatgen can read the file correctly before passing it to the job here)
+        settings: dict
+            Additional settings to pass to the input set generator 
+            (here essentially acts as metadata, not actually passed to input set generator cause of how flexible input files can be!)
+        keep_stages: bool
+            Whether to keep the stages of the input file (default is True). 
+            Check the LammpsInputFile class for more info on what this means.
+    '''
     name: str = "custom_lammps_job"
-    template : str = None
+    inputfile : str | LammpsInputFile = field(default=None)
     settings : dict = field(default_factory=dict)
+    keep_stages : bool = field(default=True)
     
     def __post_init__(self):
-        self.input_set_generator = BaseLammpsSetGenerator(template=self.template, settings=self.settings)
+        if not self.inputfile:
+            raise ValueError("Input file not specified. Use this maker only if you have a custom LAMMPS input file!")
+        self.input_set_generator = BaseLammpsSetGenerator(inputfile=self.inputfile,
+                                                          settings=self.settings,
+                                                          calc_type=self.name,
+                                                          override_updates=True,
+                                                          keep_stages=self.keep_stages)
