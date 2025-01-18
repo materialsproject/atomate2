9864f8d74874c7a0a6c172dd194381964fe7c1f1
Shehan M Parmar <parmar@login34.chn.perlmutter.nersc.gov>
2025-01-17 16:21:47 -0800

    the use of io.StringIO is seemingly a bug? works fine without needing to convert xml to string, leaving this commented for now before checking other cases

--- src/atomate2/openmm/jobs/generate.py
+++ src/atomate2/openmm/jobs/generate.py
@@ -42,7 +42,8 @@ class XMLMoleculeFF:
 
     def __init__(self, xml_string: str) -> None:
         """Create an XMLMoleculeFF object from a string version of the XML file."""
-        self.tree = ET.parse(io.StringIO(xml_string))  # noqa: S314
+        #self.tree = ET.parse(io.StringIO(xml_string))  # noqa: S314
+        self.tree = ET.parse(xml_string)  # noqa: S314
 
         root = self.tree.getroot()
         canonical_order = {}
