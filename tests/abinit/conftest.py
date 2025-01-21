400ac8721952b16dde20e024fe6c910cb0c1375d
jmmshn <jmmshn@gmail.com>
2025-01-21 13:20:57 -0800

    view failing abinit tests
    ["The variable 'tsmear' is different in the two files:\n - this file:
    '0.0036749322175665 Ha'\n - other file: '0.0036749322175655 Ha'"]

--- tests/abinit/conftest.py
+++ tests/abinit/conftest.py
@@ -132,7 +132,7 @@ def check_run_abi(ref_path: str | Path):
     # Ignore the pseudos as the directory depends on the pseudo root directory
     diffs = user.get_differences(ref, ignore_vars=["pseudos"])
     # TODO: should we still add some check on the pseudos here ?
-    assert diffs == [], "'run.abi' is different from reference."
+    assert diffs == [], f"'run.abi' is different from reference.\n{diffs}"
 
 
 def check_abinit_input_json(ref_path: str | Path):
