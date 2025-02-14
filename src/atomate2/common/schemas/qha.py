59e8b94e75691cea44f8c6d85c4be70b282ae7b5
JaGeo <janinegeorge.ulfen@gmail.com>
2025-02-14 07:42:52 +0100

    fix linting

--- src/atomate2/common/schemas/qha.py
+++ src/atomate2/common/schemas/qha.py
@@ -38,7 +38,7 @@ class PhononQHADoc(StructureMetadata, extra="allow"):  # type: ignore[call-arg]
     helmholtz_volume: Optional[list[list[float]]] = Field(
         None,
         description="Free energies (eV) at temperatures and volumes (Angstrom^3)."
-        "shape (temperatures, volumes)", #TODO: add units here
+        "shape (temperatures, volumes)",  # TODO: add units here
     )
     volume_temperature: Optional[list[float]] = Field(
         None,
--- tests/test_data/vasp/Si_qha_2/dft_phonon_static_eos_deformation_1/submit.sh
+++ tests/test_data/vasp/Si_qha_2/dft_phonon_static_eos_deformation_1/submit.sh
@@ -12,8 +12,8 @@
 #SBATCH --error=/hppfs/scratch/00/di82tut/autoplex_test/run/8a/70/ab/8a70ab31-57c5-4b5d-a2f3-1d23e48fc4a1_1/queue.err
 #SBATCH --get-user-env
 cd /hppfs/scratch/00/di82tut/autoplex_test/run/8a/70/ab/8a70ab31-57c5-4b5d-a2f3-1d23e48fc4a1_1
-export ATOMATE2_CONFIG_FILE="/dss/dsshome1/00/di82tut/.atomate2/config/atomate2.yaml" 
-source activate autoplex_test 
+export ATOMATE2_CONFIG_FILE="/dss/dsshome1/00/di82tut/.atomate2/config/atomate2.yaml"
+source activate autoplex_test
 module load slurm_setup
 module load vasp/6.1.2
 
