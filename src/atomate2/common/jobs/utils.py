e6c9476e49407e7b13b9cac2415b211594fe6a91
Aaron Kaplan <33381112+esoteric-ephemera@users.noreply.github.com>
2025-03-12 13:48:57 -0700

    Add updated MP workflows + cleanup (#1139)
    * Update pyproject.toml
    
    * Update pyproject.toml
    
    * test a new pymatgen version
    
    * Update pyproject.toml
    
    * Update test_utils.py
    
    * redraft mp flows
    
    * split off openff tests
    
    * remove deprecated flows (completely forgot at the start of the year
    
    * fix pin in strict openff wf
    
    * correctly partition tests?
    
    * revert openff_md test ref change
    
    * openff test over py310-312
    
    ---------
    
    Co-authored-by: J. George <JaGeo@users.noreply.github.com>

--- src/atomate2/common/jobs/utils.py
+++ src/atomate2/common/jobs/utils.py
@@ -2,14 +2,20 @@
 
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
 
 from jobflow import Response, job
+from monty.os.path import zpath
+from pymatgen.command_line.bader_caller import bader_analysis_from_path
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 from atomate2 import SETTINGS
 
 if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from pathlib import Path
+
     from pymatgen.core import Structure
 
 
@@ -137,3 +143,62 @@ def retrieve_structure_from_materials_project(
         output=structure,
         stored_data={"task_id": task_id, "database_version": database_version},
     )
+
+
+@job
+def remove_workflow_files(
+    directories: Sequence[str], file_names: Sequence[str], allow_zpath: bool = True
+) -> list[str]:
+    """
+    Remove files from previous jobs.
+
+    For example, at the end of an MP flow, WAVECAR files are generated
+    that take up a lot of disk space.
+    This utility can automatically remove them following a workflow.
+
+    Parameters
+    ----------
+        makers : Sequence[Maker, Flow, Job]
+            Jobs in the flow on which to remove files.
+        file_names : Sequence[str]
+            The list of file names to remove, ex. ["WAVECAR"] rather than a full path
+        allow_zpath : bool = True
+            Whether to allow checking for gzipped output using `monty.os.zpath`
+
+    Returns
+    -------
+        list[str] : list of removed files
+    """
+    abs_paths = [os.path.abspath(dir_name.split(":")[-1]) for dir_name in directories]
+
+    removed_files = []
+    for job_dir in abs_paths:
+        for file in file_names:
+            file_name = os.path.join(job_dir, file)
+            if allow_zpath:
+                file_name = zpath(file_name)
+
+            if os.path.isfile(file_name):
+                removed_files.append(file_name)
+                os.remove(file_name)
+
+    return removed_files
+
+
+@job
+def bader_analysis(dir_name: str | Path, suffix: str | None = None) -> dict:
+    """Run Bader charge analysis as a job.
+
+    Parameters
+    ----------
+    dir_name : str or Path
+        The name of the directory to run Bader in.
+    suffix : str or None (default)
+        Suffixes of the files to filter by.
+
+    Returns
+    -------
+    dict of bader charge analysis which is JSONable.
+    """
+    dir_name = os.path.abspath(str(dir_name).split(":")[-1])
+    return bader_analysis_from_path(dir_name, suffix=suffix or "")
