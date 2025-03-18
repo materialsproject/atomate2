7d63f58e2ab776ae0fe5d629ee6d83946b286474
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-03-18 02:06:19 -0700

    add matpes models / bump matgl

--- src/atomate2/forcefields/__init__.py
+++ src/atomate2/forcefields/__init__.py
@@ -24,6 +24,8 @@ class MLFF(Enum):  # TODO inherit from StrEnum when 3.11+
     NEP = "NEP"
     Nequip = "Nequip"
     SevenNet = "SevenNet"
+    MATPES_R2SCAN = "MatPES-r2SCAN"
+    MATPES_PBE = "MatPES-PBE"
 
     @classmethod
     def _missing_(cls, value: Any) -> Any:
