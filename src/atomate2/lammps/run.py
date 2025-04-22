e8e70732a37ae8487071cc9201fda52586a9d0a5
Vir Karan <virkaran123456@gmail.com>
2025-04-21 19:31:38 -0700

    Consolidate lammps run commands

--- src/atomate2/lammps/run.py
+++ src/atomate2/lammps/run.py
@@ -1,3 +1,5 @@
+"""Wrapper to invoke lammps from the CLI."""
+
 import subprocess
 from pathlib import Path
 
@@ -7,6 +9,7 @@ from atomate2 import SETTINGS
 def run_lammps(
     lammps_input_file: str = "in.lammps",
     lammps_cmd: str = SETTINGS.LAMMPS_CMD,
+    lammps_mpi_cmd: str | None = SETTINGS.MPI_CMD,
     lammps_suffix: list[str] | str | None = SETTINGS.LAMMPS_SUFFIX,
     lammps_pks: list[str] | str | None = SETTINGS.LAMMPS_PACKAGES,
     lammps_run_flags: list[str] | str | None = None,
@@ -15,21 +18,18 @@ def run_lammps(
 ) -> subprocess.Popen:
     """Run LAMMPS.
 
-    Parameters:
+    Parameters
+    ----------
         lammps_input_file: The path to the main input file to be passed to the
             LAMMPS executable with the `-in` command-line option.
         lammps_cmd: The name or path to the LAMMPS executable.
+        lammps_mpi_cmd: The command to invoke MPI (e.g., `'mpirun'` or `'mpiexec'`).
+            If None, invoke the `lammps_cmd` in serial mode.
         lammps_suffix: The suffix to use that applies style variants at runtime
             (see `LammpsSettings.LAMMPS_SUFFIX`).
         lammps_pks: The runtime packages and options to tell LAMMPS to use
             (see `LammpsSettings.LAMMPS_PACKAGES`).
         lammps_run_kwargs: Any additional arbitrary flags to invoke LAMMPS with.
-        mpi_cmd: The command to invoke MPI (e.g., `'mpirun'` or `'mpiexec'`).
-            If None, invoke the `lammps_cmd` in serial mode.
-        mpi_num_processes: The number of MPI processes to pass to `mpi_cmd` with
-            the `mpi_num_processes_flag`.
-        mpi_num_processes_flag: The command-line flag to use with MPI to pass the
-            number of processes, e.g., `-n/n` [default] or `np/-np`.
         max_walltime_hours: The maximum walltime in hours to allow for the task. If
             provided, attempts will be made to cleanly end the calculation after this
             amount of time. Note: if using with a queueing system, this value should
@@ -39,7 +39,6 @@ def run_lammps(
         stderr_file: The name of or path to a file in which to save the stderr stream.
 
     """
-
     lammps_invocation: list[str] = []
 
     if lammps_suffix is not None:
@@ -60,7 +59,10 @@ def run_lammps(
         for flag in lammps_run_flags:
             lammps_invocation += [flag]
 
-    lammps_invocation.extend([lammps_cmd, "-in", lammps_input_file])
+    if lammps_mpi_cmd is not None:
+        lammps_invocation.extend([lammps_mpi_cmd, "-in", lammps_input_file])
+    else:
+        lammps_invocation.extend([lammps_cmd, "-in", lammps_input_file])
 
     with open(stdout_file, "a") as stdout, open(stderr_file, "a") as stderr:
         process = subprocess.Popen(
