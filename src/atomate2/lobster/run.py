4539bcf583aaa7ff0ce00f6b6eef064c0d0ced19
Andrew S. Rosen <asrosen93@gmail.com>
2025-08-13 20:58:00 -0400

    Update run.py

--- src/atomate2/lobster/run.py
+++ src/atomate2/lobster/run.py
@@ -95,6 +95,7 @@ def run_lobster(
         validators=validators,
         max_errors=max_errors,
         scratch_dir=scratch_dir,
+        gzipped_output=SETTINGS.CUSTODIAN_GZIPPED_OUTPUT,
         **custodian_kwargs,
     )
 
