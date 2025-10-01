d94f37265120cb460696523ed608ecdde0047df1
Vir Karan <virkaran123456@gmail.com>
2025-10-01 13:54:26 -0700

    update tests to work with new input structure

--- tests/lammps/test_sets.py
+++ tests/lammps/test_sets.py
@@ -1,11 +1,21 @@
-from atomate2.lammps.sets.core import LammpsNVTSet, LammpsNPTSet, LammpsMinimizeSet
+from atomate2.lammps.sets.core import LammpsMinimizeSet, LammpsNVTSet
 
-def test_LammpsNVTSet():
-    nvt = LammpsNVTSet(timestep=0.005, thermostat='langevin')
-    assert nvt.ensemble.value == 'nvt'
-    assert nvt.settings.thermostat == 'langevin'
-    assert nvt.settings.timestep == 0.005
-    
+
+def test_nvt_set():
+    nvt = LammpsNVTSet(timestep=0.005, thermostat="langevin")
+    if isinstance(nvt.settings, dict):
+        assert nvt.settings["ensemble"] == "nvt"
+        assert nvt.settings["thermostat"] == "langevin"
+        assert nvt.settings["timestep"] == 0.005
+    else:
+        assert nvt.settings.ensemble == "nvt"
+        assert nvt.settings.thermostat == "langevin"
+        assert nvt.settings.timestep == 0.005
+
+
 def test_minimize_set():
     mini = LammpsMinimizeSet()
-    assert mini.settings.ensemble == 'minimize'
+    if isinstance(mini.settings, dict):
+        assert mini.settings["ensemble"] == "minimize"
+    else:
+        assert mini.settings.ensemble == "minimize"
