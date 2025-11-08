0cb1d00d1812769f427812c9df1796f338740e43
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-11-07 16:10:03 -0800

    support one_to_many transformations, and add test for both behaviors

--- tests/common/jobs/test_transform.py
+++ tests/common/jobs/test_transform.py
@@ -7,12 +7,16 @@ except ImportError:
 
 import numpy as np
 import pytest
-from jobflow import run_locally
+from jobflow import Flow, run_locally
 from pymatgen.core import Structure
 from pymatgen.transformations.advanced_transformations import SQSTransformation
+from pymatgen.transformations.standard_transformations import (
+    OrderDisorderedStructureTransformation,
+    OxidationStateDecorationTransformation,
+)
 
-from atomate2.common.jobs.transform import SQS
-from atomate2.common.schemas.transform import SQSTask
+from atomate2.common.jobs.transform import SQS, Transformer
+from atomate2.common.schemas.transform import SQSTask, TransformTask
 
 
 @pytest.fixture(scope="module")
@@ -30,6 +34,43 @@ def simple_alloy() -> Structure:
         [{"Mg": 0.5, "Al": 0.5}, {"Mg": 0.5, "Al": 0.5}],
         [[0.0, 0.0, 0.0], [1.0 / 3.0, 2.0 / 3.0, 0.5]],
     )
+
+
+def test_simple_and_advanced():
+    # simple disordered zincblende structure
+    structure = Structure(
+        3.8 * np.array([[0.0, 0.5, 0.5], [0.5, 0.0, 0.5], [0.5, 0.5, 0.0]]),
+        ["Zn", {"S": 0.75, "Se": 0.25}],
+        [[0.0, 0.0, 0.0], [0.25, 0.25, 0.25]],
+    ).to_conventional()
+
+    oxi_dict = {"Zn": 2, "S": -2, "Se": -2}
+    oxi_job = Transformer(
+        name="oxistate", transformation=OxidationStateDecorationTransformation(oxi_dict)
+    ).make(structure)
+
+    odst_job = Transformer(
+        name="odst", transformation=OrderDisorderedStructureTransformation()
+    ).make(oxi_job.output.final_structure, return_ranked_list=2)
+
+    flow = Flow([oxi_job, odst_job])
+    resp = run_locally(flow)
+
+    oxi_state_output = resp[oxi_job.uuid][1].output
+    assert isinstance(oxi_state_output, TransformTask)
+
+    # check correct assignment of oxidation states
+    assert all(
+        specie.oxi_state == oxi_dict.get(specie.element.value)
+        for site in oxi_state_output.final_structure
+        for specie in site.species
+    )
+
+    odst_output = resp[odst_job.uuid][1].output
+    # return_ranked_list = 2, so should get 2 output docs
+    assert len(odst_output) == 2
+    assert all(isinstance(doc, TransformTask) for doc in odst_output)
+    assert all(doc.final_structure.is_ordered for doc in odst_output)
 
 
 @pytest.mark.skipif(
