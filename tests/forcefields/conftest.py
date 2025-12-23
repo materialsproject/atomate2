b472bc5759f7e7edbbe5cc5657a3d22022352b76
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-12-23 10:14:56 -0800

    restore torch dtype stuff

--- tests/forcefields/conftest.py
+++ tests/forcefields/conftest.py
@@ -4,9 +4,21 @@ import hashlib
 import tempfile
 import urllib.request
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import pytest
+import torch
 from emmet.core.utils import get_hash_blocked
+
+if TYPE_CHECKING:
+    from typing import Any
+
+
+def pytest_runtest_setup(item: Any) -> None:
+    # MACE changes the default dtype, ensure consistent dtype here
+    torch.set_default_dtype(torch.float32)
+    # For consistent performance across hardware, explicitly set device to CPU
+    torch.set_default_device("cpu")
 
 
 @pytest.fixture(scope="session", autouse=True)
