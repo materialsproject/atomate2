10d9fab09fd27b549bb1b4cb52db178211b30469
Andrew S. Rosen <asrosen93@gmail.com>
2026-03-06 18:17:50 -0500

    Update qha.py

--- src/atomate2/forcefields/flows/qha.py
+++ src/atomate2/forcefields/flows/qha.py
@@ -127,20 +127,15 @@ class ForceFieldQhaMaker(CommonQhaMaker):
             eos_relax_maker=(
                 ForceFieldRelaxMaker(
                     force_field_name=force_field_name,
+                    calculator_kwargs=calculator_kwargs,
                     relax_cell=False,
                     relax_kwargs={"fmax": 1e-5},
                 )
                 if run_eos_flow
                 else None
             ),
-            phonon_maker = (
-                PhononMaker.from_force_field_name(
-                    force_field_name=force_field_name,
-                    calculator_kwargs=calculator_kwargs,
-                    relax_initial_structure=False
-                )
-            ),
-        )
+        phonon_maker = PhononMaker.from_force_field_name(
+            force_field_name=force_field_name,calculator_kwargs=calculator_kwargs, relax_initial_structure=False
         return cls(
             phonon_maker=phonon_maker,
             name=f"{phonon_maker.mlff.name} QHA Maker",
