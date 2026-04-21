b576a2d2f80d6589472b196fde0f4279e83a1d9c
Andrei Sobolev <sobolev@ms1p.org>
2026-04-21 16:40:56 +0200

    Remove unused import

--- src/atomate2/aims/jobs/core.py
+++ src/atomate2/aims/jobs/core.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Sequence
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import TYPE_CHECKING
